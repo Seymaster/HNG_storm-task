d2cba6a584e9ff89d57d2b1000326db1232bdb25
Victor Ogbonna <victorgbonna@gmail.com>
2020-05-31 12:10:38 +0100

    committed the file

---
+++ scripts/victor.py
@@ -0,0 +1,10 @@
+import json
+data={
+    "first_name": "victor",
+    "last_name": "ogbonna",
+    "language": "Python",
+    "id": "HNG-03881",
+    "email":"victorgbonna@gmail.com"    
+}
+json_data=json.dumps(data)
+print('Hello world, this is [Victor] [Ogbonna] with HNGi7 ID [HNG-03881] using [Python] for stage 2 task (email=[victorgbonna@gmail.com])')
