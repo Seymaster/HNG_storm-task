1a95e39d2cfbcc9fcd7f5c563b07c1fdd43c839c
yanmifeakeju <yanmifeakeju@gmail.com>
2020-06-03 00:08:43 +0100

    python and javascript seems fine

---
+++ scripts/n.js
@@ -0,0 +1,12 @@
+function output() {
+  const data = {
+    firstName: "Oluwayanmife",
+    last: "Akeju",
+    language: "PHP & JS",
+    id: "HNG-00197",
+  };
+
+  return "Hello World";
+}
+
+console.log(output());
