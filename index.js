1df577abe200b7deb330c35585ad75c2a1c587d4
unknown <temiloluwaabiodunojo@gamail.com>
2020-06-02 20:16:35 +0100

    HNG task 2 script

---
+++ index.js
@@ -0,0 +1,5 @@
+const ID = "HNG-04539"
+const firstName = "Temiloluwa"
+const lastName = "Abiodun-Ojo"
+const result =`Hello World, this is ${firstName} ${lastName} with HNG ID ${ID}`
+console.log(result)
