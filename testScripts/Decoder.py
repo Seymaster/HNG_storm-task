329f470b8a60204b0c55d77644e48eb49bd9f49c
Gabriel01osabuede <51996566+Gabriel01osabuede@users.noreply.github.com>
2020-06-03 20:08:25 +0100

    committed my python script to the test script folder

---
+++ testScripts/Decoder.py
@@ -0,0 +1,15 @@
+import json
+
+data = {
+    "firstName": "Andrew",
+    "lastName": "Gabriel",
+    "language": "Python",
+    "id": "HNG-04755",
+    "email": "buedgabby@gmail.com"
+}
+
+json_data = json.dumps(data)
+
+print("Hello World, this is [Andrew] [Gabriel] with HNGi7 ID [HNG-04755] using [Python] for stage 2 task. buedgabby@gmail.com")
+
+
