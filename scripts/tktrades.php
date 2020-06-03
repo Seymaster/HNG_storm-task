40593ae0bc558c724c3ad6b1487e04d4f7934cbf
tktrades <39212335+tktrades@users.noreply.github.com>
2020-06-03 21:08:55 +0800

    Update tktrades.php
    added email and stage variable to the original file

--- scripts/tktrades.php
+++ scripts/tktrades.php
@@ -10,9 +10,11 @@
  $myvar1 = "Hello world,";
  $myvar2 = " this is";
  $fullname =" Koyi olamilekan";
- $HNGi7id = " With HNGi7 ID 04682.";
-  $language = " Using php";
-  echo "$myvar1". $myvar2 . $fullname . $HNGi7id . $language;
+ $HNGi7id = " With HNGi7 ID 04682,";
+ $email = "And email lekan.koyi@gmail.com";
+  $language = " Using php for ";
+  $stage = " stage 2 task";
+  echo "$myvar1" . $myvar2 . $fullname . $HNGi7id . $email . $language . $stage;
 
 
 ?>
