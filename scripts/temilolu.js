b8bc2f7ee03c27f0d4fdd88f14bb0515abdae98d
unknown <temiloluwaabiodunojo@gamail.com>
2020-06-03 11:59:10 +0100

    my_script

---
+++ scripts/temilolu.js
@@ -0,0 +1,6 @@
+const ID = "HNG-04539"
+const firstName = "Temiloluwa"
+const lastName = "Abiodun-Ojo"
+const language ="JavaScript"
+const result =`Hello World, this is ${firstName} ${lastName} with HNG ID ${ID} using ${language} for stage 2 task`
+console.log(result)
