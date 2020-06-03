efefdc006cf3a862a6a30df4041e88b20e883852
Oluwatosin Jimoh <57687166+ekiira@users.noreply.github.com>
2020-06-03 19:11:37 +0100

    Update script

--- scripts/Jay.js
+++ scripts/Jay.js
@@ -1,12 +1,12 @@
 const output = () => {
-  const data = {
+  const myData = {
     firstName: "Oluwatosin",
     lastName: "Jimoh",
     language: "Javascript",
     id: "HNG-00977",
     email: "jayoluwatosin@gmail.com"
   };
-  return `Hello World, this is ${data.firstName} ${data.lastName} with HNGi7 ID ${data.id} using ${data.language} for stage 2 task. ${data.email}`
+  return `Hello World, this is ${myData.firstName} ${myData.lastName} with HNGi7 ID ${myData.id} using ${myData.language} for stage 2 task. ${myData.email}`
 }
 
 console.log(output());
