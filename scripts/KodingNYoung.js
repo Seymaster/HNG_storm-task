86f4317e8a4195356de72e53637f4c21acbfb9f8
Adebambo Abiodun Oluwasegun <abiodunadebambo869@yahoo.com>
2020-06-03 17:00:08 +0100

    Added task 2 file for KodingNYoung

---
+++ scripts/KodingNYoung.js
@@ -0,0 +1,7 @@
+const fullName = "Adebambo Abiodun",
+	hngi7ID = "HNG-00695",
+	language = "Javascript",
+	email = "abiodunadebambo869@yahoo.com",
+	output = `Hello World, this is ${fullName} with HNGi7 ID ${hngi7ID} using ${language} for stage 2 task. ${email}`;
+
+console.log(output);
