ed98ae5378ba7643d43c1569affdb20ac38f86de
ejooremmanuel <55317332+ejooremmanuel@users.noreply.github.com>
2020-06-03 20:56:45 +0100

    ejooremmanuel
    ejooremmanuel

---
+++ scripts/ejooremmanuel.php
@@ -0,0 +1,14 @@
+<?php
+ 
+
+ 	$first_name = 'Emmanuel';
+ 	$last_name = 'Ejoor'
+ 	$id = 'HNG-04434';
+ 	$language = 'php';
+ 	$email = 'ejooremmanuel@gmail.com';
+ 	echo '<br>';
+ 	echo('Hello World! This is '. '<b>'.$first_name.'</b>'.'<b>'.$last_name.'</b>'. ' with HNGi7 ID '.'<b>'. $id .'</b>'. ' using '.'<b>'. $language .'</b>'. ' for stage 2 task. '.$email);
+
+
+?>
+
