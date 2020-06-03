959faedab2f6e5967cea4993df93a2c67ce89807
gurusuccess <31377258+gurusuccess@users.noreply.github.com>
2020-06-03 16:32:32 -0400

    Create ezenwata.php

---
+++ scripts/ezenwata.php
@@ -0,0 +1,8 @@
+<?php
+$firstname = "Ezenwata";
+$lastname = "Destiny";
+$id = "HNG-05063";
+$lang = "PHP";
+$email = "ezenwatairoegbu97@gmail.com";
+	echo "Hello World, this is [$firstName] [$LastName] with HNGi7 ID [$id] using [$lang] for stage 2 task. $email";
+	//By ezenwata
