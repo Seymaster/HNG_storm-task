25367ff4b789f35548908251e65e52441c87138a
Folajomi Shotunde <folajor.mee@gmail.com>
2020-06-03 14:30:59 +0100

    Upload my script - Folajomi Shotunde

--- scripts/frontend_folajomi.js
+++ scripts/frontend_folajomi.js
@@ -1,6 +1,7 @@
 const name = "Folajomi Shotunde";
+const email = "folajor.mee@gmail.com"
 const id = "HNG-06103"
 
-const output = () => `Hello world, this is ${name} with HNG ID ${id}, using JavaScript for stage 2 task`
+const output = () => `Hello world, this is ${name} with HNG ID ${id}, and email ${email} using JavaScript for stage 2 task`
 
 console.log(output());
