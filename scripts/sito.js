8ebc74576a14577598bca5636e1abf275ee959ea
sito-hub <66259019+sito-hub@users.noreply.github.com>
2020-06-03 18:03:47 +0100

    Update sito.js

--- scripts/sito.js
+++ scripts/sito.js
@@ -6,7 +6,7 @@ function sitoInformation(){
   email: "ogundare_taiwo1@yahoo.com",
   language: "Javascript",
   fullHistory: function() {
-    return `Hello world, this is ${this.firstName} ${this.lastName} with HNGi7 ID ${this.hng_id} using ${this.language} for stage 2 task.`
+    return `Hello world, this is ${this.firstName} ${this.lastName} with HNGi7 ID ${this.hng_id} using ${this.language} for stage 2 task. ${this.email}`
   }
  }
   console.log(personalInformation.fullHistory());
