87ad1eb2e7ec810e91564a249a1e06366fb7efb5
Bizzdimeji <66257627+Bizzdimeji@users.noreply.github.com>
2020-06-03 15:23:21 +0100

    Bizz.py
    Script created using Python

---
+++ Bizz.py
@@ -0,0 +1,7 @@
+name = "Oladimeji Abiola"
+hng_id = "HNG-04204"
+email = "bizzdimeji@gmail.com"
+language = "Python"
+
+//printing
+print("Hello World, this is " + name + " with HNGi7 ID " + hng_id + " and email " + " using " + language + " for stage 2 task ")
