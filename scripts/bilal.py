297ed8a53184dac9e3a819d9a7586349fc74cfeb
MdBilal420 <mohdbilal16@gmail.com>
2020-06-03 22:21:20 +0530

    Team-Storm first task stage 1

---
+++ scripts/bilal.py
@@ -0,0 +1,8 @@
+def task1():
+    firstname = "Mohammad"
+    lastname = "Bilal"
+    hngid = "HNG-03370"
+    email = "mohdbilal16@gmail.com"
+    language = "Python"
+    print("Hello World, this is " + firstname + " "+ lastname +" with HNGi7 ID " + hngid + " and email " + email + " using " + language + " for stage2 task.")
+task1()
