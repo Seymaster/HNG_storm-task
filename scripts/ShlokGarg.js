ccf99bb6a97e80134571277e41d0cd7f1d576aeb
vngarg <sngargrsd@gmail.com>
2020-06-04 00:20:13 +0530

    Shlok Garg Stage-2 Task

---
+++ scripts/ShlokGarg.js
@@ -0,0 +1,7 @@
+function hng() {
+    var FirstName = 'Shlok' , LastName = 'Garg' , id = 'HNG-01167' , languaga = 'Javascript' , email = 'sngargrsd@gmail.com';
+    var result = 'Hello World, this is ' + FirstName + ' ' + LastName + ' with HNGi7 ID: ' + id + ' using ' + languaga + ' for stage-2 task. ' + email;
+    console.log(result) 
+}
+
+hng();
