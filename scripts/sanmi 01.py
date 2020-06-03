58a97f56ff871acf7ddb854790f75e75515c1ef5
Sanmi01 <66228637+Sanmi01@users.noreply.github.com>
2020-06-03 17:12:46 +0100

    Add files via upload

---
+++ scripts/sanmi 01.py
@@ -0,0 +1,8 @@
+import json
+
+full_name = "Awelewa Oluwasanmi"
+ID = "HNG-03199"
+language = "python"
+email = "supersanmi01@gmail.com"
+output = "Hello World, this is " + full_name + " with HNGi7 ID " + ID + " using " + language + " for stage 2 task. " + email
+print(output)
