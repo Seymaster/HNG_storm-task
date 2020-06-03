aa13d133f60018cd8401565ed6ad74311456af7e
Charles Okafor <54353294+OKAFORCHARLESU@users.noreply.github.com>
2020-06-03 23:06:15 +0100

    Update and rename charlesK.js to CharlesK.js

---
+++ scripts/CharlesK.js
@@ -0,0 +1,9 @@
+var myInfo = {
+    firstName: "Charles",
+    lastName: "Okafor",
+    id: "HNG-02922",
+    email: "okafor.charlesu@gmail.com",
+    language: "JavaScript"
+};
+var myData = `Hello world, this is ${myInfo.firstName} ${myInfo.lastName} with HNGi7 ID ${myInfo.id} using ${myInfo.language} for stage 2 task. ${myInfo.email}`;
+console.log(myData);
