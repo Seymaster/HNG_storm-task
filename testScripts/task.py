8180b8641a7e1297157f43f085c3427cc7395123
Esi-meci <gowinmercy@gmail.com>
2020-06-03 22:08:14 +0100

    Esi_code

--- testScripts/task.py
+++ testScripts/task.py
@@ -1,8 +1,7 @@
 def details(F,L,I,La,E):
-    return ("helloworld, this is ", F, L ,' with HNG ID ',I, ' using ', La, ' for stage 2 task. ', E)
+    return ("helloworld, this is ", F, L ,' with HNG ID ',I, ' using ', La, ' for stage 2 task. gowinmercy@gmail.com')
 
 print(details(F ='[Mercy]',
     L = '[Godwin]',
     I = '[HNG00414]',
-    La = '[python]',
-    E = '[gowinmercy@gmail.com]'))
+    La = '[python]'))
