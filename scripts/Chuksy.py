0ecf9d61ff3499c8d9ad1a89af41728001e82f1d
Your Name <onuhchukwuma12@gmail.com>
2020-06-03 21:44:58 +0100

    My Script

---
+++ scripts/Chuksy.py
@@ -0,0 +1,8 @@
+def my_hngi7_details():
+    firstName = 'Chukwuma'
+    LastName = 'Onuh'
+    print(f'Hello World, this is {firstName} {LastName} with HNGi7 ID HNG-02260 using Python for stage 2 task. onuhchukwuma12@gmail.com')
+    
+my_hngi7_details()
+
+
