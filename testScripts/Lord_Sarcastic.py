ba1c536bf6ad4b0e7f0b6d9fa01e047f8e63e5b6
Lord-sarcastic <adeoti.15.jude@gmail.com>
2020-06-03 21:09:59 +0100

    Done assignment

---
+++ testScripts/Lord_Sarcastic.py
@@ -0,0 +1,15 @@
+def details():
+	data = {
+		"first_name": "Ayodeji",
+		"last_name": "Adeoti",
+		"id": "HNG-04049",
+		"language": "Python",
+		"email": "adeoti.15.jude@gmail.com"
+	}
+
+	text = f"Hello World, this is {data['first_name']} {data['last_name']} with HNGi7 ID {data['id']} using {data['language']} for stage 2 task. {data['email']}"
+	print(text)
+	return text
+
+if __name__ == '__main__':
+	details()
