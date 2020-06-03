62fbffc814da4e7701f31f71c8c9ff960d7c91d7
gurusuccess <31377258+gurusuccess@users.noreply.github.com>
2020-06-03 16:49:30 -0400

    Update ezenwata.php
    Bug : fixed

--- scripts/ezenwata.php
+++ scripts/ezenwata.php
@@ -4,6 +4,6 @@ $lastname = "Destiny";
 $id = "HNG-05063";
 $lang = "PHP";
 $email = "ezenwatairoegbu97@gmail.com";
-	echo "Hello World, this is [$firstName] [$LastName] with HNGi7 ID [$id] using [$lang] for stage 2 task. $email";
+	echo "Hello World, this is $firstName $LastName with HNGi7 ID $id using $lang for stage 2 task. $email";
 	//By ezenwata
 ?>
