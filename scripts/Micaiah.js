577ad28517301c3e55c3b64a79ad20f4d7cf0841
micaiah-effiong <micaiah.effiong@gmail.com>
2020-06-03 09:04:16 +0100

    fix: make statment a single line

--- scripts/Micaiah.js
+++ scripts/Micaiah.js
@@ -1,6 +1,5 @@
 function result() {
-  let expression = "Hello World, this is Micaiah Effiong with HNG-05817 using"+
-  "JavaScript for stage 2 task";
+  let expression = "Hello World, this is Micaiah Effiong with HNG-05817 using JavaScript for stage 2 task";
   console.log(expression);
   return expression;
 }
