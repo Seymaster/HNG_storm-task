c783db2524690af3016a532cff35880cdddffb93
StriderKingg <44429269+StriderKingg@users.noreply.github.com>
2020-06-03 19:00:23 +0300

    My_Script
    Strider's Script

---
+++ scripts/Strider.py
@@ -0,0 +1,10 @@
+#Declaring variables
+
+internName = "Mbora Githua-Maina"
+internID = "HNG-00033"
+internLanguage = "HTML"
+internEmail = "mbora.githua@gmail.com"
+
+print(
+    "Hello World, this is" + " " + internName + " " + "with HNGi7 ID" + " " + internID + " " + "using" + " " + internLanguage + " " + "for stage 2 task," + " " + internEmail
+    )
