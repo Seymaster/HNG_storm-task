938b5724799eda3e020bc3c4226b2dca15f36db5
Beita Zaheed <57130151+zedbeit@users.noreply.github.com>
2020-06-03 17:36:41 +0100

    Add Deehaz.js

---
+++ scripts/Deehaz.js
@@ -0,0 +1,8 @@
+const data = {
+  fullname: "Zaheed Beita",
+  id: "HNG-02613",
+  language: "Javascript",
+  email: "lordzeddy31@gmail.com"
+}
+
+console.log(`Hello World, this is ${data.fullname} with HNGi7 ID ${data.id} using ${data.language} for stage 2 task. ${data.email}`)
