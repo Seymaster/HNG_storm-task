3c2a8d599fde8024d493da1e0a83a6229678caa2
LemmyFee <eniolaelemide@gmail.com>
2020-06-03 19:44:22 +0100

    Upload hngi7 task 2

---
+++ scripts/Eniola_elemide hngi7_task.py
@@ -0,0 +1,30 @@
+
+
+def fun_test():
+    """
+    Returns personal information on hngi7 intern. 
+    """
+
+    # defining user data in a dictionary
+    details = {
+    "firstname":"Eniola",
+    "lastname":"Elemide",
+    "hngi7_id":"HNG-03752",
+    "programming_language":"Python",
+    "mail":"eniolaelemide@gmail.com"
+    }
+
+    #prints data in specified format
+    print("Hello World, this is "+details["firstname"]+
+    " "+ details["lastname"]+" "+
+    "with HNGi7 ID "+details["hngi7_id"]+" and email "+
+    details["mail"]+" using "+
+    details["programming_language"]+" "+"for stage 2 task.")
+
+    return
+
+
+
+
+
+
