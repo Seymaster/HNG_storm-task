f1f945c92aecf4f970bae547befda7c80d9c289c
Mercy Esi <gowinmercy@gmail.com>
2020-06-03 22:52:19 +0100

    Update new.py

--- testScripts/new.py
+++ testScripts/new.py
@@ -1,14 +1,14 @@
-import json
-# a Python object (dict):
-x = {
-  "firstName": "[Mercy]",
-    "lastName": "[Godwin]",
-    "language": "[Python] ",
-    "id": "[HNG-00414]",
-}
+# import json
+# # a Python object (dict):
+# x = {
+#   "firstName": "[Mercy]",
+#     "lastName": "[Godwin]",
+#     "language": "[Python] ",
+#     "id": "[HNG-00414]",
+# }
 
-# convert into JSON:
-y = json.dumps(x)
+# # convert into JSON:
+# y = json.dumps(x)
 
-# the result is a JSON string:
+# # the result is a JSON string:
 print("Hello World, this is [Mercy] [Godwin] with HNGi7 ID [HNG-00414] using [python] for stage 2 task. gowinmercy@gmail.com")
