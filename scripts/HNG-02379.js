decf32b914a5cf7627a0b2126c6e557027dec243
Adeniran02 <66222989+Adeniran02@users.noreply.github.com>
2020-06-03 17:10:14 +0100

    Update HNG-02379.js

--- scripts/HNG-02379.js
+++ scripts/HNG-02379.js
@@ -1,4 +1,4 @@
-const stage2 = (fullName = "Kehinde Adeniran", id = "HNG-02379", language = "Javascript") => {
-  return `Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task`;
+const stage2 = (fullName = "Kehinde Adeniran", id = "HNG-02379", language = "Javascript", emailAddress = "adeniranolanrewajuu@gmail.com") => {
+  return `Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task. ${emailAddress}.`
 };
-stage2();
+console.log(stage2());
