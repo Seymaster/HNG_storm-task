a04e3955b41840fad30ce7f8c8dcc64da583c45d
Hafizah Muhyideen <40233874+justfizah@users.noreply.github.com>
2020-06-03 10:37:18 +0100

    Delete Hafizahmuhyideen.js

--- scripts/Hafizahmuhyideen.js
+++
@@ -1,9 +0,0 @@
-const firstName = 'Hafizah';
-const lastName = 'Muhyideen';
-const userName = 'Hafizaaah';
-const fullName = `${firstName} ${lastName}`;
-const id = 'HNG-02396';
-const language = 'JavaScript';
-const message = `Hello World, this is ${fullName} with ${id} using ${language} to solve stage 2 task.`;
-
-console.log(message);
