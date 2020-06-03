238dea01cc10bfe2438b14d3cb5b71ff95183456
Adeniran02 <66222989+Adeniran02@users.noreply.github.com>
2020-06-03 01:53:43 +0100

    Create HNG-02379.js

---
+++ scripts/HNG-02379.js
@@ -0,0 +1,4 @@
+const stage2 = (fullName = "Kehinde Adeniran", id = "HNG-02379", language = "Javascript") => {
+  return `Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task`;
+};
+stage2();
