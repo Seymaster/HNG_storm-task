db7ea9a148f240ae703f3508dbe1f6500061e5d2
micaiah-effiong <micaiah.effiong@gmail.com>
2020-06-03 08:33:33 +0100

    fix: make ID an int

--- scripts/Micaiah.js
+++ scripts/Micaiah.js
@@ -1,5 +1,5 @@
 function result() {
-  return "Hello World, this is Micaiah Effiong with HNGi7 ID Micaiah using"+
+  return "Hello World, this is Micaiah Effiong with HNGi7 ID 456 using"+
   "JavaScript for stage 2 task";
 }
 
