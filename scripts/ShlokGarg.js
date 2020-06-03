bf10d1b3684eb07fff55aff5de117d7414671ab3
vngarg <sngargrsd@gmail.com>
2020-06-04 02:08:20 +0530

    Added My File

--- scripts/ShlokGarg.js
+++ scripts/ShlokGarg.js
@@ -1,7 +1,4 @@
-function hng() {
-    var FirstName = 'Shlok' , LastName = 'Garg' , id = 'HNG-01167' , languaga = 'Javascript' , email = 'sngargrsd@gmail.com';
-    var result = 'Hello World, this is ' + FirstName + ' ' + LastName + ' with HNGi7 ID: ' + id + ' using ' + languaga + ' for stage-2 task. ' + email;
-    console.log(result) 
-}
+var FirstName = 'Shlok', LastName = 'Garg', id = 'HNG-01167', languaga = 'Javascript', email = 'sngargrsd@gmail.com';
+var result = 'Hello World, this is ' + FirstName + ' ' + LastName + ' with HNGi7 ID ' + id + ' using ' + languaga + ' for stage 2 task. sngargrsd@gmail.com';
 
-hng();
+console.log(result);
