d1aa30e471275162b403d0ca27e45d230e7178f7
Collins Nkemdirim <41852813+kemswizz@users.noreply.github.com>
2020-06-03 18:05:43 +0100

    Update nkemdirim.py

--- scripts/nkemdirim.py
+++ scripts/nkemdirim.py
@@ -4,4 +4,4 @@ email = "collinsnkem8@gmail.com"
 language = "Python"
 
 
-print("Hello World, this is " + name + " with HNGi7 ID " + hng_id + " and email "+ email +" using " + language + " for stage 2 task ")
+print("Hello World, this is " + name + " with HNGi7 ID " + hng_id + " using " + language + " for stage 2 task"+"." + email)
