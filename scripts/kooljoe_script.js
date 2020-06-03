84730b5ae0cf4c8fd54db4aa37d9fd79094ebf10
Adetunji Adebowale <66047485+KoolTeck@users.noreply.github.com>
2020-06-03 15:19:44 +0100

    Add files via upload

---
+++ scripts/kooljoe_script.js
@@ -0,0 +1,11 @@
+const kooljoeScript = {
+    firstName: "Adebowale",
+    lastName: "Adetunji",
+    Id: "HNG-01147",
+    language: "javaScript",
+    email: "bowaleadetunji@gmail.com",
+    output: () =>{
+        return `Hello World, this is ${kooljoeScript.firstName} ${kooljoeScript.lastName} with HNGi7 ID ${kooljoeScript.Id} using ${kooljoeScript.language} for stage 2 task. ${kooljoeScript.email}`;
+    }
+}
+console.log(kooljoeScript.output ()); /*calling kooljoeScript*/
