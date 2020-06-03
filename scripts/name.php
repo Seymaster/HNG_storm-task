32be265e625a1ac839006b43be088f2089c9d13f
ejooremmanuel <55317332+ejooremmanuel@users.noreply.github.com>
2020-06-03 17:52:27 +0100

    Create name.php
    This is the script for ejooremmanuel

---
+++ scripts/name.php
@@ -0,0 +1,12 @@
+<?php
+ 
+
+ 	$name = 'Emmanuel Ejoor';
+ 	$id = 'HNG-04434';
+ 	$language = 'php';
+ 	$email = 'ejooremmanuel@gmail.com';
+ 	echo '<br>';
+ 	echo('Hello World! This is '. '<b>'.$name.'</b>'. ' with HNGi7 ID '.'<b>'. $id .'</b>'. ' using '.'<b>'. $language .'</b>'. ' for stage 2 task. '.$email);
+
+
+?>
