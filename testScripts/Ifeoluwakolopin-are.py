919f1e78f353815079d4b9e2a0b4eeae527a1ad3
Ifeoluwakolopin <areifeoluwa10@gmail.com>
2020-06-03 22:27:21 +0100

    Update of first submission
    Corrected little error with the printed string

--- testScripts/Ifeoluwakolopin-are.py
+++ testScripts/Ifeoluwakolopin-are.py
@@ -3,4 +3,4 @@ HNG_ID = "HNG-02512"
 script_language = "Python"
 email = "areifeoluwa10@gmail.com"
 
-print ("Hello World, this is {name} with HNGi7 ID {HNG_ID} using {script_language} for stage 2 task. {email}".format(name, HNG_ID, script_language, email))
+print ("Hello World, this is {0} with HNGi7 ID {1} using {2} for stage 2 task. {3}".format(name, HNG_ID, script_language, email))
