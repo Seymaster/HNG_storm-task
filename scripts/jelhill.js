d097fd32f0e4dc0bf06e815160efe97054b81d8b
Jelhill <talk2jelili2000@gmail.com>
2020-06-03 17:07:49 +0100

    jelhill HNG Task

---
+++ scripts/jelhill.js
@@ -0,0 +1,12 @@
+let jelhill = {
+    firstname: "Abduljelili",
+    lastname: "Umaru",
+    id: "HNG-00204",
+    language: "Javascript",
+    email: "talk2jelili2000@gmail.com",
+    output: function() {
+        return `Hello World, this is [${this.firstname}] [${this.lastname}] with HNGi7 ID [${this.id}] and email [${this.email}] using [${this.language}] for stage 2 task.`
+    }
+}
+  
+  console.log(jelhill.output())
