500ce2cfba5bed2ae806c1351c733e6827ae9417
Jelili Umaru <talk2jelili2000@gmail.com>
2020-06-03 12:09:25 +0100

    Add files via upload

---
+++ scripts/stack_jelhill.js
@@ -0,0 +1,12 @@
+let jelhill = {
+    name: "Umaru Abduljelili",
+    id: "HNG-00204",
+    language: "Javascript"
+  }
+  
+  
+  function introduction(myObject) {
+    return `Hello World, this is ${myObject.name} with HNGi7 ID ${myObject.id} using ${myObject.language} for stage 2 task`
+  }
+  
+  console.log(introduction(jelhill))
