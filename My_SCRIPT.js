f530b44dceeea7b6a975c6854c870f561f63ddac
Micaiah Effiong <52747707+micaiah-effiong@users.noreply.github.com>
2020-06-03 14:02:04 +0100

    My_SCRIPT

---
+++ My_SCRIPT.js
@@ -0,0 +1,13 @@
+function result() {
+  let expression ={
+    name: "Micaiah Effiong",
+    id: "HNG-05817",
+    email: "micaiah.effiong@gmail.com",
+    language: "JavaScript",
+    output: "Hello World, this is Micaiah Effiong with HNG-05817 using JavaScript for stage 2 task"
+  };
+  console.log(expression);
+  return expression
+}
+
+result();
