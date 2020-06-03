0ef9e5a2b0fd695a399797bbc66bbb07ab1317af
Sanya Oluwadare <sanyaodare@gmail.com>
2020-06-03 20:35:57 +0100

    my task two for team-storm

---
+++ scripts/frontend_sanyaodare.js
@@ -0,0 +1,4 @@
+function output(firstName, lastName, id, language, email) {    
+    return `Hello World, this is ${firstName} ${lastName} with HNGi7 ID ${id} using ${language} for stage 2 task. ${email}`;
+}
+console.log(output("Sanya", "Oluwadare", "HNG-00202", "JavaScript", "sanyaodare@gmail.com",));
