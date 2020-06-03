e805197dcc0e531c55ed6e2f7d936c8902416ecb
StriderKingg <44429269+StriderKingg@users.noreply.github.com>
2020-06-04 00:28:24 +0300

    Update Strider.py
    Updated the language.

--- scripts/Strider.py
+++ scripts/Strider.py
@@ -1,10 +1,10 @@
 #Declaring variables
 
-internName = "Mbora Githua-Maina"
-internID = "HNG-00033"
-internLanguage = "HTML"
+internName = "[Mbora Githua-Maina]"
+internID = "[HNG-00033]"
+internLanguage = "[HTML/CSS]"
 internEmail = "mbora.githua@gmail.com"
 
 print(
-    "Hello World, this is" + " " + internName + " " + "with HNGi7 ID" + " " + internID + " " + "using" + " " + internLanguage + " " + "for stage 2 task," + " " + internEmail
+    "Hello World, this is" + " " + internName + " " + "with HNGi7 ID" + " " + internID + " " + "using" + " " + internLanguage + " " + "for stage 2 task." + " " + internEmail
     )
