a2107a37501a0c2a12cfb3c5cfe75a3f468e9d47
Festus <festusowumi@gmail.com>
2020-06-03 17:41:01 +0100

    @enfinity Task 2

---
+++ testScripts/enfinity.py
@@ -0,0 +1,11 @@
+def info():
+    
+    firstName = "Festus"
+    lastName = "Owumi"
+    hngid = "HNG-04019"
+    language = "Python"
+    email = "festusowumi@gmail.com"
+
+    return f"Hello World, this is {firstName} {lastName} with HNGi7 ID {hngid} using {language} for stage 2 task. {email}"
+
+print(info())
