aea7e0f5bd5d2ec51a034078aabe49a4fc0b15de
Hafizah Muhyideen <40233874+justfizah@users.noreply.github.com>
2020-06-03 18:27:02 +0100

    Update Hafizahmuhyideen.js

--- scripts/Hafizahmuhyideen.js
+++ scripts/Hafizahmuhyideen.js
@@ -3,7 +3,8 @@ const lastName = 'Muhyideen';
 const userName = 'Hafizaaah';
 const fullName = `${firstName} ${lastName}`;
 const id = 'HNG-02396';
+const email ='hafeexahabdul@gmail.com';
 const language = 'JavaScript';
-const message = `Hello World, this is ${fullName} with ${id} using ${language} to solve stage 2 task.`;
+const message = `Hello World, this is ${fullName} with ${id} and ${email} using ${language} to solve stage 2 task.`;
 
 console.log(message);
