79c935cdc1e65e229215d2f49708d809d0d9c368
Adewumi2076 <58106428+Adewumi2076@users.noreply.github.com>
2020-06-03 11:23:44 +0100

    Add files via upload

---
+++ mike.py
@@ -0,0 +1,6 @@
+def details():
+    full_name = "Adewumi Michael";
+    id = 'HNG-02740';
+    language = "Python";
+    print(f"Hello World, this is {full_name} with HNGi7 ID  {id} using {language} for stage 2 task")
+details()
