c12a4b2bab1410939300887332246a418b577496
Steve Waweru <64740444+Waweru-Steve@users.noreply.github.com>
2020-06-03 14:21:00 +0300

    MY-SCRIPT

---
+++ scripts/stevezed.js
@@ -0,0 +1,11 @@
+function printDetails() {
+  const my_id = "HNG-01325"
+  const name = "Steve Waweru"
+  const email = "wawerusteve142@gmail.com" 
+  const language = "Javascript"
+  let result = "Hello World, this is " + name + " with HNGi7 ID " + my_id + " using " + language + " for stage 2."
+
+  console.log(result)
+}
+
+printDetails()
