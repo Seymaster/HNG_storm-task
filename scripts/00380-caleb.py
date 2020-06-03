955e48760cfb4a576ce33abcabb5239e7b9b0ae8
Caleb <agbenigacaleb@gmail.com>
2020-06-04 00:46:32 +0100

    caleb_script.py added

--- scripts/00380-caleb.py
+++
@@ -1,6 +0,0 @@
-firstName = "Caleb"
-lastName = "Agbeniga"
-hngID = "00380"
-email = "agbenigacaleb@gmail.com"
-
-print("Hello World, this is "+ firstName + " " + lastName + " with HNGi7 ID " + hngID + " using python for stage 2 task. " +  email)
