ebcdca11055005920d52a8183dc4c1cd348a3c66
dharnihell <adeyinkadaniel09@gmail.com>
2020-06-03 22:48:09 +0100

    task 1

---
+++ testScripts/dharnzkraft.js
@@ -0,0 +1,10 @@
+
+  const entry = {
+    firstname: "Adeyinka",
+    lastname: "Daniel",
+    language: "Javascript",
+    id: "HNG-01589",
+    email: "adeyinkadaniel09@gmail.com"
+  };
+
+console.log("Hello world, this is " + entry.firstName + " " + entry.lastName + " with HNG7i ID " + entry.id + " using " + entry.language + " for stage 2 task." + " " + entry.email); 
