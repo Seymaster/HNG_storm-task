f6d97e38353452760d58864eb61b8b8a015ad21f
DevYoma <47899828+DevYoma@users.noreply.github.com>
2020-06-03 18:11:48 +0100

    MY_SCRIPT
    MY_SCRIPT

---
+++ scripts/Yoma.js
@@ -0,0 +1,12 @@
+    
+
+function callResult() {
+    const fullName = "Emore Ogheneyoma Lawrence";
+    const hngId = "HNG-01781";  
+    const languageUsed = "Javascript";
+    const email = "Lawrenceyoma@gmail.com"
+
+    console.log(`Hello world, this is ${fullName} with HNGi7 ID ${hngId} using ${languageUsed} for stage 2 task. ${email}`);
+};
+
+callResult();
