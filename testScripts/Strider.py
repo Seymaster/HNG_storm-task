96b19d58c8c24fa24d3c8d32788bbb026c5bbeb3
StriderKingg <44429269+StriderKingg@users.noreply.github.com>
2020-06-04 00:27:36 +0300

    my_script
    Strider's script

---
+++ testScripts/Strider.py
@@ -0,0 +1,8 @@
+internName = "[Mbora Githua-Maina]"
+internID = "[HNG-00033]"
+internLanguage = "[HTML/CSS]"
+internEmail = "mbora.githua@gmail.com"
+
+print(
+    "Hello World, this is" + " " + internName + " " + "with HNGi7 ID" + " " + internID + " " + "using" + " " + internLanguage + " " + "for stage 2 task." + " " + internEmail
+    )
