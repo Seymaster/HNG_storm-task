58d6c8ab8c9cb503637181f921b02193dbee9dd5
micaiah-effiong <micaiah.effiong@gmail.com>
2020-06-03 08:56:12 +0100

    add: console logging

--- scripts/Micaiah.js
+++ scripts/Micaiah.js
@@ -1,6 +1,8 @@
 function result() {
-  return "Hello World, this is Micaiah Effiong with HNG-05817 using"+
+  let expression = "Hello World, this is Micaiah Effiong with HNG-05817 using"+
   "JavaScript for stage 2 task";
+  console.log(expression);
+  return expression;
 }
 
 result();
