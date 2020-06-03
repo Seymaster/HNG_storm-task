eefc951f05d49e2456bd0fa5d80f8fb3fddead8a
Ifeoluwakolopin <areifeoluwa10@gmail.com>
2020-06-03 22:07:26 +0100

    first submission for the hello world task

---
+++ testScripts/Ifeoluwakolopin-are.py
@@ -0,0 +1,6 @@
+name = "Ifeoluwakolopin Are"
+HNG_ID = "HNG-02512"
+script_language = "Python"
+email = "areifeoluwa10@gmail.com"
+
+print ("Hello World, this is {name} with HNGi7 ID {HNG_ID} using {script_language} for stage 2 task. {email}".format(name, HNG_ID, script_language, email))
