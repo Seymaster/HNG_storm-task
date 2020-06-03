be4e88fead658637f9f02d23f9c5c2025fc85db3
Jerry299 <josephjeremiah564@yahoo.com>
2020-06-03 05:30:55 +0100

    added my script

---
+++ scripts/jeremyScript.js
@@ -0,0 +1,11 @@
+const myData = () => {
+  data = {
+    fullName: "Joseph Chinemerem Jeremiah",
+    language: "Javascript",
+    id: "05271",
+  };
+  let result = `Hello World, this is ${data.fullName} with HNGi7 ID ${data.id} using ${data.language} for stage 2 task`;
+  return result;
+};
+
+console.log(myData());
