fa6479f4477cce1d7c5dde428878c8a7c55f6835
Folajomi Shotunde <folajor.mee@gmail.com>
2020-06-03 14:27:08 +0100

    upload my script - Folajomi Shotunde

---
+++ scripts/frontend_folajomi.js
@@ -0,0 +1,6 @@
+const name = "Folajomi Shotunde";
+const id = "HNG-06103"
+
+const output = () => `Hello world, this is ${name} with HNG ID ${id}, using JavaScript for stage 2 task`
+
+console.log(output());
