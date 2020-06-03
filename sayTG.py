515859f26106779bb86bd15c055b0b78bc835124
sayTG <busarithienkie@gmail.com>
2020-06-03 21:40:38 +0100

    sayTG Python file for team task

---
+++ sayTG.py
@@ -0,0 +1,6 @@
+firstName = "ThankGod"
+lastName = "Busari"
+email = "busarithienkie@gmail.com"
+idNumber = "HNG-04616"
+language = "Python"
+print(f"Hello World, this is {firstName} {lastName} with HNGi7 ID {idNumber} using {language} for stage 2 task. {email}")
