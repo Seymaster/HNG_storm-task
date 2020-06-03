3cc9cb08246760b8188784d9c86457eccc06536b
Calvinoea <39657008+Calvinoea@users.noreply.github.com>
2020-06-03 06:33:06 +0100

    Javascript code created for completion of the task

---
+++ ozolua.js
@@ -0,0 +1,12 @@
+
+
+function sentence(fullname,id,language){
+
+  return `Hello World, this is ${fullname} with HNGi7 ID ${id} using ${language} for stage 2 task`
+
+}
+
+
+
+sentence("Calvin Ebun-Amu", 191, "JavaScript")
+
