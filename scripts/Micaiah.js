1fce87caa68af49753ddf10474230b65fe73d7c0
micaiah-effiong <micaiah.effiong@gmail.com>
2020-06-03 08:37:01 +0100

    fix: HNG intenship ID

--- scripts/Micaiah.js
+++ scripts/Micaiah.js
@@ -1,5 +1,5 @@
 function result() {
-  return "Hello World, this is Micaiah Effiong with HNGi7 ID 456 using"+
+  return "Hello World, this is Micaiah Effiong with HNG-05817 using"+
   "JavaScript for stage 2 task";
 }
 
