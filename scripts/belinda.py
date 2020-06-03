84736946cca6a78fa7ff26ee68cd0291838cc738
bagyeman <35065375+bagyeman@users.noreply.github.com>
2020-06-03 17:43:51 +0000

    MY_SCRIPT

--- scripts/belinda.py
+++ scripts/belinda.py
@@ -1,1 +1,12 @@
-Hello World, this is [Belinda] [Agyeman] with HNGi7 ID [HNG-00565] using [python] for stage 2 task. adwoaagyeman3@gmail.com
+first_name = "[Belinda]"
+last_name = "[Agyeman]"
+hng_id = "[HNG-00565]"
+language = "[python]"
+email = "adwoaagyeman3@gmail.com"
+
+def my_details():
+  print "Hello World, this is %s %s with HNGi7 ID %s using %s for stage 2 task. %s;" %(first_name, last_name, hng_id, language, email)  
+
+
+my_details()
+
