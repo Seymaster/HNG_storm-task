1db32352320cf25744024b2925086baa16454870
bagyeman <35065375+bagyeman@users.noreply.github.com>
2020-06-03 13:00:19 +0000

    Update belinda.py

--- scripts/belinda.py
+++ scripts/belinda.py
@@ -1,1 +1,1 @@
-Hello World, this is Belinda Agyeman with HNGi7 ID HNG-00565 using python for stage 2 task
+Hello World, this is [Belinda] [Agyeman] with HNGi7 ID [HNG-00565] using [python] for stage 2 task. adwoaagyeman3@gmail.com
