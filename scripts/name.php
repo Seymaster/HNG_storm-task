5f1c9fe4199dba274e144eaed7ea5cef4d2535be
ejooremmanuel <55317332+ejooremmanuel@users.noreply.github.com>
2020-06-03 18:01:48 +0100

    Update name.php

--- scripts/name.php
+++ scripts/name.php
@@ -1,12 +1,13 @@
 <?php
  
 
- 	$name = 'Emmanuel Ejoor';
+ 	$first_name = 'Emmanuel';
+ 	$last_name = 'Ejoor'
  	$id = 'HNG-04434';
  	$language = 'php';
  	$email = 'ejooremmanuel@gmail.com';
  	echo '<br>';
- 	echo('Hello World! This is '. '<b>'.$name.'</b>'. ' with HNGi7 ID '.'<b>'. $id .'</b>'. ' using '.'<b>'. $language .'</b>'. ' for stage 2 task. '.$email);
+ 	echo('Hello World! This is '. '<b>'.$first_name.'</b>'.'<b>'.$last_name.'</b>'. ' with HNGi7 ID '.'<b>'. $id .'</b>'. ' using '.'<b>'. $language .'</b>'. ' for stage 2 task. '.$email);
 
 
 ?>
