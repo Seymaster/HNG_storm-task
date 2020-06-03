9917f815d599c54f529cab3557ad140f8b6785e5
Sanya Oluwadare <sanyaodare@gmail.com>
2020-06-03 20:56:32 +0100

    Delete frontend_sanyaodare.js

--- scripts/frontend_sanyaodare.js
+++
@@ -1,4 +0,0 @@
-function output(firstName, lastName, id, language, email) {    
-    return `Hello World, this is ${firstName} ${lastName} with HNGi7 ID ${id} using ${language} for stage 2 task. ${email}`;
-}
-console.log(output("Sanya", "Oluwadare", "HNG-00202", "JavaScript", "sanyaodare@gmail.com",));
