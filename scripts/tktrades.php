6c1a5f0581674d6dda981c8fd2d8c275c0bde8e1
tktrades <39212335+tktrades@users.noreply.github.com>
2020-06-03 16:38:01 +0800

    tktrades php file

---
+++ scripts/tktrades.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>hello world</title>
+</head>
+<body>
+
+
+<?php
+ $myvar1 = "Hello world,";
+ $myvar2 = " this is";
+ $fullname =" Koyi olamilekan";
+ $HNGi7id = " With HNGi7 ID 04682.";
+  $language = " Using php";
+  echo "$myvar1". $myvar2 . $fullname . $HNGi7id . $language;
+
+
+?>
+</body>
+
+
+
+
+</html>
