1345192f17d5d9b8c0e4608b59e4bb86f565b7b6
Caleb <agbenigacaleb@gmail.com>
2020-06-03 15:50:08 +0100

    my hng7i task one for team storm

---
+++ scripts/00380-caleb.py
@@ -0,0 +1,6 @@
+firstName = "Caleb"
+lastName = "Agbeniga"
+hngID = "00380"
+email = "agbenigacaleb@gmail.com"
+
+print("Hello World, this is "+ firstName + " " + lastName + " with HNGi7 ID " + hngID + " using python for stage 2 task. " +  email)
