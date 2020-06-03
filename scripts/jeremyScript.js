2dbb8fee7c2c364eeacd5be330e1ea0145085791
Jerry299 <josephjeremiah564@yahoo.com>
2020-06-03 05:33:11 +0100

    added script

--- scripts/jeremyScript.js
+++ scripts/jeremyScript.js
@@ -7,5 +7,3 @@ const myData = () => {
   let result = `Hello World, this is ${data.fullName} with HNGi7 ID ${data.id} using ${data.language} for stage 2 task`;
   return result;
 };
-
-console.log(myData());
