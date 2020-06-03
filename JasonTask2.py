d6b4c9334a5d02dcce38b0c7e41559d75517ebf5
bobofaji <48582312+bobofaji@users.noreply.github.com>
2020-06-03 23:17:19 +0100

    Stage 1 task

---
+++ JasonTask2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+script = print("Hello World, this is Jason with HNGi7 03765 using python for stage 2 task.")
+
+def output():
+    return script
+
+output()
+
+
+# In[ ]:
+
+
+
+
