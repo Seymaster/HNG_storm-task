e40a4b8b919d3e535f1c094d8f637f711a086e90
Teslim Hassan <64636170+HassanTeslim007@users.noreply.github.com>
2020-06-03 22:21:48 +0100

    Delete MyScript.js to Change the Name

--- scripts/MyScript.js
+++
@@ -1,8 +0,0 @@
-var alpha = {
-	firstName: "Hassan",
-	lastName: "Teslim",
-	email: "hassanteslim12@gmail.com",
-	language: "JavaScript",
-	id: "HNG-00265",
-};
-document.write("Hello world, this is " + alpha.firstName + " " + alpha.lastName + " with HNG7i ID " + alpha.id + " using " + alpha.language + " for Stage 2 task." + alpha.email); 
