5e1d173ab85ee2c254d7fa27138165c74c18dc06
Karen Ngugi <31765863+KarenNgugi@users.noreply.github.com>
2020-06-03 12:44:29 +0300

    MY_SCRIPT

---
+++ scripts/KNgugi.py
@@ -0,0 +1,5 @@
+name = "Karen Ngugi"
+ID = 'HNG-03330'
+language = "Python"
+
+print("Hello World, this is {} with HNGi7 ID {} using language {} for Stage 2 Task.".format(name, ID, language), flush = True)
