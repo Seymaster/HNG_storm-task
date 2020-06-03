98ed8d3ae67b552f8e36c6e18c821cc92e68737a
Nicholas Karimi <30544615+nicholas-karimi@users.noreply.github.com>
2020-06-04 00:10:58 +0300

    Update nicholas-karimi.py
    fix failing test errors

--- scripts/nicholas-karimi.py
+++ scripts/nicholas-karimi.py
@@ -4,9 +4,9 @@
 """A python script that prints my name and
     HNGi7 internship ID."""
 
-def get_intern_details(firstName, lastName, intern_id,language, email):
-
-    return "Hello world, this is {} {}, with HNGi7 ID {} and using {} for stage two task.{} ".format(firstName, lastName, intern_id, language, email)
+def get_intern_details(firstName, lastName, lang, id):
 
 
-print(get_intern_details("Nicholas", "Karimi", "HNG-04333", "Python", "nicholaskarimi.dev@gmail.com"))
+    print("Hello World, this is {} {} with HNGi7 ID {} using {} for stage 2 task. nicholaskarimi.dev@gmail.com".format(firstName, lastName, id, lang))
+
+get_intern_details("[Nicholas]", "[Karimi]", "[Python]", "[HNG-04333]")
