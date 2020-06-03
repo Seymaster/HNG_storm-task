94497f2c39f6fb4359051cbaee2e6485e5385432
Archie <oluwatstar21@gmail.com>
2020-06-03 20:06:31 +0100

    My Task for stage 2

---
+++ scripts/oluwatobi-ogunwole.js
@@ -0,0 +1,9 @@
+const me = {
+    Name: 'Ogunwole Oluwatobi',
+    hngid: 'HNG-05517',
+    Language: 'javascript',
+    email: 'oluwatstar21@gmail.com'
+}
+
+let output = 'Hello World, this is '+me.Name+' with HNGi7 ID '+me.hngid+' using '+me.Language+' for stage 2 task. '+me.email
+console.log(output)
