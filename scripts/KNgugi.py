36f17b3b1f80c3dc1a2501703e7c5ef3c8602448
Karen Ngugi <31765863+KarenNgugi@users.noreply.github.com>
2020-06-03 17:41:54 +0300

    Update KNgugi.py

--- scripts/KNgugi.py
+++ scripts/KNgugi.py
@@ -1,6 +1,6 @@
 name = "Karen Ngugi"
-ID = 'HNG-03330'
+intern_id = 'HNG-03330'
 language = "Python"
 email = "karenwngugi12@gmail.com" 
 
-print("Hello World, this is {} with HNGi7 ID {} and email {} using language {} for Stage 2 Task.".format(name, ID, email, language), flush = True)
+print("Hello World, this is {} with HNGi7 ID {} and email {} using language {} for Stage 2 Task.".format(name, intern_id, email, language), flush = True)
