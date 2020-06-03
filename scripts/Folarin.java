a007218f819f0a57d0179c81a7c98c2959ec4fa4
Folarin-Isaac <61974682+Folarin-Isaac@users.noreply.github.com>
2020-06-03 18:58:22 +0100

    Create Folarin.java

---
+++ scripts/Folarin.java
@@ -0,0 +1,5 @@
+public class Folarin {
+   public static void main(String[] args) {
+       System.out.println("Hello World, this is Folarin Isaac with HNGi7 ID HNG-00927 using Java for stage 2 task. oluwaphemmyisaac@gmail.com")
+   }
+}
