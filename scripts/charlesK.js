235029594d5a87612b8be8e82e29296ab6ea211e
Charles Okafor <54353294+OKAFORCHARLESU@users.noreply.github.com>
2020-06-03 21:18:09 +0100

    Add files via upload

---
+++ scripts/charlesK.js
@@ -0,0 +1,13 @@
+
+
+var charlesK_info = {
+    file: "charlesK.js",
+    output: `Hello world, this is Charles Okafor with HNGi7 ID HNG-02922 using Javascript for stage 2 task`,
+     name: "Charles Okafor",
+     id: "HNG-02922",
+     email: "okafor.charlesu@gmail.com",
+     language: "Javascript",
+     status: ""
+}
+var myData = JSON.stringify(charlesK_info);
+console.log(myData);
