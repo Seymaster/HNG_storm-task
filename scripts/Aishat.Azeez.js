de950ce43714b416be5977610e648cac4555865d
Aishat oyindamola Azeez <48646341+ayeesha17@users.noreply.github.com>
2020-06-03 07:26:15 -0700

    Create Aishat.Azeez.js

---
+++ scripts/Aishat.Azeez.js
@@ -0,0 +1,9 @@
+//My INFO
+const firstName = "Aishat";
+const lastName = "Azeez";
+const ID = "HNG-01898";
+const email = "oyindamolaaesha@gmail.com";
+const language = "JavaScript";
+//MY OUTPUT
+const Output = `Hello World, this is ${firstName} ${lastName} with HNGi7 ${ID} and ${email} using ${language} for stage 2 task`;
+console.log(Output);
