bee9ae1478f8acaa832fdc6e1fe5ddad67c67b71
mizzleinetimi <49090824+mizzleinetimi@users.noreply.github.com>
2020-06-04 06:24:55 +1400

    MY_SCRIPT
    MY_SCRIPT

---
+++ scripts/mizzle.py
@@ -0,0 +1,8 @@
+name = 'inetimi omu'
+hng_id = 'HNG-01685'
+lang = 'python'
+email = 'inetimimizzle@gmail.com'
+
+def stage2():
+    print(f'Hello World,this is {name} with HNGi7 ID {hng_id} using {lang} for stage 2 task. {email}')
+stage2()
