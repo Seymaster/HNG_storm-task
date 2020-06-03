d3e1119958f2abe6cb8443f65cabcf01b6cc4745
Charmaine <66219459+charmaine-hng@users.noreply.github.com>
2020-06-03 15:32:46 +0100

    Add files via upload

---
+++ stack_Charmaine.py
@@ -0,0 +1,8 @@
+# This progeam is to display the expected output for Stage 2 Task
+First_name = "Charmaine"
+Last_name = "Eweka"
+HNGi7_ID = "HNG-03028"
+language = "Python"
+Email = "Charmaineeweka562@gmail.com"
+print (f"Hello World, This is {First_name} {Last_name} with HNGi7 ID {HNGi7_ID} using {language} for Stage 2 Task.")
+print (f"{Email}")
