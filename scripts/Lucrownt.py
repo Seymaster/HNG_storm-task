cb20bd153e192f8e224d195b5df477f90e0c96c6
Lucrownt <61760028+Lucrownt@users.noreply.github.com>
2020-06-03 14:38:53 +0100

    Add files via upload
    Python looks awesome.

---
+++ scripts/Lucrownt.py
@@ -0,0 +1,7 @@
+first_name = "Lukman"
+last_name = "Adeoti"
+internship_id = "HNG-01721"
+email_address = "lukmanademolaadeoti@gmail.com"
+language = "python"
+
+print (first_name + " " +last_name + ", " + internship_id+", " + email_address+ ", "+ language)
