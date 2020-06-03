6b2034a48caec18001787922b116b844081eb080
CHrisEl-coder <66277593+CHrisEl-coder@users.noreply.github.com>
2020-06-03 13:53:14 -0700

    Update Edwinchristian.py

--- Edwinchristian.py
+++ Edwinchristian.py
@@ -2,8 +2,9 @@ uni = "Hello world"
 name  = "Thomson Christian"
 _ID = "HNG-02906"
 language = "python"
+email = "Edwinchristian97'gmail.com"
 def script():
-    ID = print("{} this is {} with HNGi7 ID {} using {} for stage 2 task".format(uni, name, _ID, language))
+    ID = print("{} this is {} with HNGi7 ID {} using {} for stage 2 task, {}".format(uni, name, _ID, language, email))
     return ID
 
 
