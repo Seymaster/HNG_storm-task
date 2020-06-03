611337f54d2990080ae693d77e00590662e030f6
Esi-meci <gowinmercy@gmail.com>
2020-06-03 19:15:37 +0100

    ESI_task

---
+++ scripts/task.py
@@ -0,0 +1,5 @@
+import json
+A = ['Mercy','Godwin','PYTHON', HNG00414 ,"gowinmercy@gmail.com"]
+
+print('Hello World, this is ', [A[0]] [A[1]],' with HNGi7 ID ', [A[3]], ' using ', [A[2]], ' for stage 2 task. ',[a[4]])
+
