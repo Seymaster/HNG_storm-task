a45bd1609d5769ef33361f4cfb052d8ec83073cf
believemanasseh <danasmufasa@gmail.com>
2020-06-03 16:52:30 +0100

    python script for manasseh

---
+++ scripts/manasseh.py
@@ -0,0 +1,12 @@
+# HNGi7 Task One:
+
+def Output():
+	first_name = "Believe"
+	last_name = "Amadi"
+	hng_id = "HNG-01841"
+	language = "Python"
+	email = "believemanasseh@gmail.com"
+	
+	return f"Hello World, this is {first_name} {last_name} with HNGi7 ID {hng_id} using {language} for stage 2 task. {email}"
+
+print(Output())
