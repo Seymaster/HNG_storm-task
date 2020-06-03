d98341e0127566c3b437614c241422ed55713731
Yahaya <yasholma1@gmail.com>
2020-06-03 18:55:31 +0100

    Add yasholma.js to testScripts.js

---
+++ scripts/yasholma.js
@@ -0,0 +1,13 @@
+const userInfo = {
+    firstName: "Yahaya",
+    lastName: "Yohanna",
+    id: "HNG-04535",
+    email: "yahayahyohanna@gmail.com",
+    language: "Javascript",
+    status: "",
+};
+
+const output = ({ firstName, lastName, id, language, email }) =>
+    `Hello World, this is [${firstName}] [${lastName}] with HNGi7 ID [${id}] using [${language}] for stage 2 task. ${email}`;
+
+console.log(output(userInfo));
