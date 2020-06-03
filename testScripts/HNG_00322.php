b685be9e2f55f86e5b0f973eace16cf7e01dd1f5
Onome <onomeofogba13@gmail.com>
2020-06-03 20:41:50 +0100

    adding HNG_00322.php file

---
+++ testScripts/HNG_00322.php
@@ -0,0 +1,9 @@
+<?php
+$Name = "Ofogba Faith Onome";
+$ID = "HNG-00322";
+$Email = "onomeofogba13@gmail.com";
+
+echo "Hello World, this is " .$Name. " with HNGi7 ID " .$ID. " using PHP for stage 2 task. ";
+echo $Email;
+
+  ?>
