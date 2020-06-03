504f62c9cac9e1b3dc59ee4841f8ade9e0230732
CHrisEl-coder <66277593+CHrisEl-coder@users.noreply.github.com>
2020-06-03 14:44:57 -0700

    Add files via upload

---
+++ scripts/Edwinchristian.py
@@ -0,0 +1,12 @@
+uni = "Hello world"
+name  = "Thomson Christian"
+_ID = "HNG-02906"
+language = "python"
+def script():
+    ID = print("{} this is {} with HNGi7 ID {} using {} for stage 2 task".format(uni, name, _ID, language))
+    return ID
+
+
+
+
+script()
