2cb78584227991fbe2576bac64bbd763a306db5e
Mercy Esi <gowinmercy@gmail.com>
2020-06-04 00:18:39 +0100

    Delete task.py

--- scripts/task.py
+++
@@ -1,8 +0,0 @@
-def details(F,L,I,La,E):
-    print("helloworld, this is ", F, L ,' with HNG ID ',I, ' using ', La, ' for stage 2 task. ', E)
-
-details(F ='[Mercy]',
-    L = '[Godwin]',
-    I = '[HNG00414]',
-    La = '[python]',
-    E = '[gowinmercy@gmail.com]')
