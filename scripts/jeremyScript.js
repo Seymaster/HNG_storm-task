5210128738ba36da53f000b430b7ba2c9e4acfef
Jerry299 <josephjeremiah564@yahoo.com>
2020-06-03 21:32:24 +0100

    Revert "jeremyScript modified to team-storm standard"
    This reverts commit a489fcbfab3cd98c31208f0f07feb70bc8e24110.

--- scripts/jeremyScript.js
+++ scripts/jeremyScript.js
@@ -1,9 +1,8 @@
 const myData = () => {
   data = {
-    firstName: "Joseph",
-    lastName: "Jeremiah",
+    fullName: "Joseph Chinemerem Jeremiah",
     language: "Javascript",
-    id: "HNG-05271",
+    id: "05271",
   };
   let result = `Hello World, this is ${data.fullName} with HNGi7 ID ${data.id} using ${data.language} for stage 2 task`;
   return result;
