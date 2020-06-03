668c62873b24e4a7b5e71149b73d8abede3eff2f
Hassan Teslim <hassanteslim12@gmail.com>
2020-06-03 16:22:11 +0100

    My Hngi7 task 2

---
+++ scripts/MyScript.js
@@ -0,0 +1,8 @@
+var alpha = {
+	firstName: "Hassan",
+	lastName: " Teslim",
+	email: "hassanteslim12@gmail.com",
+	language: "JavaScript",
+	id: "HNG-00265",
+};
+document.write("this is " + alpha.firstName  + alpha.lastName + " with email " + alpha.email + " and HNG7i ID " + alpha.id + " using " + alpha.language + " for Stage 2 task"); 
