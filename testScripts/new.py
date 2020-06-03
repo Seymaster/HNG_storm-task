574a119ac24bbec5a4afa97fb6184198f29a2625
Esi-meci <gowinmercy@gmail.com>
2020-06-03 22:16:02 +0100

    Esi_code

---
+++ testScripts/new.py
@@ -0,0 +1,7 @@
+def details(F,L,I,La,E):
+    return ("helloworld, this is ", F, L ,' with HNGi7 ID ',I, ' using ', La, ' for stage 2 task. gowinmercy@gmail.com')
+
+print(details(F ='[Mercy]',
+    L = '[Godwin]',
+    I = '[HNG-00414]',
+    La = '[python]'))
