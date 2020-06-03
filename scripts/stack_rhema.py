aca723350dc97c943278c65c8860d52114f6a2fd
Ikechi Rhema <31439315+lemuelzy@users.noreply.github.com>
2020-06-03 21:22:19 +0100

    my_py_script
    MY_SCRIPT

---
+++ scripts/stack_rhema.py
@@ -0,0 +1,5 @@
+uni = 'World'
+name = "Akachi Ikechi"
+identification = "HNG-05659"  
+program="python"
+print(f'Hello {uni}, this is {name} with HNGi7 ID {identification}, using {program} for stage 2 task')
