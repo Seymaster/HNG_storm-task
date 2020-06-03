a489fcbfab3cd98c31208f0f07feb70bc8e24110
Jerry299 <josephjeremiah564@yahoo.com>
2020-06-03 21:29:32 +0100

    jeremyScript modified to team-storm standard

--- scripts/jeremyScript.js
+++ scripts/jeremyScript.js
@@ -1,8 +1,9 @@
 const myData = () => {
   data = {
-    fullName: "Joseph Chinemerem Jeremiah",
+    firstName: "Joseph",
+    lastName: "Jeremiah",
     language: "Javascript",
-    id: "05271",
+    id: "HNG-05271",
   };
   let result = `Hello World, this is ${data.fullName} with HNGi7 ID ${data.id} using ${data.language} for stage 2 task`;
   return result;
