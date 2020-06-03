fd2b0a0e61cf786063a87f64c55d836edfc92aeb
skynette <cutejosh2@gmail.com>
2020-06-03 16:54:40 +0100

    Joshua Hassan
    This is my solution script for the HNGi7 task 2

---
+++ scripts/Cutejosh.py
@@ -0,0 +1,13 @@
+"""
+	My script to solve HNG task 2
+"""
+
+def task():
+	name = "Joshua Hassan"
+	HNG_ID = "HNG-00533"
+	email = "cutejosh2@gmail.com"
+	language = "Python"
+	# using string concatenation
+	print("Hello World, this is "+name+" with HNGi7 ID "+HNG_ID+" using "+language+" for stage 2 task. "+email)
+
+task()
