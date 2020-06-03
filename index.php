cd3341d88e4707d8a5e7bd9ab7520bb2c652edef
rowend36 <rowendduke36@gmail.com>
2020-06-03 08:56:29 +0100

    fix: working version with python php and javascript support

--- index.php
+++ index.php
@@ -1,37 +1,63 @@
 <?php
-// $file = 'echo.php';
-
-// if (file_exists($file)) {
-//     $file = dirname(__DIR__) . '/test/' . $file;
-//     shell_exec("node " . $file);
-// }
-
-// $output = shell_exec('ls');
-
-// $output = explode(" ", $output);
-
-// var_dump($output);
-// $file = dirname(__DIR__) . '/test/' . $file;
-
-// include 'script.js';
-
-// error_reporting(E_ALL);
-// ini_set('display_errors', 1);
-
-// $read = exec("node script.js");
-
-// echo $read;
-$log_directory = dirname(__FILE__);
-$files = [];
-
-foreach (glob($log_directory . '/*.*') as $file) {
-    $files[] = $file;
+$html = <<<HTML
+<!doctype html>
+<html>
+<head>
+<title>Task 2</title>
+</head>
+<body>
+<h5>Team Storm</h5>
+<ul>
+HTML;
+$results = array();
+$languages = array("py"=>"python","js"=>"node","php"=>"php -f");
+$users = array();
+define("PASS_TEXT", 
+	"/^Hello\s+World,\s+this\s+is\s+((\w+\s+)+)with\s+HNGi7\s+ID\s+(\d+)\s+using\s+([^\s]+)\s+for\s+stage\s+2\s+task$/");
+$files = scandir("./scripts");
+foreach($files as $i){
+	if($i == "." || $i=="..")continue;
+	$file = file_get_contents("./scripts/".$i);
+	$ext = pathinfo("./scripts/".$i, PATHINFO_EXTENSION);
+	if(array_key_exists($ext, $languages)){
+		$value = shell_exec($languages[$ext]." ./scripts/".$i);
+	}
+	else{
+		$value = "Error";
+	}
+	$user = array();
+	$score = preg_match(PASS_TEXT,$value,$user);
+	$results[$i] = $score?"pass":"fail";
+	if($score){
+		$id = $user[3];
+		while(array_key_exists($id,$users))$id=$id."~";
+		$name = $user[1];
+		$lang = $user[4];
+		$users[$id]=array("name"=>$name,"language"=>$lang);
+	}
 }
-
-foreach ($files as $file) {
-    $ext = pathinfo($file, PATHINFO_EXTENSION);
-    if ($ext === 'js') {
-        $read = exec("node {$file}");
-        echo $read . "\n";
-    }
+if (array_key_exists("json",$_GET)){
+	$myJSON = json_encode($results);
+	header('Content-Type: application/json');
+	echo $myJSON;
+	flush();
 }
+else if (array_key_exists("users",$_GET)){
+	$myJSON = json_encode($users);
+	header('Content-Type: applicatio    n/json');
+	echo $myJSON;
+	flush();
+}
+else{
+	echo $html;
+	foreach($results as $name=>$score){
+	if($score=="pass"){
+		echo "<li>".$name." : PASS</li>";
+	}
+	else{
+		echo "<li>".$name." :FAIL</li>";
+	}
+	}
+	echo "</ul></body></html>";
+}
+?>
---
+++ scripts/Ebenezar_Owologba.py
@@ -0,0 +1,5 @@
+my_id = "50000"
+my_name = "Owologba Ebenezar"
+my_language = "Python"
+
+print("Hello World, this is %s with HNGi7 ID %s using %s for stage 2 task"%(my_name,my_id,my_language);
