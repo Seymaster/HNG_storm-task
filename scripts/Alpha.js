8b2be76e54bdc36dd93a76879b89f6e4b987f042
Teslim Hassan <64636170+HassanTeslim007@users.noreply.github.com>
2020-06-03 22:26:58 +0100

    Alpha.js
    Uploaded Alpha.js

---
+++ scripts/Alpha.js
@@ -0,0 +1,8 @@
+var alpha = {
+	firstName: "Hassan",
+	lastName: "Teslim",
+	email: "hassanteslim12@gmail.com",
+	language: "JavaScript",
+	id: "HNG-00265",
+};
+console.log("Hello world, this is " + alpha.firstName + " " + alpha.lastName + " with HNG7i ID " + alpha.id + " using " + alpha.language + " for stage 2 task." + " " + alpha.email); 
