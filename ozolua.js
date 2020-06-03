403783b759be62577ef2d589e1ef25c49e3c9347
Calvinoea <39657008+Calvinoea@users.noreply.github.com>
2020-06-03 12:58:44 +0100

    Update ozolua.js

--- ozolua.js
+++ ozolua.js
@@ -1,12 +1,12 @@
 
 
-function sentence(fullname,id,language){
+function sentence(fullname,id,language,email){
 
-  return `Hello World, this is ${fullname} with HNGi7 ID ${id} using ${language} for stage 2 task`
+  return `Hello World, this is ${fullname} with HNGi7 ID ${id} using ${language} for stage 2 task ${email}`
 
 }
 
 
 
-sentence("Calvin Ebun-Amu", 191, "JavaScript")
+sentence("Calvin Ebun-Amu", 191, "JavaScript", "calvinebunamu@gmail.com")
 
