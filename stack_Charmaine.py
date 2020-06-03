2d5a1a521503c9e7e28f8144a424e897b0905ad9
Charmaine <66219459+charmaine-hng@users.noreply.github.com>
2020-06-03 17:32:13 +0100

    Update stack_Charmaine.py

--- stack_Charmaine.py
+++ stack_Charmaine.py
@@ -1,7 +1,6 @@
-play the expected output for Stage 2 Task
+# play the expected output for Stage 2 Task
 First_name = "Charmaine"
 Last_name = "Eweka"
 HNGi7_ID = "HNG-03028"
 language = "Python"
-Email = "Charmaineeweka562@gmail.com"
 print (f"Hello World, This is {First_name} {Last_name} with HNGi7 ID {HNGi7_ID} using {language} for Stage 2 Task. Charmaineeweka562@gmail.com")
