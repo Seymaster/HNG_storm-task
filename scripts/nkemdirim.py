5be0e315672fc5f3855ef21017849058e67dca2f
Collins Nkemdirim <41852813+kemswizz@users.noreply.github.com>
2020-06-03 12:24:05 +0100

    My own python file

---
+++ scripts/nkemdirim.py
@@ -0,0 +1,7 @@
+name = "Nkemdirim Collins"
+hng_id = "HNG-02026"
+email = "collinsnkem8@gmail.com"
+language = "Python"
+
+
+print("Hello World, this is " + name + " with HNGi7 ID " + hng_id + " and email "+ email +" using " + language + " for stage 2 task ")
