3f417eb345eea437dc91408a1495613205c02e83
Mercy Esi <gowinmercy@gmail.com>
2020-06-03 22:38:03 +0100

    Update task.py

--- testScripts/task.py
+++ testScripts/task.py
@@ -1,5 +1,5 @@
 def details(F,L,I,La):
-    return ("hello world, this is ", F, L ,' with HNG ID ',I, ' using ', La, ' for stage 2 task. gowinmercy@gmail.com')
+    return ("Hello World, this is ", F, L ,' with HNGi7 ID ',I, ' using ', La, ' for stage 2 task. gowinmercy@gmail.com')
 
 print(details(F ='[Mercy]',
     L = '[Godwin]',
