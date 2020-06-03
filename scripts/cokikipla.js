31d187e77af5bf2c9df6cffc4b2c3da0bc0c344b
Collins Kiplagat <32549887+cokikip@users.noreply.github.com>
2020-06-03 17:46:31 +0300

    Update cokikipla.js

--- scripts/cokikipla.js
+++ scripts/cokikipla.js
@@ -1,1 +1,6 @@
+var name="Collins Kiplagat Kiprono";
+var hngi7ID="HNG-02589";
+var language="PHP,Javascript";
+var email="cokikipla@gmail.com"
 
+console.log("Hello World, this is "+name+" with HNGi7 ID "+hngi7ID+" using "+language+ " for stage 2 task. "+email );
