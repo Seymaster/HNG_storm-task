346bc38d97e1380adad53120db3124b0721ad75f
Joshua Folorunsho <joshuafolorunsho01@gmail.com>
2020-06-03 23:31:43 +0100

    refactor code

--- testScripts/esi.py
+++ testScripts/esi.py
@@ -7,17 +7,8 @@
 #     "id": "[HNG-00414]",
 # }
 
-<<<<<<< HEAD
-print(details(F ='[Mercy]',
-    L = '[Godwin]',
-    I = '[HNG-00414]',
-    La = '[python]'))
-
-print('Hello World, this is Godwin Mercy with HNGi7 ID HNG-00141 using python for stage 2 task. ','gowinmercy@gmail.com')
-=======
 # # convert into JSON:
 # y = json.dumps(x)
 
 # # the result is a JSON string:
 print("Hello World, this is [Mercy] [Godwin] with HNGi7 ID [HNG-00414] using [python] for stage 2 task. gowinmercy@gmail.com")
->>>>>>> f1f945c92aecf4f970bae547befda7c80d9c289c
