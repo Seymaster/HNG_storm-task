01b5acbd2928a23b236159309909347773bda70e
roscom09 <36547371+roscom09@users.noreply.github.com>
2020-06-03 20:34:15 +0100

    Add files via upload

---
+++ scripts/RasaqAwofeso.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script> /* HNG Internship
+            My First JavaScript Task */
+    </script>
+     <meta charset="utf-8">
+    <title>HNG INTERNSHIP</title>
+</head>
+
+<body>
+    <h1></h1>
+   <p>
+       <script> 
+           alert("HELLO WORLD, this is RASAQ OLUWATOBI AWOFESO with HNG-03740 using JavaScript for stage 2. Awofesorasaq@gmail.com");
+       </script>
+   
+    
+</body>
+
+</html>
