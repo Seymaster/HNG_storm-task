c94aa847d109ffb5ba9449f01ece675d4434ad4a
Qay <qay@kali.qay.com>
2020-06-03 21:09:13 +0100

    Added a new script

---
+++ scripts/Qay.py
@@ -0,0 +1,19 @@
+import json
+
+
+user = {
+    "name": "Keren-happuch Achoja",
+    "id": "HNG-01748",
+    "language": "Python",
+    "email": "gkay260@gmail.com"
+}
+
+
+def identity():
+    print("Hello World, this is " + user['name'] +  " with HNGi7 ID " + user['id'] + " using " + user['language'] + " for stage 2 task")
+    return
+
+json_data = json.dumps(user)
+
+identity()
+
