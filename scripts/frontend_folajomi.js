3e63cf45f89f4ff130bca8c71aa646af1c8dddaf
Folajomi Shotunde <folajor.mee@gmail.com>
2020-06-03 14:36:29 +0100

    Upload my script - Folajomi Shotunde

--- scripts/frontend_folajomi.js
+++ scripts/frontend_folajomi.js
@@ -1,7 +1,10 @@
-const name = "Folajomi Shotunde";
-const email = "folajor.mee@gmail.com"
-const id = "HNG-06103"
+const data = {
+    name: "Folajomi Shotunde",
+    email: "folajor.mee@gmail.com",
+    id: "HNG-06103",
+    language: "JavaScript"
+}
 
-const output = () => `Hello world, this is ${name} with HNG ID ${id}, and email ${email} using JavaScript for stage 2 task`
+const output = () => `Hello world, this is ${data.name} with HNG ID ${data.id}, and email ${data.email} using ${data.language} for stage 2 task`
 
 console.log(output());
