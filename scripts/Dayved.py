edb0d46c734e993510561d5c4e1898a3e34e5c68
David Brown <66221305+David-Brown01@users.noreply.github.com>
2020-06-03 11:31:27 +0100

    Create Dayved.py

---
+++ scripts/Dayved.py
@@ -0,0 +1,10 @@
+name = "Brown David"
+
+id = "HNG-03338"
+
+language = "Python"
+
+email = "dayvedbrown257@gmail.com"
+
+print(f'Hello world this is {name} with HNGi7ID {id}
+ using {language} for stage 2 task')
