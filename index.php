1313d56cc4d157d56cdb20d6e7b544aeceaab312
yanmifeakeju <yanmifeakeju@gmail.com>
2020-06-03 16:07:36 +0100

    styled

--- index.php
+++ index.php
@@ -19,12 +19,10 @@ function getScripts($files, $folder)
         // var_dump($ext);
         if (array_key_exists($ext, $extensions)) {
             $scripts[] = ['name' => "$folder/" . $file, 'command' => $extensions[$ext], 'filename' => $file];
-
         }
     }
 
     return $scripts;
-
 };
 
 $scripts = getScripts($files, $folder);
@@ -38,7 +36,6 @@ foreach ($scripts as $key => $script) {
         $read = exec("{$scripts[$key]['command']} {$scripts[$key]['name']}");
         $content[] = ['output' => $read, 'filename' => $scripts[$key]['name']];
     }
-
 }
 
 $members = [];
@@ -64,7 +61,13 @@ foreach ($content as $key => $data) {
 
                 $data = preg_replace('/\]/i', '', $data);
 
-                $messages[] = ['id' => $match['id'], 'message' => $data, 'pass' => true, 'filename' => $filename];
+                $fullname = $match['first'] . ' ' . $match['last'];
+
+                $fullname = preg_replace('/\[/i', '', $fullname);
+
+                $fullname = preg_replace('/\]/i', '', $fullname);
+
+                $messages[] = ['id' => $match['id'], 'message' => $data, 'name' => $fullname, 'pass' => true, 'filename' => $filename];
 
                 $members[] = ['id' => $match['id'], 'firstname' => $match['first'], 'lastname' => $match['last'], 'email' => $email, 'language' => $match['language'], 'filename' => $filename, 'output' => $data];
             }
@@ -72,15 +75,12 @@ foreach ($content as $key => $data) {
             $userMessage = str_replace($email, '', $output);
             $userMessage = preg_replace('/\[/', '', $userMessage);
             $userMessage = preg_replace('/\]/', '', $userMessage);
-            $messages[] = ['id' => 'Poorly Formated File', 'message' => $userMessage, 'pass' => false, "filename" => "Incorrect response from script {$filename}"];
-
+            $messages[] = ['id' => 'Poorly Formated File', 'message' => $userMessage, 'pass' => false, "filename" => $filename];
         }
-
     } else {
         $failed = "You did not provide a valid email address. Your String must return an email";
         $messages[] = ['id' => 'No Email Returned', 'message' => $failed, 'pass' => false, 'filename' => $filename];
     }
-
 }
 
 if ($_SERVER['QUERY_STRING'] === 'json') {
@@ -91,8 +91,8 @@ if ($_SERVER['QUERY_STRING'] === 'json') {
 }
 
 $total = count($members);
-echo $totalPassed;
-echo $totalScripts;
+// echo $totalPassed;
+// echo $totalScripts;
 
 // var_dump($message);
 
@@ -102,22 +102,127 @@ echo $totalScripts;
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Team Storm</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Team storm</title>
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet" />
+    <style>
+        table,
+        .container {
+            margin: 20px auto;
+            max-width: 1000px;
+        }
+
+        .stat {
+            color: white;
+        }
+
+        .team-name {
+            font-size: 1.6rem;
+            margin-bottom: 10px;
+        }
+
+        .leaders {
+            display: flex;
+            justify-content: space-between;
+        }
+
+        .leaders li a {
+            text-decoration: underline;
+        }
+
+        body {
+            font-weight: bold;
+            padding: 1em;
+        }
+
+        .stat {
+            display: flex;
+            justify-content: space-between;
+        }
+
+        thead {
+            color: #fff;
+        }
+
+        tbody {
+            text-align: center;
+            color: #fff;
+        }
+
+        table {
+            margin-top: 100px;
+        }
+    </style>
 </head>
 
 <body>
-    <h1>Team Storm</h1>
-    <?php foreach ($messages as $output): ?>
-    <?php if ($output['pass'] === true): ?>
-   <p><?=($output['message']) . "Passed"?></?>
-    <?php elseif ($output['pass'] === false): ?>
-        <p><?=$output['message'] . " Failed"?> </p?>
-    <?php endif;?>
+    <header>
+        <div class="container">
+            <div class="team-name">TEAM STORM</div>
+            <ul class="leaders">
+                <li>Leads:</li>
+                <li>
+                    <a href="https://www.github.com/Seymaster">ESI - Backend</a>
+                </li>
+                <li>
+                    <a href="https://www.github.com/joshuafolorunsho">__Josh__ - Frontend</a>
+                </li>
+                <li>
+                    <a href="https://www.github.com/">Aj - devOps</a>
+                </li>
+            </ul>
+        </div>
+    </header>
+
+    <div>
+        <div class="container">
+            <div class="stat">
+                <p class="bg-green-500 px-2 py-3">Total Submission: <?=$totalScripts?></p>
+                <p class="bg-green-500 px-2 py-3">Passed: <?=$totalPassed?></p>
+                <p class="bg-red-500 px-2 py-3">Failed: <?=$totalScripts - $totalPassed?></p>
+            </div>
+        </div>
+    </div>
+
+    <table>
+        <thead class="bg-blue-700">
+            <tr>
+                <th class="w-1/6 px-4 py-2">ID</th>
+                <th class="w-1/6 px-4 py-2">Author</th>
+                <th class="w-1/2 px-4 py-2">Message</th>
+                <th class="w-1/6 px-4 py-2">Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <!-- use bg-green-500 class for passed -->
+            <?php foreach ($messages as $output): ?>
+
+                <?php if ($output['pass'] === true): ?>
+
+                    <tr class="bg-green-500">
+                        <td class="border px-4 py-2"><?=$output['id']?></td>
+                        <td class="border px-4 py-2"><?=$output['name']?></td>
+                        <td class="border px-4 py-2"><?=$output['message']?></td>
+                        <td class="border px-4 py-2">Passed</td>
+                    </tr>
+                <?php elseif ($output['pass'] === false): ?>
+                    <tr class="bg-red-500">
+                        <td class="border px-4 py-2"><?=$output['id']?></td>
+                        <td class="border px-4 py-2"><?="Incorrect formatting from " . $output['filename']?></td>
+                        <td class="border px-4 py-2"><?=$output['message']?></td>
+                        <td class="border px-4 py-2">Failed</td>
+                    </tr>
+
+                <?php endif;?>
+
+
+
+                <!-- use bg-red-500 class for passed -->
+
+        </tbody>
     <?php endforeach;?>
-
-
+    </table>
 </body>
 
 </html>
