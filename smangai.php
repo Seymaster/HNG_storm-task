3d50c6f71fee2f1a87d79d3e5eca2045f9da0ed0
Stephen Mangai <sm120@students.kiron.ngo>
2020-06-04 21:31:02 +0100

    chore: create script

---
+++ smangai.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+function hngTask() {
+  echo "Hello World, this is [Stephen] [Mangai] with HNGi7 ID [HNG-05025] using [PHP] for stage 2 task.  sm120@students.kiron.ngo";
+}
+
+hngTask();
+
+?>
+
+</body>
+</html>
