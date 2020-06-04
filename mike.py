05463a881d4eb0447e34c1908f74b2601cf36e66
Adewumi2076 <58106428+Adewumi2076@users.noreply.github.com>
2020-06-04 02:00:11 +0100

    Update mike.py

--- mike.py
+++ mike.py
@@ -1,7 +1,8 @@
 def details():
-    full_name = "[Adewumi Michael]"
-    id = '[HNG-02740]'
-    email= "[adewumimichael2076@gmail.com]"
+    first_name = "[Adewumi]"
+    last_name = "[Michael]"
+    id = "[HNG-02740]"
+    email= "adewumimichael2076@gmail.com"
     language = "[Python]"
-    print(f"Hello World, this is {full_name} with HNGi7 ID  {id} and email {email} using {language} for stage 2 task")
+    print(f"Hello World, this is {first_name} {last_name} with HNGi7 ID {id} using {language} for stage 2 task.  {email}")
 details()
