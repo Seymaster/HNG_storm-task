4add2b72a569a94bb81e62a13b2e3d23750c92df
David <dav4thevid@gmail.com>
2020-06-04 01:35:29 +0100

    chore: create script

---
+++ testScripts/__davoucii__.js
@@ -0,0 +1,13 @@
+const FIRST_NAME = '[David]';
+const LAST_NAME = '[Fabusuyi]';
+const ID = '[03227]';
+const LANGUAGE = '[Javascript]';
+const EMAIL = 'dav4thevid@gmail.com'
+
+function callMyName(first_name,last_name,id, language, email) {
+    console.log(`Hello World, this is ${first_name} ${last_name}with HNGi7 ID ${id} using ${language} for stage 2 task as its output. ${email}`) 
+    
+    
+}
+
+callMyName(FIRST_NAME,LAST_NAME, ID, LANGUAGE, EMAIL);
