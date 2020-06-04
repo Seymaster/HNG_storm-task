a915d1c59231ae8c408025dade12525bb1fcd550
Akanimo <akanimoime2016@gmail.com>
2020-06-04 13:15:03 +0100

    Update devRex.js

--- testScripts/devRex.js
+++ testScripts/devRex.js
@@ -1,5 +1,5 @@
 const declare = () =>{
- return `Hello World, this is [Akanimo] [Akpan] with HNG7 ID [HNG-02228] using [javascript] for stage 2 task. akanimoime2016@gmail.com`;
+ console.log(`Hello World, this is [Akanimo] [Akpan] with HNG7 ID [HNG-02228] using [javascript] for stage 2 task. akanimoime2016@gmail.com`);
 }
 
 declare()
