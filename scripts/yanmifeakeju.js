8d5bb2d253b4853f9c7432a2aa06da42556e2dce
Mercy Esi <51007408+Seymaster@users.noreply.github.com>
2020-06-04 18:19:49 +0100

    Commited by script owner

--- scripts/yanmifeakeju.js
+++ scripts/yanmifeakeju.js
@@ -5,8 +5,7 @@ function output() {
     language: "JS",
     id: "HNG-00197",
   };
-  str =
-    "Hello World, this is [data.firstName] [data.lastName] with HNGi7 ID [data.id] using [data.language] for stage 2 task. yanmifeakeju@gmail.com";
+  str = `Hello World, this is [${data.firstName}] [${data.lastName}] with HNGi7 ID [${data.id}] using [${data.language}] for stage 2 task. yanmifeakeju@gmail.com`;
 
   return str;
 }
