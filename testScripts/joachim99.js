008774a44d9f758daa8ea00dc8a58d50e67ec325
Joachim Junior <joachimjunior5@gmail.com>
2020-06-04 12:02:04 +0100

    joachim99 file submitted

---
+++ testScripts/joachim99.js
@@ -0,0 +1,8 @@
+const userData = {
+ firstName: 'Joachim',
+ lastName: 'Junior',
+ language: 'javascript',
+ id: 'HNG-04353',
+ email: 'joachimjunior5@gmail.com'
+};
+console.log('Hello World, this is [' + userData.firstName + '] [' + userData.lastName + '] with HNGi7 ID [' + userData.id + '] using [' + userData.language + '] for stage 2 task. ' + userData.email);
