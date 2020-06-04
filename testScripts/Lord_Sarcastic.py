9dc1e13448e81b1811f8b8db2eb60c6ea76add4a
Lord-sarcastic <adeoti.15.jude@gmail.com>
2020-06-04 11:11:31 +0100

    chore: create script

--- testScripts/Lord_Sarcastic.py
+++ testScripts/Lord_Sarcastic.py
@@ -7,7 +7,7 @@ def details():
 		"email": "adeoti.15.jude@gmail.com"
 	}
 
-	text = f"Hello World, this is {data['first_name']} {data['last_name']} with HNGi7 ID {data['id']} using {data['language']} for stage 2 task. {data['email']}"
+	text = f"Hello World, this is [{data['first_name']}] [{data['last_name']}] with HNGi7 ID [{data['id']}] using [{data['language']}] for stage 2 task. {data['email']}"
 	print(text)
 	return text
 
