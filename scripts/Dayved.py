8232b58d0bf00db6d1cbdfbbe91536522400479b
David Brown <66221305+David-Brown01@users.noreply.github.com>
2020-06-04 19:32:45 +0100

    Update Dayved.py

--- scripts/Dayved.py
+++ scripts/Dayved.py
@@ -5,10 +5,10 @@ language =()
 
 def  my_introduction(firstname, lastname,id, language):
 	
-	firstname = 'David'
-	lastname = 'Brown'
-	id = 'HNG-03338'
-	language = 'python'
-	print(f'Hello world, this is (firstname) (lastname) with HNGi7ID (id) using (language) for stage 2 task. dayvedbrown257@gmail.com')
+	firstname = '[David]'
+	lastname = '[Brown]'
+	id = '[HNG-03338]'
+	language = '[python]'
+	print(f'Hello world, this is {firstname} {lastname} with HNGi7ID {id} using {language} for stage 2 task. dayvedbrown257@gmail.com')
 #Now call your fxn
 my_introduction(firstname, lastname, id, language) 
