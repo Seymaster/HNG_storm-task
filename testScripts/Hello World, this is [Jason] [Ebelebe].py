3a75de4040378bbe761a4d0ba10f92f6a1befb5b
bobofaji <48582312+bobofaji@users.noreply.github.com>
2020-06-04 22:33:52 +0100

    Add files via upload

---
+++ testScripts/Hello World, this is [Jason] [Ebelebe].py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+script = print("Hello World, this is [Jason] [Ebelebe] with HNGi7 [HNG-03765] using [PHP] for stage 2 task. ebelebejasone@gmail.com")
+
+def output():
+    return script
+
+output()
+
+
+# In[ ]:
+
+
+
+
