ec03658a5a61c77cfcf971960418ea1d4542548a
dacruz007 <chukslacafe@yahoo.com>
2020-06-04 02:25:08 +0100

    chore: create script

--- scripts/dacruz.py
+++
@@ -1,9 +0,0 @@
-def record():
-    fname = 'Chijioke'
-    lname = 'Ikechukwu'
-    lang = 'PYTHON'
-    id = 'HNG-05191'
-    email = 'chukslacafe@yahoo.com'
-    print(f'Hello World, this is [{lname}] [{fname}] with HNGi7 ID [{id}] using [{lang}] for stage 2 task. {email}')
-
-record()
---
+++ testScripts/dacruz.py
@@ -0,0 +1,9 @@
+def record(d,a,c,r,u,z=None):
+    print(f'Hello World, this is [{d}] [{a}] with HNGi7 ID [{r}] using [{c}] for stage 2 task. {u}')
+
+d = 'Chijioke'
+a = 'Ikechukwu'
+c = 'PYTHON'
+r = 'HNG-05191'
+u = 'chukslacafe@yahoo.com'
+record(d,a,c,r,u)
