b0f0e2417cea306362948004950075714bbc650f
Chukwuamaka <osujichukwuamaka@gmail.com>
2020-06-04 15:13:36 +0100

    chore: create scripts

--- testScripts/ikechukwu.js
+++ testScripts/ikechukwu.js
@@ -1,7 +1,8 @@
 var info = {
-        firstName: "Ikechukwu",
-        lastName: "Madubuike",
-        HNGi7ID: "HNG-00589",
-        language: "javascript", 
-    }
-console.log(`Hello World, this is [${info.firstName}] [${info.lastName}] with HNGi7ID [${info.HNGi7ID}] using [${info.language}] for stage 2 task. ikechukwujames902@gmail.com`);
+    firstName: "Ikechukwu",
+    lastName: "Madubuike",
+    HNGi7ID: "HNG-00589",
+    language: "javascript", 
+}
+
+console.log(`Hello World, this is [${info.firstName}] [${info.lastName}] with HNGi7 ID [${info.HNGi7ID}] using [${info.language}] for stage 2 task. ikechukwujames902@gmail.com`);
