5fd9d3f14785a71a00589309a8bd91862aefabb1
dacruz007 <chukslacafe@yahoo.com>
2020-06-04 08:35:28 +0100

    changed the f'' string to format string for stability.

--- testScripts/dacruz.py
+++ testScripts/dacruz.py
@@ -1,5 +1,5 @@
 def record(d,a,c,r,u,z=None):
-    print(f'Hello World, this is [{d}] [{a}] with HNGi7 ID [{r}] using [{c}] for stage 2 task. {u}')
+    print('Hello World, this is [{0}] [{1}] with HNGi7 ID [{3}] using [{2}] for stage 2 task. {4}'.format(d,a,c,r,u,z))
 
 d = 'Chijioke'
 a = 'Ikechukwu'
