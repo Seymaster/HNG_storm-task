9ff79b65068d71c6576bdcf0b97cdc3c74f5d48d
David <dav4thevid@gmail.com>
2020-06-04 01:42:33 +0100

    enhan:added spacing

--- testScripts/__davoucii__.js
+++ testScripts/__davoucii__.js
@@ -5,7 +5,7 @@ const LANGUAGE = '[Javascript]';
 const EMAIL = 'dav4thevid@gmail.com'
 
 function callMyName(first_name,last_name,id, language, email) {
-    console.log(`Hello World, this is ${first_name} ${last_name}with HNGi7 ID ${id} using ${language} for stage 2 task as its output. ${email}`) 
+    console.log(`Hello World, this is ${first_name} ${last_name} with HNGi7 ID ${id} using ${language} for stage 2 task as its output. ${email}`) 
     
     
 }
