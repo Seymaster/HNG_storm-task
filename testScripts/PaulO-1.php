2905a78791af19bbfbdca8eda5ad48b42226e3f3
PaulO-1 <66218789+PaulO-1@users.noreply.github.com>
2020-06-04 14:02:26 +0100

    PaulO-1
    Work done

--- testScripts/PaulO-1.php
+++ testScripts/PaulO-1.php
@@ -4,13 +4,13 @@ $firstname = "Paul";
 
 $lastname = "Okwudike";
 
-$HNG-0000 = "HNG-U014K7F166P";
+$Tag = "HNG-U014K7F166P";
 
-$PHP = "PHP";
+$Programme = "PHP";
 
 $email = "paulamaechiokwudike@gmail.com"
 
-echo "Hello World, this is [firstname] [lastname] with HNGi7 ID [HNG-0000] using [PHP] for stage 2 task. [email]"
+echo "Hello World, this is [firstname] [lastname] with HNGi7 ID [Tag] using [Programme] for stage 2 task. [email]"
 
 
 ?>
