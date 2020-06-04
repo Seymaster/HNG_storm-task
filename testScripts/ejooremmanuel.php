4826d3046218fee4d6c7626777306e443e034464
ejooremmanuel <55317332+ejooremmanuel@users.noreply.github.com>
2020-06-04 21:45:02 +0100

    chore: create script

---
+++ testScripts/ejooremmanuel.php
@@ -0,0 +1,8 @@
+<?php
+
+
+print('Hello World, this is [Emmanuel] [Ejoor] with HNGi7 ID [HNG-04434] using [php] for stage 2 task. ejooremmanuel@gmail.com')
+
+
+
+?>
---
+++ testScripts/ejooremmanuel.py
@@ -0,0 +1,16 @@
+#import json
+
+# a Python object (dict):
+# #x = {
+#   "firstName": "Emmanuel",
+#     "lastName": "Ejoor",
+#     "language": "PHP",
+#     "id": "HNG-04434",
+# }
+
+# convert into JSON:
+#y = json.dumps(x)
+
+# the result is a JSON string:
+#print("Hello World, this is [Emmanuel] [Ejoor] with HNGi7 ID [HNG-04434] using [php] for stage 2 task. ejooremmanuel@gmail.com")
+print('Hello World, this is [Emmanuel] [Ejoor] with HNGi7 ID [HNG-04434] using [php] for stage 2 task. ejooremmanuel@gmail.com')
