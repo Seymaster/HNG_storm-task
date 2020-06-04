ccfe36bbbbdc6f71c9f2687920449c7780de32c6
David <davidrums65@gmail.com>
2020-06-04 10:35:30 +0100

    committed

---
+++ testScripts/davetech.js
@@ -0,0 +1,5 @@
+const davetech = (fname, lname, id, lang, email) => {
+    console.log(`Hello World, this is [${fname}] [${lname}] with HNGi7 ID [${id}] using [${lang}] for stage 2 task. ${email}`);
+}
+
+davetech('Ajayi', 'David', 'HNG-01671', 'Javascript', 'david.ajayi.anu@gmail.com');
