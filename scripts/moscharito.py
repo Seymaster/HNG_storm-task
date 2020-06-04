e1562ed371e77ea3e389d6003611b8a8e9899866
Moshood Salaudeen <moscharito@gmail.com>
2020-06-04 13:16:26 +0100

    Update moscharito.py

--- scripts/moscharito.py
+++ scripts/moscharito.py
@@ -1,9 +1,9 @@
-def task2():
+def HNGtask2():
 
   firstname = "Moshood"
   lastname = "Salaudeen"
   hngid = "HNG-01122"
   email = "bolaji.moscharito@gmail.com"
   language = "python"
-  print("Hello World, this is", firstname, "", lastname, "with HNGi7 ID HNG-01122", "using", language, "for stage 2 task.", email)
-task2()
+  print("Hello World, this is", firstname, lastname, "with HNGi7 ID HNG-01122", "using", language, "for stage 2 task.", email)
+HNGtask2()
