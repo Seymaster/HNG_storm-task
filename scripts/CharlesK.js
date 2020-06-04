1b4c057d8e53a895cfc5a666ff181d357b6b570e
Charles Okafor <54353294+OKAFORCHARLESU@users.noreply.github.com>
2020-06-04 13:03:20 +0100

    Update CharlesK.js

--- scripts/CharlesK.js
+++ scripts/CharlesK.js
@@ -1,9 +1,9 @@
 var myInfo = {
-    firstName: "Charles",
-    lastName: "Okafor",
+    fName: "Charles",
+    lName: "Okafor",
     id: "HNG-02922",
     email: "okafor.charlesu@gmail.com",
     language: "JavaScript"
 };
-var myData = `Hello world, this is ${myInfo.firstName} ${myInfo.lastName} with HNGi7 ID ${myInfo.id} using ${myInfo.language} for stage 2 task. ${myInfo.email}`;
+var myData = `Hello World, this is [${myInfo.fName}] [${myInfo.lName}] with HNGi7 ID [${myInfo.id}] using [${myInfo.language}] for stage 2 task. ${myInfo.email}`;
 console.log(myData);
