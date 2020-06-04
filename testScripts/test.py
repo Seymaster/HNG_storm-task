32828685a075ad9e27480425e622616fcc6cd18d
NonsoEz <itsnonso97@yahoo.com>
2020-06-04 20:37:26 +0100

    chore: create script

---
+++ testScripts/test.py
@@ -0,0 +1,6 @@
+def myFunc():
+    return "Hello World, this is [Nonso] [Ezechukwu] with HNGi7 ID [HNG-01324] using [Python] for stage 2 task. ezechukwu.nonso@yahoo.com"
+
+OUTPUT = myFunc()
+
+print(OUTPUT)
