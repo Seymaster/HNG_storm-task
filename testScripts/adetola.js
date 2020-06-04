f551af3d03e3deeff5e8663a21f8fe71585b83b3
deytola <adebolaadetola65@gmail.com>
2020-06-04 08:56:44 +0100

    chore: create script

---
+++ testScripts/adetola.js
@@ -0,0 +1,9 @@
+const secondTask = () => {
+    const firstName = 'Adetola';
+    const lastName = 'Adebola';
+    const language = 'javascript';
+    const id = 'HNG-00148';
+    const email = 'adebolaadetola65@gmail.com'
+    return `Hello World, this is [${firstName}] [${lastName}] with HNGi7 ID [${id}] using [${language}] for stage 2 task. ${email}`
+}
+console.log(secondTask());
