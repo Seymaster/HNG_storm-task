adc6a2e7615007167e6aec8affdedd67cb358535
Brian Orina <brayohorina@gmail.com>
2020-06-04 14:23:15 +0300

    chore: create script

---
+++ testScripts/Brian_orinah.py
@@ -0,0 +1,8 @@
+firstname= "Brian"
+lastname = "Orina"
+language = "Python"
+email ="brayohorina@gmail.com"
+
+
+print(f"Hello World, [{firstname}][{lastname}] with HNG-0000 using [{language}] for stage 2 task.{email}")
+
