7d2e8dd8beaa76c99590cc33aa5767b3d1137bde
gabaggrey <gabaggrey@gmail.com>
2020-06-04 22:46:56 +0100

    task submission

--- GabrielAggrey.php
+++ GabrielAggrey.php
@@ -8,6 +8,6 @@ $myEmail = "gabaggrey@gmail.com";
 $languageOfChoice = "PHP";
 
 
-echo "Hello World, this is " . $firstName . " " . $lastName . " with HNGi7 ID " . $internID . " using " . $languageOfChoice . " for stage 2 task. " . $myEmail ;
+echo "Hello World, this is [{$firstName}] [{$lastName}] with HNGi7 ID [{$internID}] using [{$languageOfChoice}] for stage 2 task. {$myEmail}";
 
 ?>
