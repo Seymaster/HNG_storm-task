5f6fb00a5a7dcb04528ac49e157c5388d6e678d5
Joseph Morenike <josephmorenike2@gamil.com>
2020-06-04 09:32:39 +0100

    this is my script

---
+++ scripts/foureyes.js
@@ -0,0 +1,9 @@
+var foureyes = {
+	firstName: "[Joseph]",
+	lastName: "[Morenike]",
+	email: "[josephmorenike2@gmail.com]",
+	language: "[JavaScript]",
+	id: "[HNG-03821]",
+};
+console.log("Hello World,this is "  + foureyes.firstName +" "+ foureyes.lastName+" with HNGi7 ID " + foureyes.id + " using " + foureyes.language +" for stage 2 task."+ " "
++foureyes.email);
