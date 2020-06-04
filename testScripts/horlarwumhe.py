87daa989aca2982ece57e4d2d079038615f7d7f4
Horlarwumhe <amachiever4real@gmail.com>
2020-06-04 17:27:41 +0100

    fixed python version issue

--- testScripts/horlarwumhe.py
+++ testScripts/horlarwumhe.py
@@ -2,5 +2,5 @@ def task():
 	firstname = "Akanji"
 	lastname = "Olawumi"
 	mail= "amachiever4real@gmail.com"
-	print(f"Hello World, this is [{firstname}] [{lastname}] with HNGi7 ID [HNG-03537] using [Python] for stage 2 task. {mail}")
+	print(f"Hello World, this is [%s] [%s] with HNGi7 ID [HNG-03537] using [Python] for stage 2 task. %s"%(firstname,lastname,mail))
 task()
