cc13ece596f914083c4614d0b6fe245b54dbb5d6
Festus <festusowumi@gmail.com>
2020-06-04 18:05:33 +0100

    chore: create script

---
+++ testScript/enfinity.py
@@ -0,0 +1,5 @@
+def info(firstName,lastName,hngid,language,email):
+    
+    print("Hello World, this is {} {} with HNGi7 ID {} using {} for stage 2 task. {}".format(firstName,lastName,hngid,language,email))
+
+info("[Festus]","[Owumi]","[HNG-04019]","[Python]","festusowumi@gmail.com")
