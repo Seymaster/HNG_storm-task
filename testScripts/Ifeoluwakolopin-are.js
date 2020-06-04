6eaf6880883ef1879093f031f4dbda1cec8c6439
Ifeoluwakolopin <areifeoluwa10@gmail.com>
2020-06-04 15:40:31 +0100

    Update and rename to a js file
    I have tried testing with python in different formats but to no avail. Decided to write in JavaScript

---
+++ testScripts/Ifeoluwakolopin-are.js
@@ -0,0 +1,7 @@
+var firstName = "Ifeoluwakolopin";
+var lastName = "Are";
+var HNG_ID = [HNG-02512];
+var language = "JavaScript";
+var email = "areifeoluwa10@gmail.com";
+
+console.log("Hello World, this is [$(firstName)] [$(lastName)] with HNGi7 ID [$(HNG_ID)] using [$(language)] for stage 2 task. areifeoluwa10@gmail.com");
--- testScripts/Ifeoluwakolopin-are.py
+++
@@ -1,7 +0,0 @@
-first_name = ["Ifeoluwakolopin"]
-last_name = ["Are"]
-HNG_ID = ["HNG-02512"]
-language = ["Python"]
-email = "areifeoluwa10@gmail.com"
-
-print("Hello World, this is {0} {1} with HNGi7 ID {2} using {3} for stage 2 task. {4}".format(first_name, last_name, HNG_ID, language, email))
