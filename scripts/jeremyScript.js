304f1cbc4f3656705f7a03f74e29f62af17a05d9
Jerry299 <josephjeremiah564@yahoo.com>
2020-06-04 20:06:59 +0100

    jeremy Script

--- scripts/jeremyScript.js
+++ scripts/jeremyScript.js
@@ -4,7 +4,8 @@ const myData = () => {
     lastName: "Jeremiah",
     language: "Javascript",
     id: "HNG-05271",
+    email: "josephjeremiah564@yahoo.com",
   };
-  let result = `Hello World, this is ${data.firstName} ${data.lastName} with HNGi7 ID ${data.id} using ${data.language} for stage 2 task`;
+  let result = `Hello World, this is [${data.firstName}] [${data.lastName}] with HNGi7 ID [${data.id}] using [${data.language}] for stage 2 task`;
   return result;
 };
