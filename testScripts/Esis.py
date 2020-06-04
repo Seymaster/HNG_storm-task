bcb6d5514be771d196683292f2e006e243c6ef79
Mercy Esi <gowinmercy@gmail.com>
2020-06-04 14:06:48 +0100

    Update Esis.py

--- testScripts/Esis.py
+++ testScripts/Esis.py
@@ -1,12 +1,8 @@
-def details(F,L,I,La,E):
+def details(F,L,I,La,E = 'gowinmercy@gmail.com'):
     print("Hello World, this is",F,L,'with HNGi7 ID',I,'using', La,'for stage 2 task.',E)
 
 details(F ='[Mercy]',
     L = '[Godwin]',
     I = '[HNG-00414]',
     La = '[python]',
-    E = 'gowinmercy@gmail.com')
-
-# A = "Hello World, this is [Godwin] [Mercy] with HNGi7 ID [HNG-00414] using [python] for stage 2 task. " +"gowinmercy@gmail.com"
-# print(A)
-
+    )
