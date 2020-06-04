0c2dc2c03c9825b1825ea600fe56dad180d935d2
Isolokwu Patrick C <46923554+FunnyDigital@users.noreply.github.com>
2020-06-03 19:18:37 -1100

    updated artemiscode

--- scripts/artemiscode.js.js
+++ scripts/artemiscode.js.js
@@ -1,13 +1,16 @@
-//here i declear variable in a function called myDetails
-var myDetails = {
-    file_name: 'artemiscode.js',
-    my_Name: 'isolokwu partrick',
-    HNG_ID: 'HNG-02529',
-    Email: 'roxxleo49@gmail.com',
-    Environment: 'JavaScript',
-    status: '',
-    output: "Hello world , this is Isolokwu patrick C with HNGi7 ID HNG-02529, using JavaScript for stage 2 task. roxxleo49@gmail.com"
-}
-var data = JSON.stringify(myDetails);
-//print the data goten 
-console.log(data);
+//my script
+const userInfo = { firstName: "patrick",
+ lastName: "isolokwu",
+  id: "HNG-02529",
+   email: "roxxleo49@gmail.com",
+   language: "Javascript", };
+   
+const output =
+ ({ firstName,
+     lastName, 
+     id,
+      language,
+       email }) =>
+    `Hello World, this is [${firstName}] [${lastName}] with HNGi7 ID [${id}] using [${language}] for stage 2 task. ${email}`;
+
+console.log(output(userInfo));
