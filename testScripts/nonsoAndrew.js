2b898e839aa3f7248ed36d421beede1386b46522
Nonso Andrew <nonsoandrew1@gmail.com>
2020-06-04 17:06:11 +0100

    chore: create script

---
+++ testScripts/nonsoAndrew.js
@@ -0,0 +1,29 @@
+// const data = {
+//     firstName : "Nonso",
+//     lastName : "Ugbodu",
+//     id : "HNG-03332",
+//     language : "javascript",
+//     email : "nonsoandrew1@gmail.com"
+// }
+// var readMe = ({
+//         firstName,
+//         lastName,
+//         language,
+//         id,
+//         email
+//     }) =>
+//     `Hello World, this is [${firstName}] [${lastName}] with HNGi7 ID [${id}] using [${language}] for stage 2 task. ${email}`;
+
+
+// console.log(readMe(data));
+
+var firstName = "Nonso";
+var lastName = "Ugbodu";
+var id = "HNG-03332";
+var language = "javascript";
+var email = "nonsoandrew1@gmail.com";
+
+console.log(`Hello World, this is [${firstName}] [${lastName}] with HNGi7 ID [${id}] using [${language}] for stage 2 task. ${email}`)
+
+
+
