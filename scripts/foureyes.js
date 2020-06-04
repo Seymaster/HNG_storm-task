4e892aa369cee0c339b59355c546792d3f66147f
lightupthe-world <64196488+lightupthe-world@users.noreply.github.com>
2020-06-04 10:48:21 +0100

    corrected foureyes.js

--- scripts/foureyes.js
+++ scripts/foureyes.js
@@ -1,13 +1,8 @@
 var foureyes = {
-	firstName: "Joseph",
-	lastName: "Morenike",
 	firstName: "[Joseph]",
 	lastName: "[Morenike]",
 	email: "josephmorenike2@gmail.com",
-	language: "Javascript",
-	id: "HNG-03821",
 	language: "[Javascript]",
 	id: "[HNG-03821]",
 };
 console.log("Hello world, this is " + foureyes.firstName + " " + foureyes.lastName + " with HNG7i ID " + foureyes.id + " using " + foureyes.language + " for stage 2 task." + " " + foureyes.email); 
-console.log("Hello world, this is " + foureyes.firstName + " " + foureyes.lastName + " with HNGi7 ID " + foureyes.id + " using " + foureyes.language + " for stage 2 task." + " " + foureyes.email); 
