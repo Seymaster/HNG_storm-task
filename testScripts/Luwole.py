ba2b772d65934d8cd0d0471f5d32e8e18e180d33
GIDEON PETERS <gideonpeters85@gmail.com>
2020-06-04 03:03:45 +0100

    added my script, no errors

---
+++ testScripts/Luwole.py
@@ -0,0 +1,14 @@
+
+details = {
+    'firstname': "Oluwole",
+    'lastname': "Adetifa",
+    'id': "HNG-00048",
+    'email': "adetifaoluwole@gmail.com",
+    'language': "Python", }
+
+output = print(
+    f"Hello World, this is [{details['firstname']}] [{details['lastname']}] with HNGi7 ID [{details['id']}] using [{details['language']}] for stage 2 task. {details['email']}")
+
+
+def result():
+    return output
