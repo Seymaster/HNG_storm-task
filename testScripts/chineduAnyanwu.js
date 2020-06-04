85683a04e4361661211115cc5d2f7bb42d0d9fe5
Chukwuamaka <osujichukwuamaka@gmail.com>
2020-06-04 14:36:39 +0100

    chore: create script

---
+++ testScripts/chineduAnyanwu.js
@@ -0,0 +1,7 @@
+var fullName = "[CHINEDU]";
+var lastName = "[ANYANWU]"
+const HNGi7ID = "[HNG-00598]";
+var langUsed = "[Javascript]";
+var great = "Hello World";
+
+console.log(gre +', this is ' + firstName + lastName + ' with HNGi7ID ' + HNGI7ID + ' using ' + langUsed + ' for stage 2 task.  ' + 'chinxzypoet@gmail.com');
---
+++ testScripts/ikechukwu.js
@@ -0,0 +1,7 @@
+var info = {
+        firstName: "Ikechukwu",
+        lastName: "Madubuike",
+        HNGi7ID: "HNG-00589",
+        language: "javascript", 
+    }
+console.log(`Hello World, this is [${info.firstName}] [${info.lastName}] with HNGi7ID [${info.HNGi7ID}] using [${info.language}] for stage 2 task. ikechukwujames902@gmail.com`);
