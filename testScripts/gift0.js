83aac65d7a4c08e0c9fe2cd35ee774185fbab40e
jenykin24 <66228169+jenykin24@users.noreply.github.com>
2020-06-04 18:38:05 +0000

    Revert "Jennyking HNG Task 1"

--- testScripts/gift0.js
+++
@@ -1,5 +0,0 @@
-const display =() => {
-	
-return `Hello World, this is [Gift] [Maduabuchi] with HNGi7 ID [HNG-00767] using [javascript] for stage 2 task. giftmaduabuchi@gmail.com`;
-}
-	console.log(display());
