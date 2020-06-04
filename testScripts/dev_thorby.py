8dace8fad6b9e03f4dd627db8598546af24d50de
tobi-oye <[tobiloba5000@gmail.com]>
2020-06-04 18:09:32 +0100

    chore: create script

---
+++ testScripts/dev_thorby.py
@@ -0,0 +1,9 @@
+# The script is to return this: "Hello World, this is [full name] with HNGi7 ID [ID] using [language] for stage 2 task". 
+
+def my_hng_info (firstName, lastName, hng_id,language):
+
+    print("Hello World, this is {} {} with HNGi7 ID {} using {} for stage 2 task. tobioyewole25@gmail.com".format(firstName, lastName, hng_id, language))
+
+my_hng_info("[Oyewole]", "[Oluwatobi]","[HNG-04827]","[python]")
+
+
