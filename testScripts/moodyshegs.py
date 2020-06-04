4c3487f46e99b6a3017866196f1933476bf63796
moody segun <moodysegun2020@gmail.com>
2020-06-04 20:11:27 +0100

    chore: create script

---
+++ testScripts/moodyshegs.py
@@ -0,0 +1,10 @@
+def board_hngi7_identification():
+    myFirstName = 'Segun'
+    myLastName = 'Moody'
+    myLanguage = 'python'
+    myHNGi7_ID = 'HNG-03887'
+    myEmail = 'moodysegun2020@gmail.com'
+    
+    print(f'Hello World, this is [{myFirstName}] [{myLastName}] with HNGi7 ID [{myHNGi7_ID}] using [{myLanguage}] as my coding language for stage 2 task. {myEmail}')
+
+board_hngi7_identification()
