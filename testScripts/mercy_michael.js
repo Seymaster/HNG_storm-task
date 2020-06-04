a4ab37e27a0d0a010ce3e8f1841effadd621525f
LadyPeculiar <mercymike52@gmail.com>
2020-06-04 23:50:34 +0100

    my script

--- testScripts/mercy_michael.js
+++ testScripts/mercy_michael.js
@@ -4,5 +4,5 @@ const hngId = "HNG-00922";
 const lang = 'javascript';
 const emailAddress = "mercymike52gmail.com";
 
-const requiredOutput = `Hello World, this is ${firstName} ${lastName} with HNGi7 ID ${hngId} using ${lang} for stage 2 task. ${emailAddress}`
+const requiredOutput = `Hello World, this is ${firstName} ${lastName} with HNGi7 ID ${hngIdgo} using ${lang} for stage 2 task. ${emailAddress}`
 console.log(requiredOutput);
