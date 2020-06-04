f099e1f977f1e6833121b18f7c50e32e850f56a0
njun95 <njunfru@gmail.com>
2020-06-05 00:56:17 +0100

    njun's script comments

--- testScripts/njun.php
+++ testScripts/njun.php
@@ -7,11 +7,10 @@ $info = array(
     "id" => "HNG-00595",
     "email" => "njunfru@gmail.com"
 );
-//$obj = json_encode($info);
 
 //message to display
 $message = "Hello World, this is [{$info['firstName']}] [{$info['lastName']}] with HNGi7 ID [{$info['id']}] using [{$info['language']}] for stage 2 task. njunfru@gmail.com";
 
-//using print function to send out a string.
+//using echo function to send out a string.
 echo $message;
 ?>
