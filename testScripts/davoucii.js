f5994e8df04c7d3fd1cf88a1f5e735227e6d0c41
David <dav4thevid@gmail.com>
2020-06-04 03:25:49 +0100

    enhan: fixed typo

--- testScripts/davoucii.js
+++ testScripts/davoucii.js
@@ -4,10 +4,11 @@ const ID = '[HNG-03227]';
 const LANGUAGE = '[Javascript]';
 const EMAIL = 'dav4thevid@gmail.com'
 
-function callMyName(first_name,last_name,id, language, email) {
-    console.log(`Hello World, this is ${first_name} ${last_name} with HNGi7 ID ${id} using ${language} for stage 2 task as its output. ${email}`) 
-    
-    
+function output(first_name,last_name,id, language, email) {
+    console.log(`Hello World, this is 
+    ${first_name} ${last_name} with HNGi7 ID 
+    ${id} using ${language} for stage 2 task as its output. 
+    ${email}`);
 }
 
-callMyName(FIRST_NAME,LAST_NAME, ID, LANGUAGE, EMAIL);
+output(FIRST_NAME,LAST_NAME, ID, LANGUAGE, EMAIL);
