70c8cd385d35ff7a0201cb5f3bc7ab1e4105c0a5
JoelOvien <jovienloba1@gmail.com>
2020-06-04 15:03:55 +0100

    done my task

--- testScripts/task.py
+++
@@ -1,7 +0,0 @@
-def details(F,L,I,La,E):
-    return ("helloworld, this is ", F, L ,' with HNG ID ',I, ' using ', La, ' for stage 2 task. gowinmercy@gmail.com')
-
-print(details(F ='[Mercy]',
-    L = '[Godwin]',
-    I = '[HNG-00414]',
-    La = '[python]'))
