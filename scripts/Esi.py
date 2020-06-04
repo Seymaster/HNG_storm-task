26310ad8f948919631df6c4fda43d575990979ba
Mercy Esi <gowinmercy@gmail.com>
2020-06-04 15:19:58 +0100

    Delete Esi.py

--- scripts/Esi.py
+++
@@ -1,9 +0,0 @@
-def details(F,L,I,La,Em):
-    print("Hello World, this is",F,L,'with HNGi7 ID',I,'using', La,'for stage 2 task.',Em)
-
-details(
-    F ='[Mercy]',
-    L = '[Godwin]',
-    I = '[HNG-00414]',
-    La = '[python]',
-    Em ='gowinmercy@gmail.com')
