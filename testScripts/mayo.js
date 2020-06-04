ede8929b74bd1c1ee9840aafe4cb3e2bd8699168
Mayowa <63378559+Mayowa-tech@users.noreply.github.com>
2020-06-04 03:45:39 +0100

    Create mayo.js

---
+++ testScripts/mayo.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <script>
+ Oluwamayowa Alomasojo
+ </script>
+</head>
+<body>
+<script>
+ <p> Hello world, this is Alomasojo Oluwamayowa with HNGi7 ID HNG-03401 using JavaScript for stage 2 task </p>
+</script>
+</body>
+</html>
