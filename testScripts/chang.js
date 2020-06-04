0f475b55ac0451ba2cc9aa0035ccdec9a2a11396
chang <rorewole@gmail.com>
2020-06-04 16:46:13 +0100

    chore: create script

---
+++ testScripts/chang.js
@@ -0,0 +1,15 @@
+const me =()=> {
+    const myData = {
+      firstname: "Rahman",
+      lastname: "Orewole",
+      language: "JavaScript",
+      id: "HNG-05099",
+      mail: 'rorewole@gmail.com'
+    };
+    const result = `Hello World, this is Orewole Rahman with HNGi7 ID [HNG-05099] with email [rorewole@gmail.com] using [JavaScript] for stage two task.`  
+    return result;
+  }
+  
+  
+console.log(me());
+  
