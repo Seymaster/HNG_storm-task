0ef39c1efc0ab0b14d5875346af07d022d797f1e
the3rdbrain <tobitosin+the3rdbrain@users.noreply.github.com>
2020-06-04 22:20:16 +0100

    chore: create script

--- testScripts/sylvercodez.py
+++ testScripts/sylvercodez.py
@@ -1,4 +1,14 @@
-def MY_Details(**datas):
-    print('Hello World, this is '+ datas['FirstName']+ datas['LastName'] +' with HNGi7 ID '+ datas['Id']+' using ' + datas['Language'] + ' for stage 2 task.'+ datas['Email'])
 
-MY_Details(FirstName ='[OTENAIKE]',LastName = '[OLUWATOBI]',Id ='[HNG-00465]',Language ='[python]',Email ='tosinotenaike3@gmail')
+#my default function 
+def MY_details():
+
+    #my default variable
+    data = (FirstName = "[OTENAIKE]"
+        LastName = "[OLUWATOBI]"
+        Id = "[HNG-00465]"
+        Language = "[python]"
+        Email ='tosinotenaike3@gmail.com')
+
+    #output of the code
+    print("Hello World, this is", FirstName, LastName, "with HNGi7 ID", Id, "using", Language, "for stage 2 task.", Email)
+MY_details()
