e2b51d6426a54e16fa469c2b0560de3c23ea10c9
Teslim Hassan <64636170+HassanTeslim007@users.noreply.github.com>
2020-06-04 08:32:17 +0100

    Updated Alpha.js
    Added Square brackets as requested by team lead

--- scripts/Alpha.js
+++ scripts/Alpha.js
@@ -1,8 +1,8 @@
 var alpha = {
-	firstName: "Hassan",
-	lastName: "Teslim",
+	firstName: "[Hassan]",
+	lastName: "[Teslim]",
 	email: "hassanteslim12@gmail.com",
-	language: "JavaScript",
-	id: "HNG-00265",
+	language: "[JavaScript]",
+	id: "[HNG-00265]",
 };
-console.log("Hello world, this is " + alpha.firstName + " " + alpha.lastName + " with HNG7i ID " + alpha.id + " using " + alpha.language + " for stage 2 task." + " " + alpha.email); 
+console.log("Hello world, this is " + alpha.firstName + " " + alpha.lastName + " with HNGi7 ID " + alpha.id + " using " + alpha.language + " for stage 2 task." + " " + alpha.email); 
