91d1ee3d3bc64dcc84e7dbb6c2192bfebe97c305
Kayode <oluwaseyikayode25@gmail.com>
2020-06-04 10:27:29 +0100

    chore: create script

--- testScripts/Samson.py
+++ testScripts/Samson.py
@@ -1,11 +1,9 @@
-def code():
-    firstName = "[Kayode]"
-    lastName = "[Samson]"
-    language = "[Python]"
-    id = "[HNG-00727]"
-    email = "samcity39.sk@gmail.com"
-    print("Hello World, this is", firstName, lastName, "with HNGi7 ID", id, "using", language,
-          "for stage 2 task.", email)
+firstname = "[Kayode]"
+lastname = "[Samson]"
+language = "[Python]"
+id = "[HNG-00727]"
+email = "samcity39.sk@gmail.com"
+print("Hello World, this is {} {} with HNGi7 ID {} using {} for stage 2 task. {}".format
+      (firstname, lastname, id, language, email))
 
 
-code()
