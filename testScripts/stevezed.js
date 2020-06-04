e6db051707fe8fe64c607a7b98518679daa6250a
Joshua Folorunsho <joshuafolorunsho01@gmail.com>
2020-06-04 19:22:28 +0100

    refactor code into one script folder

---
+++ testScripts/stevezed.js
@@ -0,0 +1,5 @@
+const resultsHng = function (firstName = "Steve", lastName = "Waweru", id = "HNG-01325", language = "Javascript", email = "wawerusteve142@gmail.com") {
+  return `Hello World, this is [${firstName}] [${lastName}] with HNGi7 ID [${id}] using [${language}] for stage 2 task. ${email}`
+}
+resultsHng()
+console.log(resultsHng())
