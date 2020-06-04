6d704250ba3f9ca04c876f97948fe1944fb19ccd
Sunday Destiny <destinyjuniorsdj@gmail.com>
2020-06-04 16:01:23 +0100

    added files

---
+++ testScripts/script.js
@@ -0,0 +1,11 @@
+function output() {
+  output = {
+    name: "A",
+  };
+  return "Yeah!!!!";
+}
+
+output();
+console.log(
+  "Hello World, this is Olagesin Samuel with HNGi7 ID HNG-04817 and email seyi@hng.tech using Dart for stage 2 task"
+);
