e4e9b3cea6197725579930ffe4adac63044ca593
daKeshra7 <keshradamora@gmail.com>
2020-06-04 14:15:28 -0700

    chore: create script

--- daKeshra.py
+++ daKeshra.py
@@ -2,12 +2,13 @@
 
 def my_hng_profile():
     #Declaring variables
-    full_Name = "Keshinro Damilare"
-    hng_id = "HNG-01340"
-    lang_used = "Python"
+    first_Name = "[Keshinro]"
+    second_name = "[Damilare]"
+    hng_id = "[HNG-01340]"
+    lang_used = "[Python]"
     e_mail = "keshradamora@gmail.com"
 
 #Outputs the details
-    print("Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task".format(full_Name,hng_id,lang_used) )
+    print("Hello World, this is {} {} with HNGi7 ID {} using {} for stage 2 task. {}".format(first_Name, second_name, hng_id,lang_used, e_mail))
 
 my_hng_profile()
