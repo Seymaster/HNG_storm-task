0379162851b4b1913ddd20a260a69e6e6ad49720
= <=>
2020-06-04 13:33:01 +0100

    set up python script to print out my details to console

---
+++ testScripts/__enyason__.py
@@ -0,0 +1,11 @@
+def printDetails():
+    first_name = "[Emmanuel]"
+    last_name = "[Enya]"
+    id = "[HNG-05857]"
+    language = "[Python]"
+    email = "enyasonjnr@gmail.com"
+
+    output = "Hello World, this is " +first_name + " "+ last_name + " with HNGi7 ID "+id +" using "+language +" for stage 2 task. "+email
+    print(output)
+
+printDetails()
