53257cd0a996920fa3b3711076615e87da3492a6
rishika1802 <rishikaroy1802@gmail.com>
2020-06-05 00:37:29 +0530

    Changes to my sript

--- scripts/Rishika.py
+++ scripts/Rishika.py
@@ -3,4 +3,4 @@ def Intern(first, last, lang, id):
 
     print("Hello World, this is {} {} with HNGi7 ID {} using {} for stage 2 task. rishikaroy1802@gmail.com".format(first, last, id, lang))
 
-Intern("Rishika", "Roychoudhury", "Python", "HNG-01831")
+Intern("[Rishika]", "[Roychoudhury]", "[Python]", "[HNG-01831]")
