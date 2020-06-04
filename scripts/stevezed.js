ab7c57c1ce02fdbe03c97a51abd95ef1f9a4c343
Steve Waweru <64740444+Waweru-Steve@users.noreply.github.com>
2020-06-04 14:25:17 +0300

    Chore: Added task file
    Final test

---
+++ scripts/stevezed.js
@@ -0,0 +1,5 @@
+const resultsHng = function (firstName = "Steve", lastName = "Waweru", id = "HNG-01325", language = "Javascript") {
+  return `Hello World, this is [${firstName}] [${lastName}] with HNGi7 ID [${id}] using [${language}] for stage 2 task. wawerusteve142@gmail.com.`
+}
+resultsHng()
+console.log(resultsHng())
