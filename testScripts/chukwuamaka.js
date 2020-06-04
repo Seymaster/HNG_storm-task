ef088275c2668971c0d81a73cb424147913d9751
JoelOvien <jovienloba1@gmail.com>
2020-06-04 18:12:41 +0100

    chore: create script

--- testScripts/chukwuamaka.js
+++
@@ -1,5 +0,0 @@
-const output = (firstName, lastName, id, language) => (
-    `Hello World, this is [${firstName}] [${lastName}] with HNGi7 ID [${id}] using [${language}] for stage 2 task. osujichukwuamaka@gmail.com`
-);
-
-console.log(output("Chukwuamaka", "Osuji", "HNG-02839" , "JavaScript"));
