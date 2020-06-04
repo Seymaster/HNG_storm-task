ffa2a3b45687219159f11655c1e6094e61a07a9b
yunglord <stanleychibuike31@gmail.com>
2020-06-04 22:17:43 +0100

    techbot.py
    this is task 2 assignment

---
+++ testScripts/techbot.py
@@ -0,0 +1,22 @@
+
+
+def HNG():
+    """
+    this is the second task of the HNG intenship program. we are expected to output
+    our name, id and language
+    """
+
+
+
+    firstName = "[Stanley]"
+    LastName = "[Ekwebelem]"
+    id = "[HNG-01260]"
+    email = "stanleychibuike31@gmail.com"
+    language = "[Python]"
+
+
+    print("Hello World, this is " + firstName + " " + LastName  + " with HNGi7 ID " + id + " using " + language + " for stage 2 task. stanleychibuike31@gmail.com")
+
+
+HNG()
+
