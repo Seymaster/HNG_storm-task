da40fa52c7bd43e0c6fa61731dc1113ae93069b7
Olanrewaju Temitope Akinlade (A) <oWX740731@china.huawei.com>
2020-06-04 23:26:03 +0100

    my task

--- testScripts/olanrewaju2.py
+++ testScripts/olanrewaju2.py
@@ -1,8 +1,9 @@
-full_name = '[Olanrewaju] [Akinlade]'
+first_name = '[Olanrewaju]'
+last_name = '[Akinlade]'
 hng_id = '[hng-01665]'
 language = '[Python]'
 email = 'topelanre30@gmail.com'
-full_statement = ("Hello World," + " "+ "this is" +" "+ full_name + " " + "with HNGi7 ID" + " " + hng_id.upper() + " "+ "using" + " " + language + " " + "for stage 2 task" +"." + email)
+full_statement = ("Hello World," + " "+ "this is" +" "+ first_name + " " + last_name + " " + "with HNGi7 ID" + " " + hng_id.upper() + " "+ "using" + " " + language + " " + "for stage 2 task" +"." + email)
 print(full_statement)
 
 
