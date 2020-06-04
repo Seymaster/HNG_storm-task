5cac6770d7219bc43d7480cd438062a6d7a88e1f
Adebambo Abiodun Oluwasegun <abiodunadebambo44@gmail.com>
2020-06-04 20:35:35 +0100

    Editted the script, removed the square brackets

--- testScripts/KodinNYoung.js
+++ testScripts/KodinNYoung.js
@@ -3,6 +3,6 @@ const firstName = "Abiodun",
 	hngi7ID = "HNG-00695",
 	language = "Javascript",
 	email = "abiodunadebambo869@yahoo.com",
-	output = `Hello World, this is [${firstName}] [${lastName}] with HNGi7 ID [${hngi7ID}] using [${language}] for stage 2 task. [${email}]`;
+	output = `Hello World, this is ${firstName} ${lastName} with HNGi7 ID ${hngi7ID} using ${language} for stage 2 task. ${email}`;
 
 console.log(output); 
