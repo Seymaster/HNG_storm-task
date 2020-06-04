050edabe2e22e372fd148f27b3439a1e127bd0d0
nechekendu <54294315+nechekendu@users.noreply.github.com>
2020-06-04 10:23:46 +0100

    Update neche_ibekendu.js

--- scripts/neche_ibekendu.js
+++ scripts/neche_ibekendu.js
@@ -1,12 +1,13 @@
 // Script to return data based on this format: Hello World, this is [Your full name] with HNGi7 ID [Your HNG ID] and [email] using [language Used] for stage 2 task
 
 const details = {
-    name: "Neche Ibekendu",
-    id: "HNG-05572",
-    language: "JavaScript",
+    firstName: "[Neche]",
+    lastName: "[Ibekendu]",
+    id: "[HNG-05572]",
+    language: "[JavaScript]",
     email: "nechekendu@gmail.com"
   };
   
-  const returnedDetails = `Hello World, this is ${details.name} with HNGi7 ID ${details.id} and email ${details.email} using ${details.language} for stage 2 task`;
+  const returnedDetails = `Hello World, this is ${details.firstName} ${details.lastName} with HNGi7 ID ${details.id} using ${details.language} for stage 2 task. ${details.email}`;
   
   console.log(returnedDetails);
