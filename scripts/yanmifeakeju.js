4ff05a0931fd6d09cc483f32e17e72f1d787abad
Mercy Esi <51007408+Seymaster@users.noreply.github.com>
2020-06-04 18:04:38 +0100

    Update yanmifeakeju.js

--- scripts/yanmifeakeju.js
+++ scripts/yanmifeakeju.js
@@ -2,11 +2,11 @@ function output() {
   const data = {
     firstName: "Oluwayanmife",
     lastName: "Akeju",
-    language: "PHP & JS",
+    language: "JS",
     id: "HNG-00197",
   };
   str =
-    "Hello World, this is [Oluwayanmitoe] [Akeju] with HNGi7 ID [HNG-00197] using [PHP] for stage 2 task. yanmifeakeju@gmail.com";
+    "Hello World, this is [data.firstName] [data.lastName] with HNGi7 ID [data.id] using [data.language] for stage 2 task. yanmifeakeju@gmail.com";
 
   return str;
 }
