17d8f55f79abb58c51fc4a55cf1e6960ca0130bd
philip omoigui <philipomoigui@gmail.com>
2020-06-05 00:03:11 +0100

    chore: create script

---
+++ testScripts/TheWitness.js
@@ -0,0 +1,11 @@
+function expOutput() {
+    var Data = {
+        firstName: "[Philip]",
+        lastName: "[Omoigui]",
+        id: "[HNG-04031]",
+        email: "philipomoigui@gmail.com", 
+        language: "[Javascript]",
+    };
+    console.log("Hello World, this is " + Data.firstName + " " + Data.lastName + " with HNGi7 ID " + Data.id + " using " + Data.language + " for stage 2 task. " + Data.email)
+}
+expOutput()
