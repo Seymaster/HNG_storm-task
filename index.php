4d55261d2f1b9c79f04ed817eab5f18fda0af695
Sunday Destiny <destinyjuniorsdj@gmail.com>
2020-06-04 17:09:22 +0100

    added new design

--- index.php
+++ index.php
@@ -143,129 +143,370 @@ $total = count($members);
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Team storm</title>
-    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@900&display=swap" rel="stylesheet">
+    <title>HNGi7 | Team Storm</title>
     <style>
-    table,
-    .container {
-        margin: 20px auto;
-        max-width: 1000px;
-    }
-
-    .stat {
-        color: white;
-    }
-
-    .team-name {
-        font-size: 1.6rem;
-        margin-bottom: 10px;
-    }
-
-    .leaders {
-        display: flex;
-        justify-content: space-between;
-    }
-
-    .leaders li a {
-        text-decoration: underline;
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
     }
 
     body {
-        font-weight: bold;
-        padding: 1em;
+        color: #707070;
+        font-family: sans-serif;
     }
 
-    .stat {
+    header {
+        width: 100vw;
+        height: 100px;
+        display: flex;
+        align-items: center;
+        padding-top: 50px;
+        margin-bottom: 30px;
+    }
+
+    h1 {
+        width: 350px;
+        text-align: center;
+        color: #6FE3FF;
+        text-transform: uppercase;
+        font-size: 1.7rem;
+        font-family: 'Roboto', sans-serif;
+    }
+
+    .leads-container {
+        padding: 0 20%;
+        width: 100%;
+        /* background-color: aqua; */
         display: flex;
         justify-content: space-between;
     }
 
-    thead {
-        color: #fff;
+    .leads-container div p:nth-of-type(1) {
+        font-weight: 900;
+        margin-bottom: 5px;
     }
 
-    tbody {
-        text-align: center;
-        color: #fff;
+    .leads-container div p:nth-of-type(2) {
+        font-weight: lighter;
+    }
+
+    .flex {
+        display: flex;
+        align-items: center;
+        margin-bottom: 10px;
+    }
+
+    .list-color {
+        height: 20px;
+        width: 20px;
+        background-color: #6FE3FF;
+        margin-right: 10px;
+    }
+
+    .grid-container {
+        display: grid;
+        grid-template-columns: 250px 1fr;
+        grid-template-rows: auto;
+        grid-template-areas: 'aside main';
+    }
+
+    aside {
+        padding: 20px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding-top: 100px;
+    }
+
+    aside h3 {
+        font-size: 1.6rem;
+        margin-bottom: 40px;
+    }
+
+    aside button {
+        background-color: #6FE3FF;
+        margin-bottom: 40px;
+        border: none;
+        outline: none;
+        padding: 10px 40px;
+        color: white;
+        border-radius: 4px;
+        cursor: pointer;
+    }
+
+    section {
+        padding: 30px;
+        background-color: #F5F5F5;
+    }
+
+    .graphics-cards-container {
+        display: flex;
+        justify-content: space-between;
+        margin-bottom: 20px;
+        flex-wrap: wrap;
+    }
+
+    .graphics-cards-container div {
+        flex-grow: 1;
+        height: 120px;
+        margin: 10px;
+        border-radius: 10px;
+        padding: 20px;
+        color: white;
+        padding-top: 30px;
+    }
+
+    .graphics-cards-container div p {
+        margin-bottom: 14px;
+
+    }
+
+    .graphics-cards-container div p:nth-of-type(2) {
+        font-size: 2rem;
+        font-weight: lighter;
+    }
+
+    .card-one {
+        background-color: #6FE3FF;
+    }
+
+    .card-two {
+        background-color: #51B25C;
+    }
+
+    .card-three {
+        background-color: #FF6565;
+    }
+
+    .table--container {
+        overflow-x: auto;
     }
 
     table {
-        margin-top: 100px;
+        width: 100%;
+        /* min-width: 600px; */
+        background-color: white;
+        border-radius: 10px;
+        text-align: left;
+        padding: 20px 40px;
+        /* overflow-x: auto; */
+    }
+
+
+    table th,
+    table td {
+        height: 50px;
+        vertical-align: middle;
+        text-align: center;
+        border-bottom: 1px solid #ddd;
+        padding: 20px 0;
+    }
+
+
+
+    .pass {
+        background-color: #51B25C;
+        color: white;
+        padding: 4px;
+        margin: auto,
+            width: 80%;
+    }
+
+    .fail {
+        background-color: #FF6565;
+        color: white;
+        padding: 4px;
+        width: 80%;
+    }
+
+    @media (max-width: 1000px) {
+        .grid-container {
+            grid-template-columns: 200px 1fr;
+        }
+
+        section {
+            padding: 20px;
+        }
+
+        h1 {
+            width: 200px;
+            font-size: 1.5rem
+        }
+    }
+
+    @media (max-width: 786px) {
+        .grid-container {
+            display: block;
+        }
+
+        aside {
+            padding: 20px;
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            justify-content: center;
+            /* padding-top: 100px; */
+        }
+
+        aside h3 {
+            margin-right: 30px;
+        }
+
+        .leads-container {
+            padding: 0 10%;
+            width: 100%;
+            display: flex;
+            justify-content: space-between;
+        }
+    }
+
+    @media (max-width: 400px) {
+        header {
+            display: block;
+            padding-top: 10px;
+        }
+
+        h1 {
+            display: block;
+            width: 100%;
+            padding-bottom: 20px;
+        }
+
+        table {
+            overflow: auto;
+        }
     }
     </style>
 </head>
 
 <body>
     <header>
-        <div class="container">
-            <div class="team-name">TEAM STORM</div>
-            <ul class="leaders">
-                <li>Leads:</li>
-                <li>
-                    <a href="https://www.github.com/Seymaster">ESI - Backend</a>
-                </li>
-                <li>
-                    <a href="https://www.github.com/joshuafolorunsho">__Josh__ - Frontend</a>
-                </li>
-                <li>
-                    <a href="https://www.github.com/">Aj - devOps</a>
-                </li>
-            </ul>
-        </div>
-    </header>
-
-    <div>
-        <div class="container">
-            <div class="stat">
-                <p class="bg-green-500 px-2 py-3">Total Submission: <?=$totalScripts?></p>
-                <p class="bg-green-500 px-2 py-3">Passed: <?=$totalPassed?></p>
-                <p class="bg-red-500 px-2 py-3">Failed: <?=$totalScripts - $totalPassed?></p>
+        <h1>Team Storm</h1>
+        <div class='leads-container'>
+            <div>
+                <p>@Esi</p>
+                <p>Team Lead</p>
+            </div>
+            <div>
+                <p>@__josh__</p>
+                <p>Frontend Lead</p>
+            </div>
+            <div>
+                <p>Aj</p>
+                <p>DevOps</p>
             </div>
         </div>
-    </div>
+    </header>
+    <main class='grid-container'>
+        <aside>
+            <h3>Team List</h3>
+            <button onclick="window.location='?json'">View Json List</button>
+        </aside>
+        <section>
+            <div class='flex'>
+                <div class="list-color"></div>
+                <h5>Team List</h5>
+            </div>
+            <div class='graphics-cards-container'>
+                <div class='card-one'>
+                    <p>Total Submissions</p>
+                    <p><?=$totalScripts?></p>
+                </div>
+                <div class='card-two'>
+                    <p>Total Passed</p>
+                    <p><?=$totalPassed?></p>
+                </div>
+                <div class='card-three'>
+                    <p>Failed</p>
+                    <p><?=$totalScripts - $totalPassed?></p>
+                </div>
+            </div>
+            <div class='table-container'>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>HNGID</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Message</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <!-- use bg-green-500 class for passed -->
+                        <?php if(ob_get_level()) ob_start(); ?>
+                        <?php foreach ($messages as $output): ?>
 
-    <table>
-        <thead class="bg-blue-700">
-            <tr>
-                <th class="w-1/6 px-4 py-2">HNG ID</th>
-                <th class="w-1/6 px-4 py-2">Name</th>
-                <th class="w-1/2 px-4 py-2">Message</th>
-                <th class="w-1/6 px-4 py-2">Status</th>
-            </tr>
-        </thead>
-        <tbody>
-            <!-- use bg-green-500 class for passed -->
-            <?php if(ob_get_level()) ob_start(); ?>
-            <?php foreach ($messages as $output): ?>
+                        <?php if ($output['pass'] === true): ?>
 
-            <?php if ($output['pass'] === true): ?>
+                        <tr>
+                            <td><?=$output['id']?></td>
+                            <td><?=$output['name']?></td>
+                            <td><?=$output['message']?></td>
+                            <td>
+                                <div class='pass'>Pass</div>
+                            </td>
+                        </tr>
+                        <?php elseif ($output['pass'] === false): ?>
+                        <tr>
+                            <td><?=$output['id']?></td>
+                            <td"><?="Incorrect formatting from " . $output['filename']?></td>
+                                <td"><?=$output['message']?></td>
+                                    <td>
+                                        <div class='fail'>Fail</div>
+                                    </td>
+                        </tr>
 
-            <tr class="bg-green-500">
-                <td class="border px-4 py-2"><?=$output['id']?></td>
-                <td class="border px-4 py-2"><?=$output['name']?></td>
-                <td class="border px-4 py-2"><?=$output['message']?></td>
-                <td class="border px-4 py-2">Pass</td>
-            </tr>
-            <?php elseif ($output['pass'] === false): ?>
-            <tr class="bg-red-500">
-                <td class="border px-4 py-2"><?=$output['id']?></td>
-                <td class="border px-4 py-2"><?="Incorrect formatting from " . $output['filename']?></td>
-                <td class="border px-4 py-2"><?=$output['message']?></td>
-                <td class="border px-4 py-2">Fail</td>
-            </tr>
+                        <?php endif;?>
 
-            <?php endif;?>
-
-
-
-            <!-- use bg-red-500 class for passed -->
-
-        </tbody>
-        <?php ob_flush(); flush(); ?>
-        <?php endforeach;?>
-    </table>
+                        <?php ob_flush(); flush(); ?>
+                        <?php endforeach;?>
+                        <!-- <tr>
+                            <td>12334455</td>
+                            <td>Adedeji Abiola</td>
+                            <td>Dejiabiola@yahoo.com</td>
+                            <td>Hello World</td>
+                            <td>
+                                <div class='pass'>Pass</div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>12334455</td>
+                            <td>Adedeji Abiola</td>
+                            <td>Dejiabiola@yahoo.com</td>
+                            <td>Hello World</td>
+                            <td>
+                                <div class='pass'>Pass</div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>12334455</td>
+                            <td>Adedeji Abiola</td>
+                            <td>Dejiabiola@yahoo.com</td>
+                            <td>Hello World</td>
+                            <td>
+                                <div class='pass'>Pass</div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>12334455</td>
+                            <td>Adedeji Abiola</td>
+                            <td>Dejiabiola@yahoo.com</td>
+                            <td>Hello World</td>
+                            <td>
+                                <div class='fail'>Fail</div>
+                            </td>
+                        </tr> -->
+                    </tbody>
+                </table>
+            </div>
+        </section>
+    </main>
 </body>
 
 </html>
