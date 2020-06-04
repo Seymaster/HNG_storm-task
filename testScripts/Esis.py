c47cf388ceb104192101234dd2856239f5afeb6e
Mercy Esi <gowinmercy@gmail.com>
2020-06-04 14:14:31 +0100

    Update Esis.py

--- testScripts/Esis.py
+++ testScripts/Esis.py
@@ -8,3 +8,4 @@ details(F ='[Mercy]',
     E = 'gowinmercy@gmail.com')
 
 A = "Hello World, this is [Godwin] [Mercy] with HNGi7 ID [HNG-00414] using [python] for stage 2 task. " +"gowinmercy@gmail.com"
+print(A)
