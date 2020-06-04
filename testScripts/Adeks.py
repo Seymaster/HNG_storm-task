7adbc0d2f5ccd681bb925d7205fec841dab4b11f
Michael Afolabi <adekunleafolabi354@gmail.com>
2020-06-04 19:58:54 +0100

    chore create script adeks.py

---
+++ testScripts/Adeks.py
@@ -0,0 +1,6 @@
+firstName = "Michael"
+hngiId = "HNG-03254"
+lastName = "Afolabi"
+email = "adekunleafolabi354@gmail.com"
+
+print(f"Hello World, this is [{firstName}] [{lastName}] with HNGi7 ID [{hngiId}] using [python] for stage 2 task. {email}")
