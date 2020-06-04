06527507394f0e6798e9a7617c7e8ed5ebb71630
alex <alexmuia31@gmail.com>
2020-06-04 18:50:20 +0300

    My script

---
+++ testScripts/alexmuia.py
@@ -0,0 +1,14 @@
+def FunctionG(**kwargs):
+    print("Hello World, this is "+ kwargs["FirstName"] +" " +kwargs["LastName"] +" with " +"HNGi7 ID " 
+    +kwargs["HNGid"] +" using "+kwargs["Language"]+ " for stage 2 task. "+kwargs["Email"])
+
+FunctionG(FirstName="[Alex]", LastName="[Muia]", 
+Email="alexmuia31@gmail.com", Language="[Python]", HNGid="[HNG-04662]")
+
+
+
+
+
+
+
+
