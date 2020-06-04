6d88c95066cab52f37ddc15d6e1efc45123d4ccd
wintercameearly <oluwanifemi.bamgbose@stu.cu.edu.ng>
2020-06-04 17:04:28 +0100

    Added my submission

---
+++ testScripts/OluwanifemiBam.php
@@ -0,0 +1,7 @@
+<?php 
+function hngname(){
+    print("Hello World, this is [Oluwanifemi] [Bamgbose] with HNGi7 ID [HNG-01392] using [PHP] for stage 2 task. nifemibams@gmail.com");
+}
+
+hngname();
+?>
--- testScripts/OluwanifemiBam.py
+++
@@ -1,4 +0,0 @@
-def hngname():
-    print ("Hello World, this is Oluwanifemi Bamgbose with HNGi7 ID HNG-00197 and email nifemibams@gmail.com using PHP for stage 2 task.")
-
-hngname()
