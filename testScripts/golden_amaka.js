de0a2dbf73ab270126a26f6c1a35465b4c25cadb
Chiamaka Ezeoke <aezeokeamaka@gmail.com>
2020-06-04 23:00:01 +0100

    HNG-03836 Task

---
+++ testScripts/golden_amaka.js
@@ -0,0 +1,7 @@
+let Name = "[Chiamaka] [Ezeoke]";
+let id = "[HNG-03836]";
+let language = "[Javascript]";
+let EMail = "aezeokeamaka@gmail.com";
+
+console.log("Hello World, this is", Name , "with HNGi7 ID", id, "using", language,
+"for stage 2 task.", EMail);
