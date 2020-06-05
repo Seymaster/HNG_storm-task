19db69c86cb6c14a69fb822c11799bdf982e3ca1
Joseph Henshaw <furiousjoe16@gmail.com>
2020-06-05 10:03:18 +0100

    task completed

---
+++ testScripts/Big-jo.js
@@ -0,0 +1,12 @@
+const projectOutput = () => {
+    const data = {
+        firstName : "Joseph" ,
+        lastName : "Henshaw" ,
+        id : "HNG-04674" ,
+        language : "JavaScript" ,
+        email : "henshawjoseph.a@gmail.com"
+    }
+
+    console.log(`Hello World, this is [${data.firstName}] [${data.lastName}] with HNGi7 ID [${data.id}] using [${data.language}] for stage 2 task. ${data.email}`)
+}
+projectOutput()
