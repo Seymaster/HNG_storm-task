1859d48557a00aa7907e793373d06e36f1d382a4
Bashir Danjuma <35150721+BashDee@users.noreply.github.com>
2020-06-05 08:54:49 +0100

    Update chAce.py

--- scripts/chAce.py
+++ scripts/chAce.py
@@ -1,11 +1,13 @@
-import json
+firstName='[Bashir]'
 
-full_name='Bashir Oyekanmi Danjuma'
-identity='HNG-01092'
-language='Python'
+lastName='[Danjuma]'
 
-output=('Hello World, this is ' + full_name + ' with HNGi7 ID '+ identity+ ' using '+ language+ ' for stage 2 task')
+identity='[HNG-01092]'
 
+language='[Python]'
 
-json_output=json.dumps(output)
-print(json_output)
+email='danjumabashir58@gmail.com'
+
+​
+
+print("Hello World, this is {} {} with HNGi7 ID {} using {} for stage 2 task. danjumabashir58@gmail.com".format(firstName, lastName, identity, language))
