871b212c5b8d3ebfeb72024ec00ae91d7a26cef3
Alabee <ianmutevu96@gmail.com>
2020-06-05 11:51:27 +0300

    Updated script

--- testScripts/mutevu.php
+++ testScripts/mutevu.php
@@ -6,7 +6,7 @@ $hngId = "HNG-03643";
 $language = 'PHP';
 
 function printOutput($fname, $lname, $id, $language){
-	echo "Hello World, this is [" .$fname ."] [" .$lname ."] with HNGi7 ID [" .$id ."] using [" .$language ."] for stage 2 task";
+	return "Hello World, this is [" .$fname ."] [" .$lname ."] with HNGi7 ID [" .$id ."] using [" .$language ."] for stage 2 task";
 }
 
 printOutput($firstName, $lastName, $hngId, $language);
