a6a559be7d537d92c28e474f4a1471e5e3224de9
root <root@localhost.localdomain>
2020-06-05 12:14:24 +0300

    chore: create script

--- testScripts/benson.py
+++ testScripts/benson.py
@@ -1,13 +1,13 @@
 def main():
-    task()
+    print_task()
 
-def task():
-    f_name = ("Benson")
-    l_name = ("Ndora")
-    HNGi7_ID = ("HNG-04766")
-    la = ("python")
-    email = ("benndora2@gmil.com")
-    print (f"Hello World, this is {f_name} {l_name} with HNGi7 ID {HNGi7_ID} using {la} for stage 2 task {email}")
+def print_task():
+    f_name = '[Benson]'
+    l_name = '[Ndora]'
+    HNGi7_ID = '[HNG-04766]'
+    la = '[python]'
+    email = '[benndora2@gmil.com]'
+    print(f"Hello World, this is {f_name} {l_name} with HNGi7 ID {HNGi7_ID} using {la} for stage 2 task {email}")
 
 
 if __name__ == "__main__":
