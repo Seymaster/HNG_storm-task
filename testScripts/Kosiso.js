cc8c61bec173e19323def43eca2937be93eaa126
Kosi <kafoenyi@gmail.com>
2020-06-05 23:02:46 +0100

    chore: create script

--- testScripts/Kosiso.js
+++ testScripts/Kosiso.js
@@ -1,6 +1,5 @@
-function readMe (firstName,lastName,id,language,email){
-	console.log("Hello world, this is " + firstName + " " + lastName + " with HNGi7 ID " + id + " using " + language + " for stage 2 task. " + " " + email );
+function readMe (firstName,lastName,id,language){
+	console.log("Hello world, this is " + firstName + " " + lastName + " with HNGi7 ID " + id + " using " + language + " for stage 2 task. kafoenyi@gmail.com");
 }
 
-readMe("Kosisochukwu","Afoenyi","HNG-00803","JavaScript","kafoenyi@gmail.com");
-
+readMe("[Kosisochukwu]","[Afoenyi]","[HNG-00803]","[JavaScript]");
