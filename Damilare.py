e5ea523c737f2f689503eadc3326f323303bdc6d
drey-ux <66339613+drey-ux@users.noreply.github.com>
2020-06-05 13:00:16 +0100

    Add files via upload

---
+++ Damilare.py
@@ -0,0 +1,7 @@
+firstname = "Damilare"
+lastname = "Oyetade"
+ID = "HNG-06111"
+language = "Python"
+email = "damilareoyetade1@gmail.com"
+
+print("Hello World, this is [{}] [{}] with HNGi7 ID [{}] using [{}] for stage 2 task. {}".format(firstname, lastname, ID, language, email))
