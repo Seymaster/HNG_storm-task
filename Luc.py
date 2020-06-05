d3583e0daec24f843f7957ad929c720a32182b16
Ntwariluc <51233114+Ntwariluc@users.noreply.github.com>
2020-06-05 12:35:01 +0200

    Create Luc.py

---
+++ Luc.py
@@ -0,0 +1,6 @@
+firstName= "NTWARI NDAGIJIMANA"
+lastName= "Jean luc"
+id= "03452"
+language= "python"
+email= "ntwalinj@gmail.com"
+print("Hello World, this is "  +   firstName   +      lastName +" with HNGi7 ID " +  id  + " using "  + language  +  "  for stage 2 task. " +  email)
