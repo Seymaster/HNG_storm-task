4a05e9879d2617aceee182add2a028b6576d085e
NerdPraise <praiseajayi2@gmail.com>
2020-06-05 23:06:22 +0100

    new scripts

---
+++ scripts/Prizrak69.py
@@ -0,0 +1,6 @@
+first_name = "[Aitsebaomo]"
+last_name = "[Jerry]"
+ID_no = "[HNG-06657]"
+lang = "[python]"
+email = "jerrickson31@gmail.com"
+print("Hello World, this is " +first_name+ " " +last_name+ " with HNGi7 ID "+ID_no+" using "+lang+" for stage 2 task. " +email+"""""""")
--- scripts/mbajoshuac.js
+++ scripts/mbajoshuac.js
@@ -1,3 +1,5 @@
-const record = "Hello World, this is [Joshua] [Mba] with HNGi7 ID [HNG-04870] using [Javascript] for stage 2 task. angelicjoekeyz@gmail.com";
-
-console.log(record);
+function profile() {
+    var output = "Hello World, this is [Joshua] [Mba] with HNGi7 ID [HNG-04870] using [Javascript] for stage 2 task.angelicjoekeyz@gmail.com";
+    console.log(output);
+}
+profile();
