ffacca7110463d62d7d150a48874154f4bc384d6
Sefunmi Afere <sefunmiafere@gmail.com>
2020-06-05 11:18:51 +0100

    chore: create script

---
+++ Idadaniel.js
@@ -0,0 +1,14 @@
+function aboutMe() {
+	let firstName = "Sefunmi"
+	let lastName = "Afere"
+	let myId = "HNG-02879"
+	let language = "JavaScript"
+	let email = "sefunmiafere@gmail.com"
+
+	let aboutMe ="Hello World, this is [" + firstName + "] ["
+	 + lastName + "] with HNGi7 ID [" 
+	 + myId +  "] using [" + language  + "] for stage 2 task.\n" + email
+	console.log(aboutMe)
+}
+
+aboutMe()
