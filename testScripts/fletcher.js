0febd1d49b9a29bc5ddf88306ebfb89174332166
Olufemi Ezekiel <ororo17@gmail.com>
2020-06-05 17:13:10 +0100

    Kindly merge

--- testScripts/fletcher.js
+++ testScripts/fletcher.js
@@ -1,11 +1,10 @@
 const my_script = () => {
     const data = {
-        name : 'OLUFEMI EZEKIEL OLUFEMI',
+        name : "[OLUFEMI] [EZEKIEL]",
         id : 'HNG-04946',
         language : 'javascript'
     }
-    return `Hello World, this is ${data.name} with HNGi7 ID ${data.id} using ${data.language} for stage 2 task. ezekielolufemi09@yahoo.com`;
-    console.log(my_script);
+    return `Hello World, this is ${data.name} with HNGi7 ID ${data.id} using ${data.language} for stage 2 task. ezekielolufemi09@yahoo.com`;;
 };
 
-    my_script();
+    Console.log(my_script());
