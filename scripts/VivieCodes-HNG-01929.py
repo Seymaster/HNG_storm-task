cdaa68c52bebe6a295d8420587348b3f3c3e6a85
Vivian Patrick <vivianpatrick456gmail.com>
2020-06-05 14:23:32 -0700

    chore: create script

---
+++ scripts/VivieCodes-HNG-01929.py
@@ -0,0 +1,6 @@
+firstName = "[Vivian]"
+lastName = "[Nzekwe]"
+id = "[HNG-01929]"
+email = "vivianpatrick48@gmail.com"
+language = "[python]"
+print ("Hello World, this is {} {} with HNGi7 ID {} using {} for stage 2 task. vivianpatrick458@gmail.com" .format(firstName, lastName, id, language)) 
