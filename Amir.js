9c5b6f261aa54b80351bb531b2b08960c729b2c7
amirtechnoide <>
2020-06-05 23:24:01 +0100

    chore: create script

--- Amir.js
+++ Amir.js
@@ -1,7 +1,5 @@
-const firstname = "Tchakounte";
-const lastname = "Amir";
-const id = "HNG-06436" ;
-const language= "Javascript";
-const email = "amirzoutor@gmail.com";
-const stage = "2";
-console.log(`Hello World,this is ${firstname} ${lastname} with HNGI7 iD ${id} and email ${email} using ${language} for stage ${2} task`);
+const fullname = "Tchakounte Amir"
+const id = "HNG-06436"
+const language= "Javascript"
+const email = "amirzoutor@gmail.com"
+console.log(`Hello World, this is ${fullname} with HNGi7 ID ${id} using ${language} for stage 2 task.email ${email}`);
