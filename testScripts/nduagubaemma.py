e1887664c93127bfa5c55a470460292eb074f75f
kc emma <nduagubaemma@gmail.com>
2020-06-05 06:20:00 +0100

    Update nduagubaemma.py

--- testScripts/nduagubaemma.py
+++ testScripts/nduagubaemma.py
@@ -6,13 +6,11 @@ This is the second task of our HNG intenship program. we are expected to output 
 
 def code():
 
-    firstName = "[Emmanuel]"
-    lastName = "[Nduaguba]"
-    id = "[HNG-01522]"
-    email = "nduagubaemma@gmail.com"
-    language = "[Python]"
-    print("Hello World, this is ", firstName ," " ,lastName ," with HNGi7 ID ",  id, " using ",  language, " for stage 2 task. nduagubaemma@gmail.com@gmail.com")
+    firstName = 'Emmanuel'
+    lastName = 'Nduaguba'
+    id = 'HNG-01522'
+    email = 'nduagubaemma@gmail.com'
+    language = 'Python'
+    print('Hello World,this is', firstName , lastName ,' with HNGi7 ID ',  id, ' using ',  language, ' for stage 2 ')
 
 code()
-#print(type('language'))
-nnmn
