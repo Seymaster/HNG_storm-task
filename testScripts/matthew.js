4b7625dfc909e52a454f6accac7ea8ac6b91057d
matthew <matthewigbede@gmail.com>
2020-06-05 21:15:26 +0100

    chore: create script

--- testScripts/matthew.js
+++ testScripts/matthew.js
@@ -2,6 +2,8 @@ let firstName = "[Matthew]";
 let lastName = "[Igbede]";
 let hngID = "[HNG-03082]";
 let language = "[JavaScript]";
+let email = "[matthewigbede@gmail.com]"
 
-let output = "Hello World, this is " +firstName+ " " +lastName+ " with HNGi7 ID " +hngID+ " using " +language+ " for stage 2 task. matthewigbede@gmail.com";
+let output = "Hello World, this is " +firstName+ " " +lastName+ " with HNGi7 ID " +hngID+ " using " +language+ " for stage 2 task";
 console.log(output);
+
