2968be4c245fa98b7f6d27c6203d6426e0f14a34
OrLAN-K <55242168+OrLAN-K@users.noreply.github.com>
2020-06-05 12:34:47 +0100

    Update orlan-kay.js
    RE-edit my code.

--- orlan-kay.js
+++ orlan-kay.js
@@ -1,9 +1,9 @@
 var myData = {
-	fName: "zubair"
-	lName: "olanrewaju"
-	id: "HNG-05087"
-	email: "olanrkayus@gmail.com"
-	language: "javaScript"
-},
+	fName: "ZUBAIR",
+	lName: "OLANREWAJU",
+	id: "HNG-05087",
+	email: "olanrkayus@gmail.com",
+	language: "javaScript",
+}
 var myDetails = `Hello world, this is [${myData.fName}] [${myData.lName}] with HNGi7 ID [${myData.id}] using [${myData.language}] for stage 2 task. ${myData.email}`;
 console.log(myDetails)
