d46a6f9b6a31d0e6efc609a9bb617b11e12585a0
NerdPraise <praiseajayi2@gmail.com>
2020-06-05 20:23:20 +0100

    new file

--- scripts/oduche.js
+++ scripts/oduche.js
@@ -3,7 +3,6 @@ let myData = {
     lastName:'OKerulu',
     language: 'Javascript',
     id: 'HNG-03508',
-    email: 'oduchep@gmail.com'
 }
 
-console.log(`Hello World, this is ${myData.firstName} ${myData.lastName} with HNGi7 ID ${myData.id} using ${myData.language} for stage 2 task. ${myData.email}`);
+console.log(`Hello World, this is [${myData.firstName}] [${myData.lastName}] with HNGi7 ID [${myData.id}] using [${myData.language}] for stage 2 task. oduchep@gmail.com`);
