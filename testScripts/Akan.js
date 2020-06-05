54a9808e80634d26a8e7536406b06ddf7eaa59bf
Uko Akanowo <ukoakanowo98@gmail.com>
2020-06-05 05:29:40 +0100

    chore: create script

--- testScripts/Akan.js
+++ testScripts/Akan.js
@@ -4,8 +4,7 @@
   const lastName = 'Uko';
   const hngID = 'HNG-05016';
   const lang = 'Javascript';
-  const email = 'ukoakanowo98@gmail.com';
   
   return console.log(`Hello World, this is ${[firstName]} ${[lastName]} 
-  with HNGi7 ID ${[hngID]} using ${[lang]} for stage 2 task. ${email}"`);
+  with HNGi7 ID ${[hngID]} using ${[lang]} for stage 2 task.`);
 }());
