3a30e8051d5df1a071c2bdb82be1f6d10b96d2d4
moodyshegs <61108798+moodyshegs@users.noreply.github.com>
2020-06-05 09:47:59 +0100

    Update moodyshegs.py

--- testScripts/moodyshegs.py
+++ testScripts/moodyshegs.py
@@ -1,11 +1,9 @@
-def board_hngi7_identification( myFirstName,myLastName,myHNGi7_ID,myLanguage,myEmail):
-  
-    
-    print("Hello World, this is " + myFirstName + ' ' + myLastName + ' with HNGi7 ID ' + myHNGi7_ID + ' using ' + myLanguage + ' as my coding language for stage 2 task. ' + myEmail)
-
+def board_hngi7_identification( myFirstName,myLastName,myHNGi7_ID,myLanguage,myEmail):
+    print("Hello World, this is " + myFirstName + ' ' + myLastName + ' with HNGi7 ID ' + myHNGi7_ID + ' using ' + myLanguage + ' for stage 2 task. ' + myEmail)
 board_hngi7_identification(
-    myFirstName = '[Segun]',
-    myLastName = '[Moody]',
-    myHNGi7_ID = '[HNG-03887]',
-    myLanguage = '[python]',
-    myEmail = 'moodysegun2020@gmail.com'
+    myFirstName = '[Segun]',
+    myLastName = '[Moody]',
+    myHNGi7_ID = '[HNG-03887]',
+    myLanguage = '[python]',
+    myEmail = 'moodysegun2020@gmail.com'
+)
