a74a46ab59de59c5e56a13077a18bb08933a54d7
Ntwariluc <ntwalinj@gmail.com>
2020-06-05 23:54:25 +0200

    chore: create script

---
+++ testScripts/luc.py
@@ -0,0 +1,6 @@
+firstName= "NTWARI NDAGIJIMANA"
+lastName= "Jean luc"
+id= "HNG-03452"
+language= "python"
+email= "ntwalinj@gmail.com"
+print("Hello World, this is"+ firstName + lastName +"withHNGi7 ID"+id + "using"+ language + "for stage 2 task." + email)
