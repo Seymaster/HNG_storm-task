97242126cf36bc53805f4f31f035bb3071bdb4bd
Diczen <josephingio@gmail.com>
2020-06-05 17:18:16 +0100

    chore: create script

---
+++ Joey.py
@@ -0,0 +1,11 @@
+#MY SOLUTION TO TASK 2 PROBLEM
+#def myProfile(): #function for carrying out the process
+def myProfile(Fname,Lname,Id,Language,EmailID):
+    print("Hello World, this is",Fname,Lname,"with HNGi7 ID",Id,"using", Language,"for stage 2 task.",EmailID)
+#Data for myProfile function
+myProfile(
+    Fname ="[Joseph]",
+    Lname = "[Ingio]",
+    Id = "[HNG-00320]",
+    Language = "[python]",
+    EmailID ="josephingio@gmail.com")
