13c841afb40fead7f27ecc5d91ee02b5d63dc45f
opadotun-taiwo <opadotunnnoluwaseun@yahoo.com>
2020-06-05 22:20:42 +0100

    opadotun-taiwo

---
+++ scripts/HNG-05374.js
@@ -0,0 +1,9 @@
+var info = {
+    firstname: "seun",
+    lastname: "opadotun",
+    Id: "[HNG-05374]",
+    programme: "[javascript]"
+}
+
+console.log("hello world " + "this is " + info.firstname + " " +  info.lastname +" " +
+ "with" +" "+ info.Id +" " + "using " +" "+ info.programme + " for stage 2 task " + "opadotunoluwaseun@yahoo.com")
