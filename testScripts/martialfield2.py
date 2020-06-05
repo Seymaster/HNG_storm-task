f3798bbfbf5b68ba36545cf31929271e6fc1895b
martialfield <66356840+martialfield@users.noreply.github.com>
2020-06-05 08:13:21 +0100

    updated using double quotation

--- testScripts/martialfield2.py
+++ testScripts/martialfield2.py
@@ -4,6 +4,6 @@ def details():
     hngi7_id = "HNG-06237"
     email_address = "kemy0217@gmail.com"
     language = "PYTHON3"
-    message = 'Hello World, this is [{}] [{}] with HNGi7 ID [{}] using [{}] for stage 2 task. {}' .format(first_name, last_name, hngi7_id, language, email_address)
+    message = "Hello World, this is [{}] [{}] with HNGi7 ID [{}] using [{}] for stage 2 task. {}" .format(first_name, last_name, hngi7_id, language, email_address)
     print(message)
 details()
