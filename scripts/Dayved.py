2686a1cfa57ed6abcf1bfd1668237d115f7abf64
NerdPraise <praiseajayi2@gmail.com>
2020-06-05 22:28:14 +0100

    add new scripts

--- scripts/Dayved.py
+++ scripts/Dayved.py
@@ -11,4 +11,4 @@ def  my_introduction(firstname, lastname,id, language):
 	language = '[python]'
 	print(f'Hello World, this is {firstname} {lastname} with HNGi7 ID {id} using {language} for stage 2 task. dayvedbrown257@gmail.com')
 #Now call your fxn
-my_introduction(firstname, lastname, id, language) 
+my_introduction(firstname, lastname, id, language)
--- scripts/__Charlespreshy__.py
+++ scripts/__Charlespreshy__.py
@@ -1,13 +1,13 @@
 
-first_name = "[Precious"
-last_name = "Charles]"
+first_name = "[Precious]"
+last_name = "[Charles]"
 hng_id = "[HNG-06536]"
 language = "[Python]"
 email = "princesspc16@yahoo.com"
 
 def intern_details():
-    print("Hello World, this is ", first_name, last_name ,"with HNGi7 ID ",hng_id, 'using ', language,
-          'for stage 2 task. ',email)
+    print("Hello World, this is", first_name, last_name ,"with HNGi7 ID",hng_id, 'using', language,
+          'for stage 2 task.',email)
 
 intern_details() 
 
