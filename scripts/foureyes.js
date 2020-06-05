007a96efb06b08e3b64e30df1dec91ae09bebd49
NerdPraise <praiseajayi2@gmail.com>
2020-06-05 20:57:41 +0100

    new scripts

--- scripts/foureyes.js
+++ scripts/foureyes.js
@@ -1,1 +1,9 @@
 
+var foureyes = {
+	firstName: "[Joseph]",
+	lastName: "[Morenike]",
+	email: "josephmorenike2@gmail.com",
+	language: "[Javascript]",
+	id: "[HNG-03821]",
+};
+console.log("Hello World, this is " + foureyes.firstName +" "+ foureyes.lastName+" with HNGi7 ID " + foureyes.id + " using " + foureyes.language +" for stage 2 task."+ " " +foureyes.email);
