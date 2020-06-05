bf5ee36b0236a97ba0a22bd8166ec99b00f279df
Brianorinah <Brayohorina@gmail.com>
2020-06-05 11:26:46 +0300

    Fixing of HNGi7 ID

--- testScripts/Brian_orinah.py
+++ testScripts/Brian_orinah.py
@@ -4,6 +4,6 @@ language = "Python"
 email ="brayohorina@gmail.com"
 
 def print_details(fname,lname,lang,email):
-    print(f"Hello World, [{fname}][{lname}] with HNG-0000 using [{language}] for stage 2 task. {email}")
+    print(f"Hello World, [{fname}][{lname}] with HNGi7 ID [HNG-00264] using [{language}] for stage 2 task. {email}")
 
 print_details(firstname,lastname,language,email)
