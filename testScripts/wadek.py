d48e27bbdb619c75f7c85840a2ed16a22852d009
Derrick Wadek <wadeq@Derricks-MacBook-Air.local>
2020-06-05 17:23:28 +0300

    chore: recreated script

--- testScripts/wadek.py
+++ testScripts/wadek.py
@@ -3,8 +3,8 @@ wadek_info = {
 	'fName': "[Derrick]",
 	'lName': "[Wadek]",
 	'id': "[HNG-00080]",
-	'email': "[derrickwadek@mail.com]",
+	'email': "derrickwadek@mail.com",
 	'language': "[Python]",
 }
-print("Hello world, this is {} {} with HNGi7 ID {} using {} for stage 2 task. {}.".format(
+print("Hello world, this is {} {} with HNGi7 ID {} using {} for stage 2 task. {}".format(
     wadek_info['fName'], wadek_info['lName'], wadek_info['id'], wadek_info['language'], wadek_info['email']))
