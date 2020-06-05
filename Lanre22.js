7325cb55cd9b07b8b66750497b8456c5c594f1b0
Adeniran02 <66222989+Adeniran02@users.noreply.github.com>
2020-06-05 08:34:31 +0100

    Update and rename scripts/Lanre22.js to Lanre22.js

---
+++ Lanre22.js
@@ -0,0 +1,11 @@
+const firstName = "Kehinde";
+const lastName = "Adeniran";
+const id = "HNG-02379";
+const language = "JavaScript";
+const emailAddress = "adeniranolanrewajuu@gmail.com";
+
+
+const stage2 = (firstName, lastName, id, language, emailAddress) => {
+    return `Hello World, this is ${firstName} ${lastName} with HNGi7 ID ${id} using ${language} for stage 2 task. ${emailAddress}`
+  };
+  console.log(stage2());
