dc479196629740ed41856d01d50ce80d5f4bfe13
Aitsebaomo jerry <jerrickson31@gmail.com>
2020-06-05 19:55:39 +0100

    chore: create script

---
+++ testScripts/Prizrak31.py
@@ -0,0 +1,11 @@
+internsprofile = {
+    "first_name": "Aitsebaomo",
+    "last_name": "Jerry",
+    "ID_no": "HNG-06657",
+    "language": "[python]",
+    "email": "jerrickson31@gmail.com",
+    }
+
+output = "Hello World, this is [{}] [{}] with HNGi7 ID [{}] using [{}] for stage 2 task. {}".format(internsprofile["first_name"], internsprofile["last_name"], internsprofile["ID_no"],internsprofile["language"], internsprofile["email"],)
+print(output)
+
