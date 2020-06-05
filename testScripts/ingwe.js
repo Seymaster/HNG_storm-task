e33966ffd22f8aed162799936c347ac219016f0f
Richard Ingwe <richardingwe5@gmail.com>
2020-06-05 09:19:01 +0100

    chore:create script

---
+++ testScripts/ingwe.js
@@ -0,0 +1,14 @@
+const intern = {
+	firstName : 'Richard',
+	lastName  : 'Ingwe',
+	hngi7_id  : 'HNG-01648',
+	lan       : 'JavaScript'
+};
+
+const outputFunction = () => {
+	const output = `Hello World, this is [${intern.firstName}] [${intern.lastName}] with HNGi7 ID [${intern.hngi7_id}] using [${intern.lan}] for stage 2 task. richardingwe5@gmail.com`;
+
+	console.log(output);
+};
+
+outputFunction();
