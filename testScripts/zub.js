bd3bd0042108e9f824c948ff1b638c1b85b7c8a5
zubeir <zubkayare@gmail.com>
2020-06-05 21:27:04 +0300

    chaging the document.write of the DOM to console.log

--- testScripts/zub.js
+++ testScripts/zub.js
@@ -1,1 +1,10 @@
-console.log("Hello World, this is [Zubeir Abubakar] with HNGi7 ID [HNG-00994] using [Javascript] for stage 2 task. zubkayare@gmail.com");
+function test2() {
+    var Firstname = "[Zubeir]";
+    var Lastname = "[Abubakar]";
+    var HNGID = "[HNG-00994]";
+    var language = "[javascript]";
+    console.log(
+      "Hello World, this is", Firstname,Lastname, "with HNGi7 ID",HNGID,"using", language,"for stage 2 task. zubkayare@gmail.com"
+    );
+  }
+  return test2();
