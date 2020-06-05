b2c52c3381fc2e92d78ed47c7b770cc3174bd016
Lucrownt <61760028+Lucrownt@users.noreply.github.com>
2020-06-05 15:20:03 +0100

    Add files via upload

---
+++ testScripts/lucrownt.py
@@ -0,0 +1,11 @@
+lucrownt_info = {
+    "fname": "Lukman",
+    "lname": "Adeoti",
+    "ID": "HNG-01721",
+    "lang": "python",
+    "email": "lukmanademolaadeoti@gmail.com",
+    }
+    
+
+text = "Hello World, this is [{}] [{}] with HNGi7 ID [{}] using [{}] for stage 2 task. {}".format(lucrownt_info["fname"], lucrownt_info["lname"], lucrownt_info["ID"], lucrownt_info["lang"], lucrownt_info["email"])
+print(text)
