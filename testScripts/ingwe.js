aaf4c15ccdce0b0311fcf84b7ede0da0c1b20a60
Richard Ingwe <richardingwe5@gmail.com>
2020-06-05 10:46:24 +0100

    chore: create script

--- testScripts/ingwe.js
+++ testScripts/ingwe.js
@@ -1,14 +1,15 @@
 const intern = {
-	firstName : 'Richard',
-	lastName  : 'Ingwe',
-	hngi7_id  : 'HNG-01648',
-	lan       : 'JavaScript'
+	firstName: "Richard",
+	hngi7_id: "HNG-01648",
+	lan: "JavaScript",
+	lastName: "Ingwe",
 };
 
 const outputFunction = () => {
 	const output = `Hello World, this is [${intern.firstName}] [${intern.lastName}] with HNGi7 ID [${intern.hngi7_id}] using [${intern.lan}] for stage 2 task. richardingwe5@gmail.com`;
-
 	console.log(output);
+	return output;
 };
 
 outputFunction();
+
