d6baab21043e8dff96c8fea95fed17146d224c28
weebj <edi-benjamin@hotmail.com>
2020-06-05 21:20:13 +0100

    feat: added weebj.py

---
+++ testScripts/weebj.py
@@ -0,0 +1,7 @@
+firstname = "Benjamin"
+lastname = "Edi"
+id = "HNG-00309"
+email = "edi-benjamin@hotmail.com"
+language = "Python"
+output = '"' + 'Hello world' + ',' + ' this is ' + firstname + ' ' + lastname + ' with HNGi7 ID ' + id + ' using ' + language + ' for stage 2 task.' + '"'
+print(output)
