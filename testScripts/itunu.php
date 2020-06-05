24c3a402041d38c04ec5abe642770b7839088b8b
Itunu Raimi <54324355+pelumy@users.noreply.github.com>
2020-06-05 11:01:25 +0100

    Update Itunu.php

--- testScripts/itunu.php
+++ testScripts/itunu.php
@@ -3,11 +3,11 @@
 $first_Name = "Itunu";
 $last_Name = "Raimi";
 
-$Id = "HNG-00735";
-$Email = "itunuraimi@yahoo.com";
-$language = "PHP";
+$myId = "HNG-00735";
+$myEmail = "itunuraimi@yahoo.com";
+$myLanguage = "PHP";
 
 
-echo " Hello World, this is [{$first_Name}] [{$last_Name}] with HNGi7 ID [{$Id}] using [{$language}] for stage 2 task. {$Email} " ;
+echo " Hello World, this is [{$first_Name}] [{$last_Name}] with HNGi7 ID [{$myId}] using [{$myLanguage}] for stage 2 task. {$myEmail} " ;
 
 ?>
