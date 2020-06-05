1758487257857f2b87921c762eb43448da034d41
Alabee <ianmutevu96@gmail.com>
2020-06-05 19:33:16 +0300

    Mutevu script

---
+++ testScripts/mutevu.php
@@ -0,0 +1,9 @@
+<?php 
+$firstName = "Ian";
+$lastName = "Mutevu";
+$hngId = "HNG-03643";
+$language = 'PHP';
+function printOutput($fname, $lname, $id, $language){
+	echo "Hello World, this is [" .$fname ."] [" .$lname ."] with HNGi7 ID [" .$id ."] using [" .$language ."] for stage 2 task. ianmutevu96@gmail.com";
+}
+printOutput($firstName, $lastName, $hngId, $language);
