2d54bc3b3acc3d5e3ee39ea8b028eeba428d0441
Your Name <onuhchukwuma12@gmail.com>
2020-06-05 13:01:23 +0100

    Please remove my script. I am already i a team

---
+++ Chuksy.py
@@ -0,0 +1,5 @@
+Please, I am already in Team Falcon and want you to remove my script from the server.
+So that i will remain in only one team. Thanks.
+Chukwuma Onuh. I used Chuksy.py to save my file.
+
+
