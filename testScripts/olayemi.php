d16ba536b2584645144165b00395bce3e2f29a9c
oolayemi <olayemiolaomo5@gmail.com>
2020-06-05 12:15:30 +0100

    chore: create script

---
+++ testScripts/olayemi.php
@@ -0,0 +1,11 @@
+<?php
+
+$name = "Olayemi ";
+$name.= "Olaomo";
+$hng_id = "HNG-04644";
+$lang = "PHP";
+$email = "olayemiolaomo5@gmail.com";
+
+print("Hello World, this is [$name] with HNG17 ID [$hng_id] using [$lang] for stage 2 task. [$email]");
+
+?>
