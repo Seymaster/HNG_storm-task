8f3d8af4686ba3646a91a84af3069d1003c2afe4
Oyinkansola <yahayaoyinkansola@gmail.com>
2020-06-05 09:01:32 +0100

    Stage 2 task

--- testScripts/oyinkan.js
+++ testScripts/oyinkan.js
@@ -1,13 +1,29 @@
+// function Output() {
+//   const Internship = {
+//     lastName: "yahaya",
+//     firstName: "oyinkansola",
+//     id: "HNG-01876",
+//     language: "javascript",
+//     email: "yahayaoyinkansola@gmail.com",
+//   };
+
+//   const text = `Hello World, this is ${Internship.lastName} ${Internship.firstName} with HNGi7 ID [${Internship.id}] with email [${Internship.email}] using [${Internship.language}] for stage two task.`;
+
+//   return text;
+// }
+
+// console.log(Output());
+
 function Output() {
   const Internship = {
-    lastName: "yahaya",
-    firstName: "oyinkansola",
+    firstName: "Oyinkansola",
+    lastName: "Yahaya",
     id: "HNG-01876",
     language: "javascript",
     email: "yahayaoyinkansola@gmail.com",
   };
 
-  const text = `Hello World, this is ${Internship.lastName} ${Internship.firstName} with HNGi7 ID [${Internship.id}] with email [${Internship.email}] using [${Internship.language}] for stage two task.`;
+  const text = `Hello World, this is [${Internship.firstName}] [${Internship.lastName}] with HNGi7 ID [${Internship.id}] using [${Internship.language}] for stage 2 task. yahayaoyinkansola@gmail.com`;
 
   return text;
 }
