e96261651859625715ba21cee6e8923dc144c33b
matthew <matthewigbede@gmail.com>
2020-06-05 17:01:21 +0100

    chore: create script

---
+++ testScripts/matthew.js
@@ -0,0 +1,15 @@
+const matthew_details = {
+    firstName: "Matthew",
+    lastName: "Igbede",
+    id: "HNG-03082",
+    email: "matthewigbede@gmail.com",
+    language: "Javascript",
+
+};
+
+const output = `Hello World, this is [${matthew_details.firstName}] [${matthew_details.lastName}] with HNGi7 [${matthew_details.id}] using [${matthew_details.language}] for stage 2 task. [${matthew_details.email}]`;
+
+console.log(output);
+
+
+
