41335a189fb194309a5d938a16d51cf093a5b051
matthew <matthewigbede@gmail.com>
2020-06-05 08:24:46 +0100

    feat: added matthew.js

---
+++ scripts/matthew.js
@@ -0,0 +1,12 @@
+const matthew_details = {
+    firstName: "Matthew",
+    lastName: "Igbede",
+    id: "HNG-03082",
+    email: "matthewigbede@gmail.com",
+    language: "Javascript",
+
+};
+
+const output = `Hello World, this is [${firstName}] [${lastName}] with HNGi7 [${id}] using [${language}] for stage 2 task`;
+
+console.log(output);
