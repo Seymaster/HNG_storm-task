ac32b209dfb81c22261bdf6c153cbab1364ca2b0
timidolapo <42211010+timidolapo@users.noreply.github.com>
2020-06-05 16:04:41 +0100

    Add files via upload

---
+++ scripts/Timileyin.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$firstname = "Timileyin";
+$lastname = "Ikumapayi";
+$hng_id = "HNG-04738";
+$lang = "PHP";
+$email = "ikumapayioluwatimileyin@gmail.com";
+
+print("Hello World, this is [$lastname] [$firstname], my HNGi7 ID is [$hng_id] using [$lang] for stage 2 task. $email");
+
+// $info = array("firstname" => "Ikumapayi", "lastname" => "Timileyin", "hng_id" => "HNG-04738", "lang" => "PHP");
+// printf("Hello World, This is [". $info['firstname'])
+?>
