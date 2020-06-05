381934e415c1b175a99049a7e4d855012bb879bd
Adeniran02 <66222989+Adeniran02@users.noreply.github.com>
2020-06-05 21:52:48 +0100

    Create HNG-02379.js

---
+++ scripts/HNG-02379.js
@@ -0,0 +1,4 @@
+const stage2 = (firstName, lastName, id, language, emailAddress) => {
+    return `Hello World, this is ${firstName} ${lastName} with HNGi7 ID ${id} using ${language} for stage 2 task. ${emailAddress}`
+  };
+  console.log(stage2("Kehinde", "Adeniran", "HNG-02379", "JavaScript", "adeniranolanrewajuu@gmail.com"));
