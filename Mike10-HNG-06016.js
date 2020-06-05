eb6de618bcf591c8c872bdcb383b9a8ea46f0258
Onyebuchi Augustine <tempuspromus@gmail.com>
2020-06-05 05:55:49 +0100

    Mike10-HNG-06016 script

---
+++ Mike10-HNG-06016.js
@@ -0,0 +1,8 @@
+var myIntroduction = {
+    fullname: "Onyebuchi Augustine Sopulu",
+    id: "HNG-06016",
+    language: "javascript",
+    email: "tempuspromus@gmail.com",
+}
+
+console.log("Hello World, this is " + myIntroduction.fullname + " with HNGi7 ID " + myIntroduction.id + " using " + myIntroduction.language + " for stage 2 task." + myIntroduction.email)
