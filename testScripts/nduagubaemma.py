aab831096b7cd02791dfec95d5da390d32ee3ae9
kc emma <nduagubaemma@gmail.com>
2020-06-05 05:40:13 +0100

    my code kc emma

---
+++ testScripts/nduagubaemma.py
@@ -0,0 +1,18 @@
+
+"""
+This is the second task of our HNG intenship program. we are expected to output our name, id and language
+"""
+
+
+def code():
+
+    firstName = "[Emmanuel]"
+    lastName = "[Nduaguba]"
+    id = "[HNG-01522]"
+    email = "nduagubaemma@gmail.com"
+    language = "[Python]"
+    print("Hello World, this is ", firstName ," " ,lastName ," with HNGi7 ID ",  id, " using ",  language, " for stage 2 task. nduagubaemma@gmail.com@gmail.com")
+
+code()
+#print(type('language'))
+nnmn
