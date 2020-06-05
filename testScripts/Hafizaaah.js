ae66c0fac4114d68a32f5ecbeca8756ca9b91d91
Hafizah Muhyideen <40233874+justfizah@users.noreply.github.com>
2020-06-05 06:20:43 +0100

    Update

--- testScripts/Hafizaaah.js
+++ testScripts/Hafizaaah.js
@@ -1,10 +1,10 @@
-Const firstName = "Hafizah";
-Const lastName = "Muhyideen";
-Const id = "HNG-02396";
-Const lang = "JavaScript";
-Const email = "hafeexahabdul@gmail.com";
+const firstName = "Hafizah";
+const lastName = "Muhyideen";
+const id = "HNG-02396";
+const lang = "JavaScript";
+const email = "hafeexahabdul@gmail.com";
 
-Const myDetailsData = {
+const myDetailsData = {
     firstName,
     lastName,
     lang,
@@ -12,14 +12,7 @@ Const myDetailsData = {
     email,
 };
 
-Const output = ({
-        firstName,
-        lastName,
-        lang,
-        id,
-        email
-    }) =>
-    `Hello World, this is [${firstName}] [${lastName}] with HNGi7 ID [${id}] using [${lang}] for stage 2 task. ${email}`;
+const output =  `Hello World, this is [${firstName}] [${lastName}] with HNGi7 ID [${id}] using [${lang}] for stage 2 task. ${email}`;
 
 
-console.log(output(myDetailsData));
+console.log(output);
