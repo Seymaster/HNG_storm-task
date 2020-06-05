b25e0fffb2652f073bf5352d7e8ebf8e7099e39b
NerdPraise <praiseajayi2@gmail.com>
2020-06-05 21:04:07 +0100

    new scripts

--- scripts/__Charlespreshy__.py
+++ scripts/__Charlespreshy__.py
@@ -6,8 +6,8 @@ language = "[Python]"
 email = "princesspc16@yahoo.com"
 
 def intern_details():
-    print("Hello World, this is ", first_name, last_name ,",with HNGi7 ID ",hng_id, 'using ', language,
-          ' for stage 2 task. ',email)
+    print("Hello World, this is ", first_name, last_name ,"with HNGi7 ID ",hng_id, 'using ', language,
+          'for stage 2 task. ',email)
 
 intern_details() 
 
