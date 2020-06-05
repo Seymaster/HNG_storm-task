ac570f700ad80ca85b0e569c37bed459e5141bfa
OrLAN-K <55242168+OrLAN-K@users.noreply.github.com>
2020-06-05 21:11:13 +0100

    Create orlan-k.js

---
+++ testScripts/orlan-k.js
@@ -0,0 +1,9 @@
+const myData = {
+	fName: "ZUBAIR",
+	lName: "OLANREWAJU",
+	id: "HNG-05087",
+	email: "olanrkayus@gmail.com",
+	language: "javaScript",
+}
+const myDetails = `Hello world, this is [${myData.fName}] [${myData.lName}] with HNGi7 ID [${myData.id}] using [${myData.language}] for stage 2 task. ${myData.email}`;
+console.log(myDetails)
