1680e75df44de90456d60f28892416320f7c988f
Oyinkansola <yahayaoyinkansola@gmail.com>
2020-06-05 09:05:50 +0100

    Stage 2 task

--- testScripts/oyinkan.js
+++ testScripts/oyinkan.js
@@ -1,19 +1,3 @@
-// function Output() {
-//   const Internship = {
-//     lastName: "yahaya",
-//     firstName: "oyinkansola",
-//     id: "HNG-01876",
-//     language: "javascript",
-//     email: "yahayaoyinkansola@gmail.com",
-//   };
-
-//   const text = `Hello World, this is ${Internship.lastName} ${Internship.firstName} with HNGi7 ID [${Internship.id}] with email [${Internship.email}] using [${Internship.language}] for stage two task.`;
-
-//   return text;
-// }
-
-// console.log(Output());
-
 function Output() {
   const Internship = {
     firstName: "Oyinkansola",
