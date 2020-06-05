7fe93b218a0e6156ade4689d8ec5a29eadd08f43
usmanajibola <usmanajibolaabass@gmail.com>
2020-06-05 09:50:08 -0700

    chore: create script

--- testScripts/Usmanajibola1.py
+++ testScripts/Usmanajibola1.py
@@ -1,11 +1,9 @@
 def task():
-    first_name = "Usman"
-    last_name = "Abass"
+    first_name = "[Usman]"
+    last_name = "[Abass]"
     email = "usmanajibolaabass@gmail.com"
-    my_id = "HNG-05550"
-    language= "python"
-
-    reply = "Hello World, this is" + str([first_name]) + str([last_name]) +  "with HNGi7 ID" + str([my_id]) + "using" + str([language]) + "for stage 2 task." + str(email) 
+    my_id = "[HNG-05550]"
+    language= "[python]"
+    reply = "Hello World, this is " + first_name + " " + last_name +  " with HNGi7 ID " + my_id + " using " + language + " for stage 2 task. " +email 
     return reply
-
-
+print(task())
