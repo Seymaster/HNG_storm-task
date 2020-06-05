c15aeb9382096a0fb469bdbb29d7ac68f71133c2
Emilsone <lawsonezekiel68@gmail.com>
2020-06-05 20:54:33 +0100

    made some changes to my code

---
+++ testScripts/emilson.js
@@ -0,0 +1,18 @@
+const firstName = ["Ezekiel"];
+const lastName = ["Lawson"];
+const identity = ["HNG-01760"];
+const language = ["JavaScript"];
+const email = ["lawsonezekiel68@gmail.com"];
+
+const myDataInfo = {
+    firstName,
+    lastName,
+    language,
+    identity,
+    email,
+};
+
+const output =  `Hello World, this is [${firstName}] [${lastName}] with HNGi7 ID [${identity}] using [${language}] for stage 2 task. ${email}`;
+
+
+console.log(output);
