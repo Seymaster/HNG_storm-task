e96ac7c3da0ba3fc08c233aeaecdea757670d4d1
Aitsebaomo jerry <jerrickson31@gmail.com>
2020-06-05 12:39:37 +0100

    chore: create script

---
+++ testScripts/Prizrak.py
@@ -0,0 +1,11 @@
+
+# Task 2
+def interns_profile():
+    first_name = "[Aitsebaomo]"
+    last_name = "[Jerry]"
+    ID_no = "[HNG-06657]"
+    lang = "[python]"
+    email = "jerrickson31@gmail.com"
+    print("Hello World, this is {} {} with HNGi7 ID {} using {} for stage 2 task. {}".format(first_name, last_name, ID_no,lang, email))
+
+interns_profile()
