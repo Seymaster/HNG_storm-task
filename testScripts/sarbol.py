0a16eef9be90c7b200e87d00a42127465dd33294
sarbol <47760610+sarbol@users.noreply.github.com>
2020-06-05 17:04:17 +0100

    Sarbol branch (#609)
    wrote few lines of code to introduce myself to the team

---
+++ testScripts/sarbol.py
@@ -0,0 +1,4 @@
+def introduce_me_to_team_storm():
+    message='Hello World, this is [Saburi] [Yusuf] with HNGi7 ID [HNG-05682] using [Python] for stage 2 task. sarboldipo80@gmail.com'
+    return message
+print(introduce_me_to_team_storm())
