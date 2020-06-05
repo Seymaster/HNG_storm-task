1ccb55f7463cf3af9867a43b2de12b2752de461f
NerdPraise <praiseajayi2@gmail.com>
2020-06-05 21:23:33 +0100

    add new scripts

--- scripts/Nkebo2020.php
+++ scripts/Nkebo2020.php
@@ -1,11 +1,7 @@
-
-        <?php
-
-             $variable = "Hello World, this is Nkechi Ebogha with HNGi7 ID HNG-06074
-             using PHP for stage 2 task";
-
-             echo $variable;
-        ?>
-     
-
-
+<?php
+​
+     $variable = "Hello World, this is [Nkechi Ebogha] with HNGi7 ID [HNG-06074]
+     using [PHP]for stage 2 task. favourebogha@gmail.com";
+​
+     echo $variable;
+?>
