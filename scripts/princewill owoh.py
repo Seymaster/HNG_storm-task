5714accf2197d57a652fcf9e191827fbab307874
owoh princewill <princewillowoh@gmail.com>
2020-06-05 20:02:47 +0100

    updated princewillowoh.py

--- scripts/princewill owoh.py
+++ scripts/princewill owoh.py
@@ -1,12 +1,12 @@
 greeting = "Hello world"
-first_name = "princewill"
-last_name = "owoh"
+first_name = "Princewill"
+last_name = "Owoh"
 HNGi7_ID = "HNG-04232"
 language = "Python"
 
 
 def script():
-    result = f'{greeting}, this is {first_name} {last_name} with HNGi7 ID {HNGi7_ID} using {language} for stage2 task'
+    result = f'{greeting}, this is {first_name} {last_name} with HNGi7 ID {HNGi7_ID} using {language} for stage 2 task'
     return result
 
 
