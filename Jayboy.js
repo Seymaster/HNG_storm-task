fe4752750ff1d053e82b5b6a008be6e2a1abe200
Jayboy921 <66254456+Jayboy921@users.noreply.github.com>
2020-06-05 02:46:55 -0700

    added MY_SCRIPT

---
+++ Jayboy.js
@@ -0,0 +1,8 @@
+var myInfoSubmite = {
+    fullName: "[Jehoshaphat] [Ugwu] [Obioma]",
+    id: "[HNG-03637]",
+    language: "[Javascript]",
+    email: "ugwuezeobioma@gmail.com",
+};
+const myInfo = `Hello World, this is ${Info.fullName} with HNGi7 ID ${Info.id} using ${Info.language} for stage 2 task. ${Info.email}`;
+console.log(myInfoSubmite);
