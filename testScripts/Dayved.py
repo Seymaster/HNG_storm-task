db8044abd4d5bb7bdd76b7dc47d5b3c6b1e71f3f
David Brown <66221305+David-Brown01@users.noreply.github.com>
2020-06-05 21:55:15 +0100

    Update Dayved.py

--- testScripts/Dayved.py
+++ testScripts/Dayved.py
@@ -1,14 +1,9 @@
-firstname = ()
-lastname = ()
-id = ()
-language =()
+import json
+firstname = "David"
+lastname = "Brown"
+hngid = "HNG -03338"
+language = "Python"
 
-def  my_introduction(firstname, lastname,id, language):
-	
-	firstname = '[David]'
-	lastname = '[Brown]'
-	id = '[HNG-03338]'
-	language = '[python]'
-	print(f'Hello World, this is {firstname} {lastname} with HNGi7 ID {id} using {language} for stage 2 task. dayvedbrown257@gmail.com')
-#Now call your fxn
-my_introduction(firstname, lastname, id, language) 
+output = "Hello World, this is [" + firstname + "] [" + lastname + "] with HNGi7 ID [" + hngid + " using [" + language + "] for stage 2 task. dayvedbrown257@gmail.com"
+
+print(json.dumps(output))
