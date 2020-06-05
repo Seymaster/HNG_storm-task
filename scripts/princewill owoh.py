f18ea6936890ea76941a252d569d2a86f444320e
owoh princewill <princewillowoh@gmail.com>
2020-06-05 19:56:56 +0100

    added princewillowoh.py

---
+++ scripts/princewill owoh.py
@@ -0,0 +1,13 @@
+greeting = "Hello world"
+first_name = "princewill"
+last_name = "owoh"
+HNGi7_ID = "HNG-04232"
+language = "Python"
+
+
+def script():
+    result = f'{greeting}, this is {first_name} {last_name} with HNGi7 ID {HNGi7_ID} using {language} for stage2 task'
+    return result
+
+
+print(script())
