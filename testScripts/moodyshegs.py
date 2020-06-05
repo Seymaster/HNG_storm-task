82ef0d599dfba0e36c83348207f8f76cb21c7088
moodyshegs <61108798+moodyshegs@users.noreply.github.com>
2020-06-05 11:01:38 +0100

    Moodyshegs.py
    Hello me merge the new update please.

--- testScripts/moodyshegs.py
+++ testScripts/moodyshegs.py
@@ -1,8 +1,5 @@
 def board_hngi7_identification( myFirstName,myLastName,myHNGi7_ID,myLanguage,myEmail):
-  
-    
-    print('Hello World, this is',myFirstName,myLastName,'with HNGi7 ID',myHNGi7_ID,'using',myLanguage,'as my coding language for stage 2 task.',myEmail)
-
+    print("Hello World, this is " + myFirstName + ' ' + myLastName + ' with HNGi7 ID ' + myHNGi7_ID + ' using ' + myLanguage + ' for stage 2 task. ' + myEmail)
 board_hngi7_identification(
     myFirstName = '[Segun]',
     myLastName = '[Moody]',
