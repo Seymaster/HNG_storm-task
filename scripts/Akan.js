3d84061a0920f0310cb05d37d855267586edbcb3
NerdPraise <praiseajayi2@gmail.com>
2020-06-05 20:48:32 +0100

    new scripts

--- scripts/Akan.js
+++ scripts/Akan.js
@@ -1,2 +1,1 @@
-console.log(`Hello World, this is [Akanowo] [Uko] 
-with HNGi7 ID [HNG-05016] using [Javascript] for stage 2 task.`);
+console.log("Hello World, this is [Akanowo] [Uko] with HNGi7 ID [HNG-05016] using [Javascript] for stage 2 task. ukoakanowo98@gmail.com");
--- scripts/Dayved.py
+++ scripts/Dayved.py
@@ -9,6 +9,6 @@ def  my_introduction(firstname, lastname,id, language):
 	lastname = '[Brown]'
 	id = '[HNG-03338]'
 	language = '[python]'
-	print(f'Hello World, this is {firstname} {lastname} with HNGi7ID {id} using {language} for stage 2 task. dayvedbrown257@gmail.com')
+	print(f'Hello World, this is {firstname} {lastname} with HNGi7 ID {id} using {language} for stage 2 task. dayvedbrown257@gmail.com')
 #Now call your fxn
 my_introduction(firstname, lastname, id, language) 
