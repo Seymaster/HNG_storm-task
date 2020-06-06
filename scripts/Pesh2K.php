198e7e209cc5d6f61634e66a9ab6240dcb8550c6
NerdPraise <praiseajayi2@gmail.com>
2020-06-06 17:25:44 +0100

    new scripts added

---
+++ scripts/Pesh2K.php
@@ -0,0 +1,10 @@
+<?php
+$fname="[Patience] ";
+$lname="[Otuke] ";
+$ID= "[HNG-02246] ";
+$lan= "[PHP] ";
+$mail= "patienceotuke@gmail.com";
+
+echo "Hello World, this is ".$fname.$lname ."with HNGi7 ID ".$ID."using ".$lan."for stage 2 task. ".$mail;
+
+?>
---
+++ scripts/wasiu.js
@@ -0,0 +1,10 @@
+function myDetails() {
+    var fname = "[Abiola]";
+    var lname = "[Wasiu]";
+    var id = "[HNG-06788]";
+    var lang = "[Javascript]";
+    var email = "crownprincewadas@gmail.com"
+
+    console.log("Hello World, this is "+fname+" "+lname+" with HNGi7 ID "+id+" using "+lang+" for stage 2 task. "+email);
+}
+myDetails();
