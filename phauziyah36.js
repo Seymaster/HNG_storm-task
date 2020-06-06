5be265951f5bdf1f07a94e62590b86bb7400ce8c
Ntwariluc <51233114+Ntwariluc@users.noreply.github.com>
2020-06-06 12:38:23 +0200

    Delete phauziyah36.js

--- phauziyah36.js
+++
@@ -1,5 +0,0 @@
-function profile(firstname,lastname,id,language)
-    {
-        console.log( "Hello World, this is ["+firstname+"] ["+lastname+"] with HNGi7 ID<< ["+id+"] using ["+language+"] for stage 2 task. kennyanis36@gmail.com")
-    }
-       profile("Kehinde","Gbagba","HNG-02421","JavaScript" );
