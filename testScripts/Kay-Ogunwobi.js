62b4fe90e679b286b32056efa201e710ac32b315
kayogunwobi <62080242+kayogunwobi@users.noreply.github.com>
2020-06-06 23:21:50 +0100

    Delete Kay-Ogunwobi.js

--- testScripts/Kay-Ogunwobi.js
+++
@@ -1,8 +0,0 @@
-var person = {
-    name: 'Isaiah Oluwakayode Ogunwobi',
-    id: 'HNG-01733',
-    email: 'kayogunwobi@gmail.com',
-    language: 'Javascript'
-};
-
-console.log(`Hello World, this is ${person.name} with HNGi7 ID ${person.id} and email ${person.email} using ${person.language} for stage 2 task`);
