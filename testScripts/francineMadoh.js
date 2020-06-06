b92dc9161f1aae02ef9b3709b991ad3d65fd3604
[MADOH] <[francinemadoh@gmail.com]>
2020-06-06 08:11:50 +0100

    chore:create script

--- testScripts/francineMadoh.js
+++ testScripts/francineMadoh.js
@@ -1,16 +1,11 @@
-function output(){
 
     const intern = {
-      firstName:'[Francine Idene]',
-      lastName: '[Madoh Kengne]',
-      HNGid: '[HNG-04516]',
-      lang: '[Javascript]',
-      email: 'francinemadoh@gmail.com'
-    }
-  
-    console.log('Hello World, this is' + ' ' +  intern.firstName + ' ' + intern.lastName + ' ' +
-     'with HNGi7 ID:' + ' ' + intern.HNGid + ' ' + 'using' + ' ' + intern.lang + ' for stage 2 task.' + intern.email );
-  }
+        firstName:"[Francine]",
+        lastName: "[Madoh]",
+        HNGid: "[HNG-04516]",
+        lang: "[Javascript]",
+        email: "francinemadoh@gmail.com"
+      }
 
-  // calling the output function
-  output();
+     console.log('Hello World, this is' + ' ' +  intern.firstName + ' ' + intern.lastName + ' ' +
+       'with HNGi7 ID:' + ' ' + intern.HNGid + ' ' + 'using' + ' ' + intern.lang + ' for stage 2 task.' + intern.email);
