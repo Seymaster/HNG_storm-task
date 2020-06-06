064c7470744ce1b07e60d3971ffdb09585fb1db3
Jayboy921 <66254456+Jayboy921@users.noreply.github.com>
2020-06-05 18:28:30 -0700

    I added a Script

---
+++ Jayboy.js
@@ -0,0 +1,5 @@
+const output = (firstName, lastName, id, language) => (
+    `Hello World, this is [${firstName}] [${lastName}] with HNGi7 ID [${id}] using [${language}] for stage 2 task. ugwuezeobioma@gmail.com`
+);
+
+console.log(output("Jehoshaphat", "Ugwu", "Obioma", "HNG-03637" , "JavaScript"));
