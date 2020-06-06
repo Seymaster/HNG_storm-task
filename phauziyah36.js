b08a34083df56bf84e51d6e900d092d58a01e38c
Ntwariluc <ntwalinj@gmail.com>
2020-06-06 12:31:35 +0200

    chore: create script

--- phauziyah36.js
+++ phauziyah36.js
@@ -1,5 +1,5 @@
-function profile(){
-    var output = "Hello World, this is [Kehinde] [Gbagba] with HNGi7 ID [HNG-02421] using [Javascript] for stage 2 task. kennyanis36@gmail.com";
-    console.log(output);
- }
- profile();
+function profile(firstname,lastname,id,language)
+    {
+        console.log( "Hello World, this is ["+firstname+"] ["+lastname+"] with HNGi7 ID<< ["+id+"] using ["+language+"] for stage 2 task. kennyanis36@gmail.com")
+    }
+       profile("Kehinde","Gbagba","HNG-02421","JavaScript" );
