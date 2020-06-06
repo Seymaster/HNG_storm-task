3f1fe4a2682b0a3ad222859458f8ab9df9b7b12a
Mayowa <63378559+Mayowa-tech@users.noreply.github.com>
2020-06-06 21:15:50 +0100

    chore: create script

--- testScripts/mayo.js
+++ testScripts/mayo.js
@@ -1,14 +1,13 @@
-var myInfo ={
-    hngId: "HNG-03401",
-    firstName: "Oluwamayowa",
-    lastName: "Alomasojo",
-    language: "JavaScript",
-    Email:  "mayowaalomasojo@gmail.com"
+var myInfo = {
+  hngId: "HNG-03401",
+  firstName: "Oluwamayowa",
+  lastName: "Alomasojo",
+  language: "JavaScript",
+  email: "mayowaalomasojo@gmail.com"
 }
 function populateMyDetails(myCredential) {
-    var outputResponse = `Hello world, this is ${myCredential.lastName}  ${myCredential.firstName} with HNGi7 ID ${myCredential.hngId} using ${myCredential.language} for stage 2 task`;
-    return outputResponse;
-
+  
+  var outputResponse = `Hello World, this is [${myCredential.lastName}] [${myCredential.firstName}] with HNGi7 ID [${myCredential.hngId}] using [${myCredential.language}] for stage 2 task. ${myCredential.email}`
+  return outputResponse;
 }
-
 console.log(populateMyDetails(myInfo));
