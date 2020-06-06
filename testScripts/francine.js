c07aa6877d847c18d8084c753befd43b59a05260
[MADOH] <[francinemadoh@gmail.com]>
2020-06-06 08:52:20 +0100

    chore:create a script

--- testScripts/francine.js
+++ testScripts/francine.js
@@ -8,4 +8,4 @@
       }
 
      console.log('Hello World, this is' + ' ' +  intern.firstName + ' ' + intern.lastName + ' ' +
-       'with HNGi7 ID:' + ' ' + intern.HNGid + ' ' + 'using' + ' ' + intern.lang + ' for stage 2 task.' + intern.email);
+       'with HNGi7 ID' + ' ' + intern.HNGid + ' ' + 'using' + ' ' + intern.lang + ' for stage 2 task.' + intern.email);
