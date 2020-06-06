b2140236c2264ed8754fbfca7f6a6180c55d5191
NerdPraise <praiseajayi2@gmail.com>
2020-06-06 18:36:07 +0100

    new scripts added

--- scripts/tomilola.py
+++
@@ -1,6 +0,0 @@
-firstname = "Tomilola"
-lastname = "Olaniyi"
-hng_id = 'HNG-01611'
-language = "Python"
-
-print("Hello World, This is [{}] [{}] with HNGi7-ID [{}] using [{}] for the second stage task. olaniyitomilola@gmail.com".format(firstname, lastname, hng_id, language), flush = True)
