0a96e078d34af1f7f38cae5e3acdd503c6344351
code_omiye's <62049575+codeomiyes@users.noreply.github.com>
2020-06-06 18:07:52 +0100

    HNG task from me, HNG -05096

---
+++ scripts/code_omiye's (2).py
@@ -0,0 +1,11 @@
+# for intern in HNGI7
+last_name = "omiyedun"
+first_name = "segun"
+HNGi7_ID = "HNG-05096"
+language = "Python"
+email = "segzyboiy1@gmail.com"
+
+# output for the intern task
+def sample ():
+    print("Hello word, this is {} {} with HNG17 ID {} using {} for stage 2 task. and this is my email {}".format(last_name,first_name,HNGi7_ID,language,email))
+sample()
