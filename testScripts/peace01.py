bc026daa2270ee12ae6ea3f974f75a48be5b7272
Peace-01 <66219454+Peace-01@users.noreply.github.com>
2020-06-07 00:29:15 +0100

    peace detail (#958)
    approved

---
+++ testScripts/peace01.py
@@ -0,0 +1,5 @@
+FirstN = '[Peace] '
+Lastn = '[Okey]'
+Id = '[HNG-00917]'
+language = '[python]'
+print('Hello world, this is '+FirstN+Lastn +' with HNGi7 ID '+Id+ ' using '+language+ ' for stage 2 task. peaceodunze@gmail.com')
