54c14bdef29846e201d5ab9a6bc3327c2211d793
peeterx <peterxnjoroge@gmail.com>
2020-06-06 21:34:26 +0300

    chore: create script

---
+++ scripts/peeterx.js
@@ -0,0 +1,11 @@
+const peeterx = {
+  firstName: '[Peter]',
+  lastName: '[Njoroge]',
+  id: '[HNG-03449]',
+  language: '[JavaScript]',
+  email: 'peterxnjoroge@gmail.com',
+}
+
+let output = "Hello World, this is " + peeterx.firstName + " " + peeterx.lastName + " with HNGi7 ID " + peeterx.id + " using " + peeterx.language + " for stage 2 task";
+
+console.log(output);
