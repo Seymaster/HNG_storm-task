99323365588c1e8220e0af5f1e4668bb76290c08
Pepzini <52154339+Pepzini@users.noreply.github.com>
2020-06-06 10:27:07 +0100

    --

--- testScripts/Nini.js
+++ testScripts/Nini.js
@@ -1,8 +1,8 @@
-let firstName= 'Olajide';
-let lastName='Niniola';
+let firstName = 'Olajide';
+let lastName = 'Niniola';
 let id = 'HNG-01615';
 let lang = 'JavaScript';
-let email= 'jideniniola@gmail.com' ;
+let email = 'jideniniola@gmail.com';
 console.log(`Hello World, this is [${firstName}] [${lastName}] with HNGi7 ID [${id}] using [${lang}] for stage 2 task. ${email}`);
 
 
