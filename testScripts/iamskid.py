ee096569f18bd8c1d2c1a883737da9f2c0e1250e
obinna nwaneri <cutechybyke01@gmail.com>
2020-06-06 00:23:16 -0700

    chore: createscript

---
+++ testScripts/iamskid.py
@@ -0,0 +1,8 @@
+
+first_name = "obinna"
+last_name = "nwaneri"
+Email ="cutechybyke01@gmail.com"
+id_number = "HNG-05627"
+language = "Python"
+
+print("Hello World, This is [obinna] [nwaneri] with HNGi7 ID [HNG-05627] using [Pythton] for stage 2 task. cutechybyke01@gmail.com")
