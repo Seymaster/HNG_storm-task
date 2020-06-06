e766017e9d662b3d81bed3392367c469aafdcdc0
udofiaofonime <66363301+udofiaofonime@users.noreply.github.com>
2020-06-06 08:45:55 +0000

    Update udofiaofonime.js

--- udofiaofonime.js
+++ udofiaofonime.js
@@ -2,4 +2,4 @@ let name = "Ofonime Udofia";
 let email = "udofiaofonime@gmail.com";
 let id = "HNG-06339";
 let language = "JavaScript";
-console.log("Hello World, this is " + name + " with HNGi7 ID " + id + " and email " + email + " using " + language + " for stage 2 task.");
+console.log("Hello World, this is " + name + " with HNGi7 ID " + id +  " using " + language + " for stage 2 task.");
