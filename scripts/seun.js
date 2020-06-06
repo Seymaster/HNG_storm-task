273a82b080b0b2c024b90155022dbc551bff27e8
opadotun-taiwo <opadotunnnoluwaseun@yahoo.com>
2020-06-06 16:01:15 +0100

    have tested it with repit

---
+++ scripts/seun.js
@@ -0,0 +1,11 @@
+function viewDetails() {
+    var details = {
+      firstname: "[Taiwo]",
+      lastname: "[Opadotun]",
+      hngid: "[HNG-05374]",
+      language: "[javascript]",
+      email: "opadotunoluwaseun@yahoo.com"
+    };
+  console.log("Hello World, this is "+details.firstname+" "+details.lastname+" with HNGi7 ID "+details.hngid+" using "+details.language+" for stage 2 task. "+details.email);
+  }
+  viewDetails();
