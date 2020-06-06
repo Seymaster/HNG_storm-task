e02dbf792178aaf9b6394f5bfad27daede050fb8
dharnihell <56403457+dharnihell@users.noreply.github.com>
2020-06-06 08:10:38 +0100

    dharnzkraft.js

---
+++ testScripts/dharnzkraft.js
@@ -0,0 +1,10 @@
+
+  const entry = {
+    firstname: "[Adeyinka]",
+    lastname: "[Daniel]",
+    language: "[Javascript]",
+    id: "[HNG-01589]",
+    email: "adeyinkadaniel09@gmail.com"
+  };
+
+console.log("Hello world, this is " + entry.firstname + " " + entry.lastname + " with HNGi7 ID " + entry.id + " using " + entry.language + " for stage 2 task." + " " + entry.email); 
