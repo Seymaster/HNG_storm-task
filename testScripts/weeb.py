c6496e927775b2484c3175ba805a746b17f5f314
weebj <edi-benjamin@hotmail.com>
2020-06-06 19:31:12 +0100

    feat: added weeb.py

---
+++ testScripts/weeb.py
@@ -0,0 +1,7 @@
+firstname = "[Benjamin]"
+lastname = "[Edi]"
+id = "[HNG-00309]"
+email = "edi-benjamin@hotmail.com"
+language = "[Python]"
+output = 'Hello World' + ',' + ' this is ' + firstname + ' ' + lastname + ' with HNGi7 ID ' + id + ' using ' + language + ' for stage 2 task.' + ' ' + email
+print(output)
