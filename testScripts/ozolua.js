4c730669a56b40dd051dfb8ad89fdd074826a64e
Calvinoea <39657008+Calvinoea@users.noreply.github.com>
2020-06-06 16:38:30 +0100

    symbol in name removed to pass test

---
+++ testScripts/ozolua.js
@@ -0,0 +1,11 @@
+function sentence(firstname,lastname, id,language){
+
+   console.log(`Hello World, this is [${firstname}] [${lastname}] with HNGi7 ID [${id}] using [${language}] for stage 2 task. calvinebunamu@gmail.com`)
+  
+  return `Hello World, this is [${firstname}] [${lastname}] with HNGi7 ID [${id}] using [${language}] for stage 2 task. calvinebunamu@gmail.com`
+
+}
+
+
+
+sentence("Calvin","EbunAmu", "HNG-00191", "JavaScript")
