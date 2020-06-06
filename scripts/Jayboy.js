02c36359dbb8fd2e291031ddc562621547842b9a
NerdPraise <praiseajayi2@gmail.com>
2020-06-06 03:00:55 +0100

    newscripts

--- scripts/Jayboy.js
+++ scripts/Jayboy.js
@@ -1,8 +1,5 @@
-var myInfoSubmite = {
-    fullName: "[Jehoshaphat] [Ugwu] [Obioma]",
-    id: "[HNG-03637]",
-    language: "[Javascript]",
-    email: "ugwuezeobioma@gmail.com",
-};
-const myInfo = `Hello World, this is ${Info.fullName} with HNGi7 ID ${Info.id} using ${Info.language} for stage 2 task. ${Info.email}`;
-console.log(myInfoSubmite);
+const output = (firstName, lastName, id, language) => (
+    `Hello World, this is [${firstName}] [${lastName}] with HNGi7 ID [${id}] using [${language}] for stage 2 task. ugwuezeobioma@gmail.com`
+);
+
+console.log(output("Jehoshaphat", "Ugwu", "Obioma", "HNG-03637" , "JavaScript"));
---
+++ scripts/joshmide.py
@@ -0,0 +1,1 @@
+print ("Hello World, this is [Joshua] [Afolayan] with HNGi7 ID [HNG-01696] using [python] for stage 2 task. afolayanjoshuaolumide97@gmail.com")
