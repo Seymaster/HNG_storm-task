7e9f9cee1df06fc42700651b6f2ec250a71449dc
NerdPraise <praiseajayi2@gmail.com>
2020-06-06 17:28:10 +0100

    new scripts added

--- scripts/klewzy.py
+++ scripts/klewzy.py
@@ -1,5 +1,3 @@
 def details(firstName, lastName, id, language):
-​
     print("Hello World, this is {} {} with HNGi7 ID {} using {} for stage 2 task. klewzyadeeh@yahoo.com".format(firstName, lastName, id, language))
-​
 details('[Ademola]', '[Kolawole]', '[HNG-05027]', '[python]')
