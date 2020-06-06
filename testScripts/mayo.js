fe6e6cfe7f70d3f6c4ab70fb8a79e18301fadb30
Mayowa <63378559+Mayowa-tech@users.noreply.github.com>
2020-06-06 14:19:22 +0100

    Update mayo.js

--- testScripts/mayo.js
+++ testScripts/mayo.js
@@ -2,7 +2,8 @@ var myInfo ={
     hngId: "HNG-03401",
     firstName: "Oluwamayowa",
     lastName: "Alomasojo",
-    language: "PHP & JS"
+    language: "JavaScript",
+    Email:  "mayowaalomasojo@gmail.com"
 }
 function populateMyDetails(myCredential) {
     var outputResponse = `Hello world, this is ${myCredential.lastName}  ${myCredential.firstName} with HNGi7 ID ${myCredential.hngId} using ${myCredential.language} for stage 2 task`;
