5825422b38ff289220963aa125b851ecd39fdbef
davidfayanju1 <62009189+davidfayanju1@users.noreply.github.com>
2020-06-06 19:25:33 +0100

    Create bayo.js

---
+++ bayo.js
@@ -0,0 +1,12 @@
+const projectOutput = function() {
+    const info = {
+        filename : "Bayo-1.js",
+        firstName : "Fayanju",
+        lastName :"Adebayo",
+        id : "HNG-00989",
+        language : "JavaScript",
+        email : "davidfayanju01@gmail.com"
+    }
+    console.log(`Hello World, this is [${info.firstName}] [${info.lastName}] with HNGi7 ID [${info.id}] using [${info.language}] for stage 2 task. ${info.email}`)
+}
+projectOutput()
