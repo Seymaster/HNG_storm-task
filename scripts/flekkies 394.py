6b350bfa822bac229df69dff45cf0ea01be5a200
NerdPraise <praiseajayi2@gmail.com>
2020-06-06 21:07:11 +0100

    new scripts added

--- scripts/flekkies 394.py
+++ scripts/flekkies 394.py
@@ -3,4 +3,4 @@ Lastname ='[Olanrewaju]'
 HNGID= '[HNG-03335]'
 Language ='[python]'
 Email = 'olafolake13@gmail.com'
-print('Hello World, this is '+ Firstname +' '+Lastname+ ' with HNGi7 ID ' + HNGID+ ' using ' + Language + ' for stage 2 task. ' + Email)
+print('Hello World, this is '+ Firstname, Lastname, 'with HNGi7 ID', HNGID,'using', Language, 'for stage 2 task. ' + Email)
