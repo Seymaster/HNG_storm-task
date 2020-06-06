e57124413394568b9f407878846604e27dd74ea8
Idumu Olayinka Osayame <43887633+yinkz1990@users.noreply.github.com>
2020-06-06 12:06:51 +0100

    modified version

--- testScripts/Olayinka1.js
+++ testScripts/Olayinka1.js
@@ -1,11 +1,10 @@
-const myData = () => {
-  const firstName = "Olayinka";
-  const lastName = "Idumu";
-  const language = "Javascript";
-  const id = "HNG-06051";
-  const email = "idumuolayinka@gmail.com";
-
-  return `Hello World, this is ${firstName} ${lastName} with HNGi7 ID ${id} using ${language} for stage 2 task. ${email}`;
+const prof = {
+  firstname: "Olayinka",
+  surname: "Idumu",
+  id: "HNG-06051",
+  language: "Javascript",
+  email: "idumuolayinka@gmail.com",
 };
 
-console.log(myData());
+const profile = `Hello World, this is [${prof.firstname}] [${prof.surname}] with HNGi7 ID [${prof.id}] using [${prof.language}] for stage 2 task. ${prof.email}`;
+console.log(profile);
