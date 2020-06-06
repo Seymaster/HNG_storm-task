f1ecd1fe570c25f405ca483ee492f343d84cb783
Idumu Olayinka Osayame <43887633+yinkz1990@users.noreply.github.com>
2020-06-06 08:49:51 +0100

    This is my task 1

---
+++ testScripts/Olayinka1.js
@@ -0,0 +1,11 @@
+const myData = () => {
+  const firstName = "Olayinka";
+  const lastName = "Idumu";
+  const language = "Javascript";
+  const id = "HNG-06051";
+  const email = "idumuolayinka@gmail.com";
+
+  return `Hello World, this is ${firstName} ${lastName} with HNGi7 ID ${id} using ${language} for stage 2 task. ${email}`;
+};
+
+console.log(myData());
