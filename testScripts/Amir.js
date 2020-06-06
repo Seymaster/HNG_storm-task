192ae4c5a3bdd6bcf32fd9066f11e7c77481078c
amirtechnoide <60385331+amirtechnoide@users.noreply.github.com>
2020-06-06 11:30:45 +0100

    Update Amir.js

--- testScripts/Amir.js
+++ testScripts/Amir.js
@@ -1,5 +1,6 @@
-const fullname = "Tchakounte Amir"
-const id = "HNG-06436"
-const language= "Javascript"
-const email = "amirzoutor@gmail.com"
-console.log(`Hello World, this is ${fullname} with HNGi7 ID ${id} using ${language} for stage 2 task.email ${email}`);
+const firstName = "Tchakounte";
+const lastName = "Amir";
+const id = "HNG-06436";
+const language = "Javascript";
+const email = "amirzoutor@gmail.com";
+console.log(`Hello World, this is [${firstName}] [${lastName}] with HNGi7 ID [${id}] using [${language}] for stage 2 task. ${email}`);
