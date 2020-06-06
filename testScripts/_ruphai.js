2ab352e6fe35f738586bf230ac5136dedb208f66
Rufai Omowunmi Balogun <rufaibalogun1@gmail.com>
2020-06-06 22:31:58 +0100

    added new file

--- testScripts/_ruphai.js
+++ testScripts/_ruphai.js
@@ -5,5 +5,5 @@ const language = '[JavaScript]';
 const email = 'rufaibalogun1@gmail.com';
 
 console.log(
-        `Hello World, this is ${firstName} ${lastName} with HNGi7 ID ${hngId} using ${language} for stage 2 task. ${email}`
+        `Hello World, this is ${firstName} ${lastName} with HNGi7 ID ${id} using ${language} for stage 2 task. ${email}`
     );
