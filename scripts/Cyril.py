e36896ff8aef51379e12ceeea9d01e89ea0bd3b2
NerdPraise <praiseajayi2@gmail.com>
2020-06-06 23:00:13 +0100

    new files to be added

--- scripts/Cyril.py
+++ scripts/Cyril.py
@@ -1,13 +1,17 @@
-import json
-
-# A program that will take me to task 2
-print("Hello World,", 
-
-"this is '[Gabriel] [Ibanga]'",
-
-"with HNGi7 ID '[05494]'," 
-
-"using '[PYTHON 3.8.2]' for stage 2 task.ibgabcy@gmail.com")
 
 
+def details():
+    data = {
+      "first_name": "Gabriel",
+      "last_name": "Ibanga",
+      "id": "HNG-05494",
+      "language": "Python",
+      "email": "ibgabcy@gmail.com"
+    }
 
+    text = "Hello World, this is [{}] [{}] with HNGi7 ID [{}] using [{}] for stage 2 task. {}".format(data['first_name'], data['last_name'], data['id'], data['language'], data['email'])
+    print(text)
+    return text
+
+if __name__ == '__main__':
+	  details()
