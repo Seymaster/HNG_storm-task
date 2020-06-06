8977eba321c20d7e8fc229ff4117921ee2db2dbe
udofiaofonime <66363301+udofiaofonime@users.noreply.github.com>
2020-06-06 11:37:45 +0000

    Update udofiaofonime.js

--- udofiaofonime.js
+++ udofiaofonime.js
@@ -1,5 +1,6 @@
-let name = "Ofonime Udofia";
+let fName = "Ofonime";
+let lname = "Udofia";
 let email = "udofiaofonime@gmail.com";
 let id = "HNG-06339";
 let language = "JavaScript";
-console.log["Hello World, this is " + name + " with HNGi7 ID " + id +  " using " + language + " for stage 2 task."] ;
+console.log("Hello World, this is " + [fName] + [lName] + " with HNGi7 ID " + [id] +  " using " + [language] + " for stage 2 task. "+ [email]) ;
