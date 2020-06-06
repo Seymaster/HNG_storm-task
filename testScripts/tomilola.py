615fe09ea501dc36e46e6cd75865e5849e9f6d9f
Olaniyi Tomilola <54294935+olaniyitomilola@users.noreply.github.com>
2020-06-06 17:02:31 +0100

    Update tomilola.py

--- testScripts/tomilola.py
+++ testScripts/tomilola.py
@@ -1,6 +1,12 @@
-firstname = "Tomilola"
-lastname = "Olaniyi"
-hng_id = 'HNG-01611'
-language = "Python"
+details = {
+    "firstname": "Tomilola",
+    "lastname": "Olaniyi",
+    "ID": "HNG-01611",
+    "lang": "python",
+    "email": "olaniyitomilola@gmail.com",
+    }
+    
 
-print("Hello World, This is [{}] [{}] with HNGi7-ID [{}] using [{}] for stage 2  task. olaniyitomilola@gmail.com".format(firstname, lastname, hng_id, language), flush = True)
+
+echo = "Hello World, this is [{}] [{}] with HNGi7 ID [{}] using [{}] for stage 2 task. {}".format(details["firstname"], details["lastname"], details["ID"],  details["lang"], details["email"],)
+print(echo)
