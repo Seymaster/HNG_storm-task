fdedafbfdc002871b4d4101c420a6470fb442d81
Rufai Omowunmi Balogun <rufaibalogun1@gmail.com>
2020-06-06 21:49:18 +0100

    Task2: Modified

--- testScripts/ruphai.js
+++ testScripts/ruphai.js
@@ -4,10 +4,6 @@ const id = '[HNG-01161]';
 const language = '[JavaScript]';
 const email = 'rufaibalogun1@gmail.com';
 
-function mainOutput() {
-    console.log(
+console.log(
         `Hello World, this is ${firstName} ${lastName} with HNGi7 ID ${hngId} using ${language} for stage 2 task. ${email}`
-    )
-}
-
-mainOutput();
+    );
