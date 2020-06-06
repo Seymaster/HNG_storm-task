9f74e0c6583632a8b7a69682762886160d085d7d
Collins Kiplagat <32549887+cokikip@users.noreply.github.com>
2020-06-06 12:12:38 +0300

    Update
    New changes

--- scripts/cokikip.js
+++ scripts/cokikip.js
@@ -1,6 +1,6 @@
-var name="Collins Kiplagat Kiprono";
+var name="Collins Kiplagat";
 var hngi7ID="HNG-02589";
-var language="PHP,Javascript";
+var language="PHP";
 var email="cokikipla@gmail.com"
 
 console.log("Hello World, this is "+name+" with HNGi7 ID "+hngi7ID+" using "+language+ " for stage 2 task. "+email );
