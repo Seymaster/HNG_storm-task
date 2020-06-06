c7f84bc4dd984c38b3e2667abbac3cc2c65c1fe4
Collins Kiplagat <32549887+cokikip@users.noreply.github.com>
2020-06-06 13:21:35 +0300

    Updates changes
    corecting slack id and email

--- scripts/cokikip.js
+++
@@ -1,6 +0,0 @@
-var name="Collins Kiplagat";
-var hngi7ID="HNG-02589";
-var language="PHP";
-var email="cokikipla@gmail.com"
-
-console.log("Hello World, this is "+name+" with HNGi7 ID "+hngi7ID+" using "+language+ " for stage 2 task. "+email );
---
+++ scripts/cokikip.php
@@ -0,0 +1,10 @@
+<?php
+$fname = "Collins";
+$lname = "Kiplagat";
+$id = "HNG-02589";
+$language = 'PHP';
+function output($fname, $lname, $id, $language){
+    echo "Hello World, this is [" .$fname ."] [" .$lname ."] with HNGi7 ID [" .$id ."] using [" .$language ."] for stage 2 task. cokikipla@gmail.com";
+}
+output($fname , $lname, $id, $language);
+?>
