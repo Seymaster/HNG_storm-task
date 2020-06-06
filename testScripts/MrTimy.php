87b868e58ac9306b4f109b87c1e924f5f8e946a6
Eke Timothy Chiemeziem <eketimothy84@gmail.com>
2020-06-06 01:47:37 -0700

    chore: create script

---
+++ testScripts/MrTimy.php
@@ -0,0 +1,13 @@
+<?php 
+
+$fN = "Eke";
+$lN = "Timothy";
+$hngId = "HNG-05937";
+$language = 'PHP';
+
+function printOutput($fname, $lname, $id, $language){
+	echo "Hello World, this is [" .$fname ."] [" .$lname ."] with HNGi7 ID [" .$id ."] using [" .$language ."] for stage 2 task. timothy.eke.235043@unn.edu.ng";
+}
+
+printOutput($fN , $lN, $hngId, $language);
+?>
