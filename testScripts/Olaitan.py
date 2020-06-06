05dbb5ca6b87eaeceebacb7a6c737f062afbee85
olaitanlasisi <orlaeetern@gmail.com>
2020-06-06 00:25:27 -0700

    chore: create script

---
+++ testScripts/Olaitan.py
@@ -0,0 +1,11 @@
+#Define the function intern's details.
+
+def interns_details():
+    first_name = "[Olaitan]"
+    sur_name = "[Lasisi]"
+    hng_id = "[HNG-04774]"
+    language = "[Python]"
+    email = "orlaeetern@gmail.com"
+    #concatenate the output
+    print("Hello World, this is {} {} with HNGi7 ID {} using {} for stage 2 task. {}").format(first_name, sur_name, hng_id, language, email)
+interns_details()
