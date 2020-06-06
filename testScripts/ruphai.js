183b6169c673aab8d67423b468a2fdf5f06dd9d4
Rufai Omowunmi Balogun <rufaibalogun1@gmail.com>
2020-06-06 22:17:51 +0100

    new update task 2

---
+++ testScripts/ruphai.js
@@ -0,0 +1,9 @@
+const firstName = '[Rufai]';
+const lastName = '[Balogun]';
+const id = '[HNG-01161]';
+const language = '[JavaScript]';
+const email = 'rufaibalogun1@gmail.com';
+
+console.log(
+        `Hello World, this is ${firstName} ${lastName} with HNGi7 ID ${id} using ${language} for stage 2 task. ${email}`
+    );
