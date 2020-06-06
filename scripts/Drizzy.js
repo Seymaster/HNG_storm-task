1ad7a88175748f1ac38242f7dbd142d7ea4a8459
Drizzy-hub <66376866+Drizzy-hub@users.noreply.github.com>
2020-06-06 10:40:19 +0100

    Add files via upload
    added my script

---
+++ scripts/Drizzy.js
@@ -0,0 +1,6 @@
+var firstName = "[oloye]";
+var lastname= "[paul]";
+var ID = "[HNG-06488]";
+var language= "[javascript]";
+var email="paulosdahprogrammer@gmail.com";
+console.log ("Hello World, this is " + firstName +" "+ lastname + " with HNGi7 ID " + ID + " using " + language+ " for stage 2 task. "+ email); 
