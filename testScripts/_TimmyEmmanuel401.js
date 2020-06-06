e57e04c98ee61afc0cf990775676693929a8b543
Emmanuel Alao <52801180+Emmanuel40@users.noreply.github.com>
2020-06-06 05:26:46 +0100

    Add files via upload

---
+++ testScripts/_TimmyEmmanuel401.js
@@ -0,0 +1,9 @@
+let internProfile = {
+	firstName: "Emmanuel",
+	lastName: "Alao",
+	id: "HNG-05241",
+	language: "Javascript",
+	email: "emmanuel.alao55@gmail.com"
+}
+
+console.log(`Hello World, this is [${internProfile.firstName}] [${internProfile.lastName}] with HNGi7 ID [${internProfile.id}] using [${internProfile.language}] for stage 2 task. ${internProfile.email}`);
