45b60d2f0d7b856abc9515630a59ace1633b99f3
ogunbanwo opeyemi <58431589+papycoda@users.noreply.github.com>
2020-06-06 11:50:05 +0100

    Add files via upload
    it passed the repl

---
+++ testScripts/__papycoda__.py
@@ -0,0 +1,12 @@
+def details():
+    firstname = '[Opeyemi]'
+    lastname = '[Ogunbanwo]'
+    hng_id = '[HNG-05135]'
+    language = '[Python]'
+    email = 'opeyemi655@gmail.com'
+
+    print("Hello World, this is {} {} with HNGi7 ID {} using {} for stage 2 task. {}".format(lastname,firstname,hng_id,language,email))
+
+
+
+details()
