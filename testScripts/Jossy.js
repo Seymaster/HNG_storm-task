611cded05236ce8ffb5ffb9b9dea1db4e5606b1c
Giwa Jossy <giwajosef@gmail.com>
2020-06-06 02:07:26 +0200

    Created Jossy.js

---
+++ testScripts/Jossy.js
@@ -0,0 +1,6 @@
+const fullname = "Giwa Jossy";
+const identity = "HNG-01584";
+const language= "Javascript";
+const email = "giwajossy@gmail.com";
+
+console.log(`Hello World, this is ${fullname} with HNGi7 ID ${identity} using ${language} for stage 2 task email ${email}`);
