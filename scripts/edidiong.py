2c2e70c4051970f3f7f8fb9a8b17fae492ff96ec
NerdPraise <praiseajayi2@gmail.com>
2020-06-07 14:19:02 +0100

    new script added

---
+++ scripts/edidiong.py
@@ -0,0 +1,6 @@
+email = "edyzinspiring@gmail.com"
+firstName = "[Edidiong]"
+lastName = "[Ekanem]"
+id_number = "[HNG-05206]"
+language = "[python]"
+print("Hello World, this is {} {} with HNGi7 ID {} using {} for stage 2 task. {}".format(firstName, lastName, id_number, language, email))
