304a8e1f6f2bb283673fbdc7e8be02a2e7fa507f
mbajoshuac <angelicjoekeyz@gmail.com>
2020-06-07 15:17:38 +0100

    corrected the script

--- testScripts/mbajoshuac.js
+++ testScripts/mbajoshuac.js
@@ -1,5 +1,8 @@
-function profile() {
-    var output = "Hello World, this is [Joshua] [Mba] with HNGi7 ID [HNG-04870] using [Javascript] for stage 2 task.angelicjoekeyz@gmail.com";
-    console.log(output);
-}
-profile();
+
+
+var myName = "[Joshua] [Mba]";
+var HNG_ID = "[HNG-04870]";
+var ChosenLang = "[JavaScript]";
+var myEmail = "angelicjoekeyz@gmail.com";
+var Output = "Hello World, this is " + myName + " with HNGi7 ID " + HNG_ID + " using " + ChosenLang + " for stage 2 task. " + myEmail;
+console.log(Output);
