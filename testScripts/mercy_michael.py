779cbab720bc47aab0e698f7ad04b0b1f546d0db
LadyPeculiar <mercymike52@gmail.com>
2020-06-07 15:35:59 +0100

    script: my script

---
+++ testScripts/mercy_michael.py
@@ -0,0 +1,7 @@
+firstName = "[Mercy]"
+lastName = "[Michael]"
+hngId = "[HNG-00922]"
+lang = "[python]"
+emailAddress = "mercymike52@gmail.com"
+
+print("Hello World, this is {} {} with HNGi7 ID {} using {} for stage 2 task. {}".format(firstName, lastName, hngId, lang, emailAddress))
