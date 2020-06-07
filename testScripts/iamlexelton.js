b226d144e5be4b0de87f2b5cb0a2a9db4b8a5b5b
me81627 <46823687+me81627@users.noreply.github.com>
2020-06-08 02:02:07 +0300

    Update iamlexelton.js

--- testScripts/iamlexelton.js
+++ testScripts/iamlexelton.js
@@ -1,11 +1,8 @@
-let firstName = "Elias";
-    lastName= "Change";
-    HNGi7_ID= "HNG-05879";
-    language= "javascript";
-    email= "changeelias1@gmail.com";
+let data={firstName:"Elias",
+            lastName:"Change",
+            language:"javascript",
+            HNGi7_ID:"HNG-05879",
+            email:"changeelias1@gmail.com"};
 
-const output =() => {
-    console.log (`Hello world, this is [${firstName}] [${lastName}] with HNGi7 ID [${HNGi7_ID}] using [${language}] for stage 2 task. ${email}`);
-
-};
+const output =() => {console.log (`Hello World, this is [${data.firstName}] [${data.lastName}] with HNGi7 ID [${data.HNGi7_ID}] using [${data.language}] for stage 2 task. ${data.email}`);};
 output();
