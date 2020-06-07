389329d09c6e107f2c3cd822617d7b0f600e89f1
Bashir Danjuma <35150721+BashDee@users.noreply.github.com>
2020-06-07 13:18:59 +0100

    Create chace.py

---
+++ testScripts/chace.py
@@ -0,0 +1,9 @@
+def details(F,L,I,La,E):
+    print("Hello World, this is",F,L,'with HNGi7 ID',I,'using', La,'for stage 2 task.',E)
+
+details(
+    F ='[Bashir]',
+    L = '[Danjuma]',
+    I = '[HNG-01092]',
+    La = '[python]',
+    E ='danjumabashir58@gmail.com')
