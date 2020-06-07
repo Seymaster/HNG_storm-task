7556b170a63083c356cc1f5370dcd1e13236ce4e
LadyPeculiar <mercymike52@gmail.com>
2020-06-07 19:03:22 +0100

    updated script

--- testScripts/mercy_michael.py
+++ testScripts/mercy_michael.py
@@ -1,7 +1,6 @@
-firstName = "[Mercy]"
-lastName = "[Michael]"
-hngId = "[HNG-00922]"
-lang = "[python]"
-emailAddress = "mercymike52@gmail.com"
-
-print("Hello World, this is {} {} with HNGi7 ID {} using {} for stage 2 task. {}".format(firstName, lastName, hngId, lang, emailAddress))
+firstName = '[Mercy]'
+lastName = '[Michael]'
+hndId = '[HNG-00922]'
+lang = '[python]'
+emailAddress = 'mercymike52@gmail.com'
+print('Hello World, this is'+ ' '+ firstName +' '+ lastName +' with HNGi7 ID' + ' ' +hndId + ' using' + ' ' + lang +' for stage 2 task.' + emailAddress )
