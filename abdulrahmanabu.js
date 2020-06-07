212b891f053564eec3374433e1f54da799c845ec
AbdulRahman(Master Ray) <aburahman430@gmail.com>
2020-06-07 09:24:13 +0100

    adding my HNG script for AbdulRahman Abu

---
+++ abdulrahmanabu.js
@@ -0,0 +1,4 @@
+const myDetails = (name, id, email, language) => {
+    console.log(`Hello World, this is [${name}] with HNGi7 ID [${id}]  using [${language}] for Stage 2. [${email}]`);
+}
+myDetails("AbdulRahman-Abu", "HNG-03461", "aburahman430@gmail.com", "JavaScript");
