e0ba7d05147fa1d7a6aa71b95ab2c9504003b0a1
mbajoshuac <angelicjoekeyz@gmail.com>
2020-06-07 19:00:20 +0100

    renamed file

---
+++ testScripts/mbajoshua.js
@@ -0,0 +1,6 @@
+var myName = "[Joshua] [Mba]";
+var HNG_ID = "[HNG-04870]";
+var ChosenLang = "[JavaScript]";
+var myEmail = "angelicjoekeyz@gmail.com";
+var Output = "Hello World, this is " + myName + " with HNGi7 ID " + HNG_ID + " using " + ChosenLang + " for stage 2 task. " + myEmail;
+console.log(Output);
