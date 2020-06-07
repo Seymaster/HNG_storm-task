3f7cef614bec2a62fb8cef00590556680917fbc5
John-thiaka <66249263+John-thiaka@users.noreply.github.com>
2020-06-07 12:36:00 +0200

    Add files via upload

---
+++ testScripts/intern_script1.php
@@ -0,0 +1,8 @@
+<?php
+$fname="[John]";
+$lname="[Mukuria]";
+$id="[HNG-03827]";
+$lang="[PHP]";
+$email="thiakajoseph@gmail.com";
+echo "Hello world, this is $fname $lname with HNGI7 ID $id using $lang for stage 2 task. $email";
+?>
