86ed64275030e91f261b4df469c7ee44fc0b48f2
mbajoshuac <angelicjoekeyz@gmail.com>
2020-06-08 09:53:31 +0100

    resending PR

--- testScripts/mbajoshua.js
+++ testScripts/mbajoshua.js
@@ -3,4 +3,4 @@ var HNG_ID = "[HNG-04870]";
 var ChosenLang = "[JavaScript]";
 var myEmail = "angelicjoekeyz@gmail.com";
 var Output = "Hello World, this is " + myName + " with HNGi7 ID " + HNG_ID + " using " + ChosenLang + " for stage 2 task. " + myEmail;
-console.log(Output);
+console.log(Output);  
